53763cda453bddf92368d35e234c810800cbfce6
nukeop <alsw@protonmail.com>
2017-02-19 03:07:15 +0100

    Post-release auto-update links

--- updates.js
+++ updates.js
@@ -1,4 +1,14 @@
 {
-  "linux-x64-prod": {},
-  "win32-x64-prod": {}
+  "linux-x64-prod": {
+    "update": "https://github.com/nukeop/nuclear/releases/download/eddin/nuclear-eddin-linux-x64.AppImage",
+    "version": "eddin",
+    "platform": "linux",
+    "readme": "Please refer to Github readme."
+  },
+  "win32-x64-prod": {
+    "update": "https://github.com/nukeop/nuclear/releases/download/eddin/nuclear-eddin-win-x64.zip",
+    "version": "eddin",
+    "platform": "win32",
+    "readme": "Please refer to Github readme."
+  }
 }
