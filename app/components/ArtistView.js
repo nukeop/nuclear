c11953929e019391d5e16d122de39d427b03a798
nukeop <alsw@protonmail.com>
2017-03-11 17:18:14 +0100

    New artist page header

--- app/components/ArtistView.js
+++ app/components/ArtistView.js
@@ -13,33 +13,41 @@ export default class ArtistView extends Component {
     return (
       <div className={styles.artist_view_container}>
 
-        <table>
-          <tr>
-            <td>
-              <img className={styles.artist_view_main_photo} src={this.props.artist.image[2]['#text']} />
-            </td>
-            <td className={styles.artist_view_info_cell}>
-              <div className={styles.artist_view_artist_name}>{this.props.artist.name}</div>
-              {
-                this.props.artist.ontour==='1'
-                ? <div className={styles.artist_view_on_tour}>ON TOUR!</div>
-                : null
-              }
-              <div className={styles.artist_view_tags}>
-                {
-                  this.props.artist.tags.tag.map((el, i) => {
-                    return (
-                      <div className={styles.artist_view_tag}>{'#' + el.name}</div>
-                    );
-                  })
-                }
-              </div>
-              <div className={styles.artist_view_bio}>
-                {this.props.artist.bio.summary.split('<a')[0]}
-              </div>
-            </td>
-          </tr>
-        </table>
+        <div className={styles.artist_view_header}>
+          <div className={styles.artist_view_header_img} style={{backgroundImage: 'url('+this.props.artist.image[4]['#text']+')'}} />
+
+          <div className={styles.artist_view_header_overlay}>
+            <table>
+              <tr>
+                <td>
+                  <img className={styles.artist_view_main_photo} src={this.props.artist.image[2]['#text']} />
+                </td>
+                <td className={styles.artist_view_info_cell}>
+                  <div className={styles.artist_view_artist_name}>{this.props.artist.name}</div>
+                  {
+                    this.props.artist.ontour==='1'
+                    ? <div className={styles.artist_view_on_tour}>ON TOUR!</div>
+                    : null
+                  }
+                  <div className={styles.artist_view_tags}>
+                    {
+                      this.props.artist.tags.tag.map((el, i) => {
+                        return (
+                          <div className={styles.artist_view_tag}>{'#' + el.name}</div>
+                        );
+                      })
+                    }
+                  </div>
+                  {/* <div className={styles.artist_view_bio}>
+                    {this.props.artist.bio.summary.split('<a')[0]}
+                  </div> */}
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+
+        <hr className={styles.artist_view_hr}/>
 
         <table style={{marginTop: '24px', padding: '12px 12px 12px 12px'}}>
           <tr>
