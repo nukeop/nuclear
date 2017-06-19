b9b015a6d9aaf63abfbff3d1f944a0f080ae9520
nukeop <alsw@protonmail.com>
2017-06-19 23:42:09 +0200

    Display artist thumbnails

--- app/components/SearchResults/ArtistResults/index.js
+++ app/components/SearchResults/ArtistResults/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './styles.css';
 
-import AlbumCoverContainer from '../../../containers/AlbumCoverContainer';
+import AlbumCover from '../../../components/AlbumCover';
 import Header from '../../Header';
 
 var _ = require('lodash');
@@ -12,6 +12,16 @@ class ArtistResults extends React.Component {
   renderArtists() {
     if(_.has(this.props.artists, 'results')){
       console.log(this.props.artists.results);
+      return this.props.artists.results.map((el, i) => {
+        return (
+          <AlbumCover
+            nameOnly
+            key={i}
+            artist={el.title}
+            cover={el.thumb}
+          />
+        );
+      });
     }
   }
 
