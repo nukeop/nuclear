b64487597570fbfe9bb4bae417bd609324215d41
nukeop <alsw@protonmail.com>
2017-07-01 21:51:01 +0200

    React and Redux dev tools

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
@@ -13,6 +14,16 @@ function createWindow() {
       experimentalFeatures: true
     }
   });
+
+
+  installExtension(REACT_DEVELOPER_TOOLS)
+  .then((name) => console.log(`Added Extension:  ${name}`))
+  .catch((err) => console.log('An error occurred: ', err));
+
+
+  installExtension(REDUX_DEVTOOLS)
+  .then((name) => console.log(`Added Extension:  ${name}`))
+  .catch((err) => console.log('An error occurred: ', err));
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
