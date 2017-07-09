215dcd1e744b1dbb26adfe113db81f8de67dc02f
nukeop <alsw@protonmail.com>
2017-07-09 23:58:29 +0200

    Album view progress

--- app/components/AlbumView/index.js
+++ app/components/AlbumView/index.js
@@ -11,12 +11,28 @@ class AlbumView extends React.Component {
   render() {
     return (
       <div className={styles.album_view_container}>
-
-        <Dimmer active={this.props.album.loading}>
-          <Loader  />
-
-          {this.props.album.title}
+        <Dimmer.Dimmable>
+          <Dimmer active={this.props.album.loading}>
+            <Loader/>
           </Dimmer>
+
+          {
+            this.props.album.loading
+            ? null
+            : <div className={styles.album_info_box}>
+              <img src={this.props.album.images[0].uri}/>
+              <div className={styles.album_details}>
+                <div className={styles.album_title}>{this.props.album.title}</div>
+                <div className={styles.album_artist}>by {this.props.album.artists[0].name}</div>
+                <div className={styles.album_genre}>Genre: {this.props.album.styles[0]}</div>
+
+                <div className = {styles.album_year}>Year: {this.props.album.year}</div>
+                <div>Tracks: {this.props.album.tracklist.length}</div>
+              </div>
+            </div>
+          }
+
+        </Dimmer.Dimmable>
       </div>
     );
   }
