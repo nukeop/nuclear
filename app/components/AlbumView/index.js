55a78ee6e174cce075b3296382c7a59259c9efd4
nukeop <alsw@protonmail.com>
2017-07-26 00:25:18 +0200

    album view track list

--- app/components/AlbumView/index.js
+++ app/components/AlbumView/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 import {Dimmer, Loader, Image, Segment} from 'semantic-ui-react'
 
 import styles from './styles.scss';
@@ -18,19 +19,53 @@ class AlbumView extends React.Component {
 
           {
             this.props.album.loading
-            ? null
-            : <div className={styles.album_info_box}>
-              <img src={this.props.album.images[0].uri}/>
-              <div className={styles.album_details}>
-                <div className={styles.album_title}>{this.props.album.title}</div>
-                <div className={styles.album_artist}>by {this.props.album.artists[0].name}</div>
-                <div className={styles.album_genre}>Genre: {this.props.album.styles[0]}</div>
+              ? null
+              : <div className={styles.album}>
+                  <div className={styles.album_info_box}>
+                    <img src={this.props.album.images[0].uri}/>
+                    <div className={styles.album_details}>
+                      <div className={styles.album_title}>{this.props.album.title}</div>
+                      <div className={styles.album_artist}>by {this.props.album.artists[0].name}</div>
+                      <div className={styles.album_genre}>
+                        <label>Genre:</label>
+                        {this.props.album.styles[0]}</div>
 
-                <div className = {styles.album_year}>Year: {this.props.album.year}</div>
-                <div>Tracks: {this.props.album.tracklist.length}</div>
-              </div>
-            </div>
+                      <div className={styles.album_year}>
+                        <label>Year:</label>
+                        {this.props.album.year}</div>
+                      <div className={styles.album_tracks}>
+                        <label>Tracks:</label>
+                        {this.props.album.tracklist.length}</div>
+                    </div>
+
+                  </div>
+
+                  <table className={styles.album_tracklist}>
+                    <thead>
+                      <tr>
+                        <th className={styles.center}><FontAwesome name="hashtag" /></th>
+                        <th className={styles.left}>Song</th>
+                        <th className={styles.center}><FontAwesome name="clock-o" /></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {
+                      this.props.album.tracklist.map((el, i) => {
+                        return (
+                          <tr>
+                            <td className={styles.center}>{i + 1}</td>
+                            <td className={styles.left}>{el.title}</td>
+                            <td className={styles.center}>{el.duration}</td>
+                          </tr>
+                        )
+                      })
+                    }
+                  </tbody>
+                  </table>
+
+                </div>
           }
+
 
         </Dimmer.Dimmable>
       </div>
