0d83c7f1feb9ce4421a0ce6239807f47fe26b516
nukeop <nukeop@gumblert.tech>
2017-10-06 16:00:02 +0200

    playlist view

--- app/components/PlaylistView/index.js
+++ app/components/PlaylistView/index.js
@@ -3,14 +3,43 @@ import React from 'react';
 import styles from './styles.scss';
 
 class PlaylistView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
   	let {
   		playlist
   	} = this.props;
 
     return (
-      <div>
-        Playlist view placeholder
+      <div className={styles.playlist_view_container}>
+        <div className={styles.playlist}>
+          <div className={styles.playlist_info}>
+            <div>
+              <img 
+                className={styles.playlist_thumbnail} 
+                src={playlist.tracks[0].thumbnail} 
+              />
+            </div>
+            <div className={styles.playlist_name}>
+              {playlist.name}
+            </div>
+            
+
+          </div>
+          
+          {
+            playlist.tracks.map(track => {
+              return (
+                <div>
+                  <img src={track.thumbnail} />
+                  {track.artist} - {track.name}
+                </div>
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
