77e34e2b48ef3fa147bd724c1b427609703a5948
nukeop <alsw@protonmail.com>
2018-01-24 16:57:00 +0100

    Tag view page

--- app/components/TagView/index.js
+++ app/components/TagView/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import {Dimmer, Loader} from 'semantic-ui-react';
 
+import TagHeader from './TagHeader';
+import TagTopArtists from './TagTopArtists';
 import styles from './styles.scss';
 
 class TagView extends React.Component {
@@ -17,10 +20,39 @@ class TagView extends React.Component {
       tag,
       tags
     } = this.props;
+
+    let tagInfo, topTracks, topAlbums, topArtists;
+    if (tags[tag] && tags[tag].loading !== true) {
+      tagInfo = tags[tag][0].tag;
+      topTracks = tags[tag][1].tracks.track;
+      topAlbums = tags[tag][2].albums.album;
+      topArtists = tags[tag][3].topartists.artist;
+    }
     
     return (
-      <div>
-	{tag}
+      <div className={styles.tag_view_container}>
+	<Dimmer.Dimmable>
+	  <Dimmer active={tags[tag] === undefined || tags[tag].loading}>
+	    <Loader/>
+          </Dimmer>
+
+	  {
+	    tags[tag] === undefined || tags[tag].loading
+	      ? null
+	      : (
+		<div className={styles.tag_view}>
+                  <TagHeader
+		     tag={tag}
+		     tagInfo={tagInfo}
+		     topArtists={topArtists}
+		     />
+		  <TagTopArtists
+		     topArtists={topArtists}
+		     />
+		</div>
+	      )
+	  }
+	</Dimmer.Dimmable>
       </div>
     );
   }
