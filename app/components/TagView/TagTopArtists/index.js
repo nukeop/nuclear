04c226bad0417aeb9380074b1ad5d177b4e496d9
nukeop <alsw@protonmail.com>
2018-01-24 16:57:00 +0100

    Tag view page

---
+++ app/components/TagView/TagTopArtists/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import styles from './styles.scss';
+
+class TagTopArtists extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let {
+
+    } = this.props;
+    return (
+      <div className={styles.tag_top_artists}>
+      </div>
+    );
+  }
+}
+
+export default TagTopArtists;
