03f0f0bedae16d1485e8d67556d71f90b4434c29
nukeop <alsw@protonmail.com>
2018-01-30 00:13:35 +0100

    Add top albums to tag view

---
+++ app/components/TagView/TagTopList/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import classnames from 'classnames';
+import _ from 'lodash';
+
+import styles from './styles.scss';
+
+class TagTopList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let {
+      topList,
+      header
+    } = this.props;
+
+    return(
+      <div className={styles.tag_top_list}>
+	<h4>{header}</h4>
+	<div className={styles.top_list_items}>
+	  <div className={styles.top_item}>
+	    <div
+	      className={styles.top_item_photo}
+	      style={{backgroundImage: `url(${_.last(topList[0].image)['#text']})`}}
+	      />
+            <div
+	      className={styles.item_overlay}
+	      >
+              <div className={styles.item_name}>{topList[0].name}</div>
+	    </div>
+	  </div>
+	  <div className={styles.other_items}>
+	    {
+	      topList.slice(1, 5).map((item, i) => {
+		return (
+                  <div
+		    key={i}
+		    className={styles.other_item}
+		    >
+		    <div
+		      className={styles.other_item_photo}
+		      style={{backgroundImage: `url(${_.last(item.image)['#text']})`}}
+		      />
+		    <div className={styles.item_overlay}
+			 >
+		      <div className={classnames(styles.item_name, styles.other_item_name)}>{item.name}</div>
+		    </div>
+		  </div>
+		);
+	      })
+	  }
+      </div>
+	</div>
+	</div>
+    );
+  }
+}
+
+export default TagTopList;
