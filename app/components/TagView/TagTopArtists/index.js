b92b50603bacb6f62387c0af3dd663054b68ae0e
nukeop <alsw@protonmail.com>
2018-01-29 16:44:19 +0100

    Tag description and top artists

--- app/components/TagView/TagTopArtists/index.js
+++ app/components/TagView/TagTopArtists/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 
 import styles from './styles.scss';
 
@@ -9,10 +10,29 @@ class TagTopArtists extends React.Component {
 
   render() {
     let {
-
+      topArtists
     } = this.props;
+    console.log(topArtists)
     return (
       <div className={styles.tag_top_artists}>
+	<h4>Top Artists</h4>
+        <div className={styles.top_artists}>
+          <div className={styles.top_artist}>
+            <div
+	       className={styles.top_artist_photo}
+	       style={{backgroundImage: `url(${_.last(topArtists[0].image)['#text']})`}}
+	       />
+            <div
+	       className={styles.artist_overlay}
+	       >
+              <div className={styles.artist_name}>{topArtists[0].name}</div>
+	    </div>
+	  </div>
+
+          <div className={styles.other_artists}>
+
+	  </div>
+	</div>
       </div>
     );
   }
