18d3450f45e4a4058a6c99304ca3a52ad51890b2
nukeop <alsw@protonmail.com>
2018-02-15 00:36:06 +0100

    Queue popups development, remove rollbar from dev version

--- app/components/QueuePopup/index.js
+++ app/components/QueuePopup/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Popup } from 'semantic-ui-react';
+import _ from 'lodash';
+import {
+  Dropdown,
+  Popup
+} from 'semantic-ui-react';
 
 import styles from './styles.scss';
 
@@ -30,51 +34,68 @@ class QueuePopup extends React.Component {
   render() {
     let {
       trigger,
-      track
+      track,
+      musicSources
     } = this.props;
-    console.log(track);
+    
+    let dropdownOptions = _.map(musicSources, s => {
+      return {
+	key: s.sourceName,
+	text: s.sourceName,
+	value: s.sourceName,
+	content: s.sourceName
+      };
+    });
     return (
       <div
 	 onContextMenu={this.toggleOpen.bind(this)}
 	 >
 	<Popup
-	   className={styles.queue_popup}
-	   trigger={
-	       <div
-		    ref={element => { this.container = element; }}
-		   >
-	       {trigger}
-	     </div>
-      }
-      open={this.state.isOpen}
-      onClose={this.handleClose.bind(this)}
-      onOpen={this.handleOpen.bind(this)}
-      hideOnScroll
-      position='left center'
-      on=''
-      >
-      {
-	track.streams && Object.keys(track.streams).length > 0
-	  ? (
-	    <div className={styles.stream_info}>
-              <div className={styles.stream_source}>
-		Source: <span className={styles.stream_source_string}>{track.streams[0].source}</span>
-	      </div>
-              <div className={styles.stream_title}>
-		Title: {track.streams[0].title}
-	      </div>
-              <div className={styles.stream_id}>
-		Stream ID: {track.streams[0].id}
-	      </div>
-              <img alt="" src={track.streams[0].thumbnail} />
-	      
+	  className={styles.queue_popup}
+	  trigger={
+	      <div
+		   ref={element => { this.container = element; }}
+		  >
+	      {trigger}
 	    </div>
-	  )
-	: null
-      }
+	  }
+	  open={this.state.isOpen}
+	  onClose={this.handleClose.bind(this)}
+	  onOpen={this.handleOpen.bind(this)}
+	  hideOnScroll
+	  position='left center'
+	  on=''
+        >
+	  {
+	    track.streams && Object.keys(track.streams).length > 0
+	      ? (
+		<div className={styles.stream_info}>
+		  <div className={styles.stream_thumbnail}>
+		    <img alt="" src={track.streams[0].thumbnail} />
+		  </div>
+		  <div className={styles.stream_text_info}>
+		    <div className={styles.stream_source}>
+		      Source: <Dropdown
+				inline
+				options={dropdownOptions}
+				defaultValue={_.find(dropdownOptions, o => o.value === track.streams[0].source).value}
+			/>
+		    </div>
+		    <div className={styles.stream_title}>
+		      Title: {track.streams[0].title}
+		    </div>
+		    <div className={styles.stream_id}>
+		      Stream ID: {track.streams[0].id}
+		    </div>
+		  </div>
+
+		</div>
+	      )
+	    : null
+	  }
       
-      </Popup>
-</div>
+        </Popup>
+      </div>
     );
   }
 }
--- app/plugins/MusicSources/YoutubePlugin.js
+++ app/plugins/MusicSources/YoutubePlugin.js
@@ -8,6 +8,7 @@ class YoutubePlugin extends MusicSourcePlugin {
   constructor() {
     super();
     this.name = 'Youtube Plugin';
+    this.sourceName = 'Youtube';
     this.description = 'A plugin allowing nuclear to search for music and play it from youtube';
   }
 
