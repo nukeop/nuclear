a02cf79af53b604ebeb489ebb4066e5da77aadbc
nukeop <alsw@protonmail.com>
2018-02-20 23:50:08 +0100

    Store options state in local db

--- app/actions/settings.js
+++ app/actions/settings.js
@@ -3,6 +3,8 @@ import { store } from '../persistence/store';
 export const SET_BOOLEAN_OPTION = 'SET_BOOLEAN_OPTION';
 
 export function setBooleanOption(option, state) {
+  store.set(`settings.${option}`, state).write();
+
   return {
     type: SET_BOOLEAN_OPTION,
     payload: {option, state}
