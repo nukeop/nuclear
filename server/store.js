bbb7ddb805a5a523a08905dc6c8597478f8eacd5
nukeop <alsw@protonmail.com>
2018-02-22 15:26:42 +0100

    Use default option value if it's not present in db

--- server/store.js
+++ server/store.js
@@ -1,8 +1,19 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const _ = require('lodash');
+const options = require('../app/constants/settings');
 
 const store = low(new FileSync('nuclear.json'));
 
+function getOption(key) {
+  var value = store.get(key).value();
+  if (!value || Object.keys(value).length < 1) {
+    value = _.find(options, option => option.name === key);\
+  }
+
+  return value;
+};
+
 module.exports = {
-  store
+  getOption
 };
