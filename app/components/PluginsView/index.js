d4962bc39fbd4bc7e1a6f6c33d13878caee524fe
nukeop <alsw@protonmail.com>
2018-03-02 14:04:07 +0100

    Plugins view progress

--- app/components/PluginsView/index.js
+++ app/components/PluginsView/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import _ from 'lodash';
 
 import Header from '../Header';
 import styles from './styles.scss';
@@ -10,13 +12,41 @@ class PluginsView extends React.Component {
 
   render() {
     let {
-
+      plugins
     } = this.props;
     return (
       <div className={styles.plugins_view_container}>
         <Header>
           Plugins
         </Header>
+        <div className={styles.plugin_settings}>
+          <Menu vertical inverted>
+            {
+              _.map(plugins, (category, key) => {
+                return (
+                  <Menu.Item>
+                    <Menu.Header>{key}</Menu.Header>
+                    <Menu.Menu>
+                      {
+                        category.map(plugin => {
+                          return (
+                            <Menu.Item>
+                              {plugin.name}
+                            </Menu.Item>
+                          );
+                        })
+                      }
+                    </Menu.Menu>
+                  </Menu.Item>
+                );
+              })
+            }
+          </Menu>
+          <div className={styles.plugin_well}>
+            Plugin settings
+          </div>
+        </div>
+
       </div>
     );
   }
