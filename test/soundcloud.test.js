cb22e1e91d15278d2534a012ff944e281faec5c6
nukeop <alsw@protonmail.com>
2018-03-05 01:37:53 +0100

    Soundcloud api and tests

---
+++ test/soundcloud.test.js
@@ -0,0 +1,15 @@
+import { soundcloudSearch } from '../app/rest/Soundcloud';
+import { expect } from 'chai';
+
+describe('Soundcloud REST API tests', () => {
+  it('performs a basic search', () => {
+    soundcloudSearch('death grips - get got')
+    .then(data => data.json())
+    .then(results => {
+      expect(results[0]).to.be.an('object').that.includes.all.keys('id', 'title', 'duration', 'stream_url');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  });
+});
