af40d3f58d2e9236d4bda45985c130f20edfd4b7
nukeop <alsw@protonmail.com>
2018-03-05 01:37:53 +0100

    Soundcloud api and tests

--- app/rest/Youtube.js
+++ app/rest/Youtube.js
@@ -4,10 +4,6 @@ function prepareUrl(url) {
   return `${url}&key=${globals.ytApiKey}`;
 }
 
-function trackSearch(track) {
+export function trackSearch(track) {
   return fetch(prepareUrl("https://www.googleapis.com/youtube/v3/search?part=id,snippet&type=video&maxResults=50&q="+encodeURIComponent(track)));
 }
-
-module.exports = {
-  trackSearch
-};
