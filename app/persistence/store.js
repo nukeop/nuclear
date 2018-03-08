85016740d7b1d7d7cbf7966e72f4502ad189107e
nukeop <alsw@protonmail.com>
2018-03-08 16:19:32 +0100

    Store database in user data folder

--- app/persistence/store.js
+++ app/persistence/store.js
@@ -1,14 +1,15 @@
+let { app } = require('electron').remote;
+import path from 'path';
 import low from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 
-const store = low(new FileSync('nuclear.json'));
+const store = low(new FileSync(path.join(app.getPath('userData'), 'nuclear.json')));
 initStore();
 
 function initStore() {
   store.defaults({lastFm: {}, settings: {}}).write();
   store.write();
 }
-
 
 module.exports = {
   store
