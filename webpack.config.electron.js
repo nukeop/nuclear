bbc412a75fa9e6f1d60d687572561bdf4835f431
nukeop <alsw@protonmail.com>
2018-04-20 23:58:53 +0200

    separate build process and main files for linux

--- webpack.config.electron.js
+++ webpack.config.electron.js
@@ -2,36 +2,40 @@ const webpack = require('webpack');
 const path = require('path');
 const HappyPack = require('happypack');
 
-module.exports = {
-  entry: './main.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.electron.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        use: 'happypack/loader?id=jsx',
-        exclude: /node_modules/
-      },
-      {
-        test: /.node$/,
-        use: 'node-loader'
-      }
-    ]
-  },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new HappyPack({
-      id: 'jsx',
-      loaders: [ 'babel-loader' ]
-    }),
-  ],
-  node: {
-    fs: "empty",
-    __dirname: false,
-    __filename: false
-  },
-  target: 'electron-main'
+module.exports = env => {
+  let entry = env && env.LINUX ? './server/main.dev.linux.js' : './server/main.dev.js';
+
+  return {
+    entry: entry,
+    output: {
+      path: __dirname,
+      filename: 'bundle.electron.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /.jsx?$/,
+          use: 'happypack/loader?id=jsx',
+          exclude: /node_modules/
+        },
+        {
+          test: /.node$/,
+          use: 'node-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+      new HappyPack({
+        id: 'jsx',
+        loaders: [ 'babel-loader' ]
+      }),
+    ],
+    node: {
+      fs: "empty",
+      __dirname: false,
+      __filename: false
+    },
+    target: 'electron-main'
+  }
 };
