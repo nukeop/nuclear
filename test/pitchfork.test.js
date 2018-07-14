5de1f78952fd0d238d00a37555c71125b119d4e5
nukeop <alsw@protonmail.com>
2018-07-14 13:52:17 +0200

    correct promise-based tests

--- test/pitchfork.test.js
+++ test/pitchfork.test.js
@@ -5,25 +5,46 @@ import {
 import { expect } from 'chai';
 
 describe('Pitchfork API tests', () => {
-  it('gets best new albums', () => {
-    getBestNewAlbums().
+  it('gets best new albums', async () => {
+    const result = await getBestNewAlbums().
       then(albums => {
-        
+        return albums;
       })
       .catch(error => {
         console.error(error);
         expect(false).to.equal(true);
       });
+    
+    expect(result).to.be.an('array');
+    result.forEach(entry => {
+      expect(entry).to.be.an('object').that.has.all.keys('thumbnail',
+                                                         'artist',
+                                                         'title',
+                                                         'reviewUrl',
+                                                         'genres',
+                                                         'score',
+                                                         'abstract',
+                                                         'review');
+    });
   });
 
-  it('gets best new tracks', () => {
-    getBestNewTracks().
+  it('gets best new tracks', async () => {
+    const result = await getBestNewTracks().
       then(tracks => {
-        
+        return tracks;
       })
       .catch(error => {
         console.error(error);
         expect(false).to.equal(true);
       });
+    
+    expect(result).to.be.an('array');
+    result.forEach(entry => {
+      expect(entry).to.be.an('object').that.has.all.keys('thumbnail',
+                                                         'artist',
+                                                         'title',
+                                                         'reviewUrl',
+                                                         'review');
+    });
   });
 });
