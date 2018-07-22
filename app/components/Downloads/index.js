4aa70e9d01f7d9981c4e3c7690cbb1d6616ee5cf
nukeop <alsw@protonmail.com>
2018-07-23 00:41:57 +0200

    Downloads screen placeholder

---
+++ app/components/Downloads/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.scss';
+
+const Downloads = props => {
+  return (
+    <div className={styles.downloads_container}>
+      <h3>
+        Downloads in Nuclear are coming soon.
+      </h3>
+    </div>
+  );
+}
+
+Downloads.propTypes = {
+
+};
+
+export default Downloads;
