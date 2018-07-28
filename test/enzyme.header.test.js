d7835ceb4b19e3c3e51de51e468805c86fca3b8b
nukeop <alsw@protonmail.com>
2018-07-28 23:25:45 +0200

    Enzyme test

---
+++ test/enzyme.header.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Header from '../app/components/Header';
+
+describe('<Header />', () => {
+  it('renders a header with no children', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.containsMatchingElement(
+      <div className='header_container' />
+    ));
+  });
+
+  it('renders a header with children', () => {
+    const wrapper = shallow(<Header>test</Header>);
+    expect(wrapper.containsMatchingElement(
+      <div className='header_container' />
+    ));
+  });
+});
