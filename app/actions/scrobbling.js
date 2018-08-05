ca04fada5c9a5189291521041679b633cdf12bd9
nukeop <alsw@protonmail.com>
2018-08-05 21:18:13 +0200

    Use nuclear-ui and nuclear-core

--- app/actions/scrobbling.js
+++ app/actions/scrobbling.js
@@ -1,8 +1,8 @@
 import { store } from '../persistence/store';
-import {LastFmApi} from 'nuclear-core';
+import core from 'nuclear-core';
 import globals from '../globals';
 const electron = window.require('electron');
-const lastfm = new LastFmApi(globals.lastfmApiKey, globals.lastfmApiSecret);
+const lastfm = new core.LastFmApi(globals.lastfmApiKey, globals.lastfmApiSecret);
 
 export const LASTFM_CONNECT = 'LASTFM_CONNECT';
 export const LASTFM_LOGIN = 'LASTFM_LOGIN';
