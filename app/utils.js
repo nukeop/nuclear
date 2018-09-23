ba21c22c1b996f458b4882a5046d0980c8967afd
nukeop <alsw@protonmail.com>
2018-09-24 00:02:13 +0200

    Fix time formatting for queue items

--- app/utils.js
+++ app/utils.js
@@ -6,11 +6,11 @@ export function formatDuration(duration) {
   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
   var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-  if (hours   < 10) {hours   = "0"+hours;}
-  if (minutes < 10) {minutes = "0"+minutes;}
-  if (seconds < 10) {seconds = "0"+seconds;}
+  if (hours   < 10) {hours   = '0'+hours;}
+  if (minutes < 10) {minutes = '0'+minutes;}
+  if (seconds < 10) {seconds = '0'+seconds;}
 
-  if (hours === 0) {
+  if (hours === '00') {
     return minutes+':'+seconds;
   } else {
     return hours+':'+minutes+':'+seconds;
