f660c7a33144bf576347be3fee410d1a31860696
Benjamin <benjamin@fidelisa.com>
2019-01-11 17:34:59 +0100

    Adding an auto radio feature
    When this feature enabled in the playback options, if we are reaching the latest track of the queue, the player adds a random track of a similar artist in the queue.

--- app/components/PlayOptionsControls/index.js
+++ app/components/PlayOptionsControls/index.js
@@ -11,34 +11,48 @@ import styles from './styles.scss';
 const PlayOptionsControls = props => {
   return (
     <div className="play_options_controls">
-      <div className={classnames("icon", {active: props.settings.loopAfterQueueEnd})}
-        onClick={() => props.toggleOption(
-          _.find(settingsConst, { name: 'loopAfterQueueEnd' }),
-          props.settings
+      <div
+        className={classnames('icon', {
+          active: props.settings.loopAfterQueueEnd,
+        })}
+        onClick={() =>
+          props.toggleOption(
+            _.find(settingsConst, { name: 'loopAfterQueueEnd' }),
+            props.settings
           )
         }
       >
         <FontAwesome name="repeat" />
       </div>
-      <div className={classnames("icon", {active: props.settings.shuffleQueue})}
-        onClick={() => props.toggleOption(
-          _.find(settingsConst, { name: 'shuffleQueue' }),
-          props.settings
+      <div
+        className={classnames('icon', { active: props.settings.shuffleQueue })}
+        onClick={() =>
+          props.toggleOption(
+            _.find(settingsConst, { name: 'shuffleQueue' }),
+            props.settings
           )
         }
       >
         <FontAwesome name="random" />
       </div>
+
+      <div
+        className={classnames('icon', { active: props.settings.autoradio })}
+        onClick={() =>
+          props.toggleOption(
+            _.find(settingsConst, { name: 'autoradio' }),
+            props.settings
+          )
+        }
+      >
+        <FontAwesome name="magic" />
+      </div>
     </div>
   );
-}
-
-PlayOptionsControls.propTypes = {
-
 };
 
-PlayOptionsControls.defaultProps = {
+PlayOptionsControls.propTypes = {};
 
-};
+PlayOptionsControls.defaultProps = {};
 
 export default PlayOptionsControls;
