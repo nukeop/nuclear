1d6932c3d23cd5d9ed26491f9ba92474043dc1ca
Benjamin <benjamin@fidelisa.com>
2019-01-12 19:21:49 +0100

    Added keys to map & arrays to remove react warnings

--- app/components/PlaylistView/index.js
+++ app/components/PlaylistView/index.js
@@ -11,7 +11,13 @@ class PlaylistView extends React.Component {
     super(props);
   }
 
-  addPlaylistToQueue(musicSources, playlist, addTracks, selectSong, startPlayback) {
+  addPlaylistToQueue(
+    musicSources,
+    playlist,
+    addTracks,
+    selectSong,
+    startPlayback
+  ) {
     addTracks(musicSources, playlist.tracks);
     selectSong(0);
     startPlayback();
@@ -20,70 +26,83 @@ class PlaylistView extends React.Component {
   renderOptions(trigger, playlist) {
     return (
       <ContextPopup
-	      trigger={trigger}
-	      artist={null}
-	      title={playlist.name}
-	      thumb={playlist.tracks[0].thumbnail}
-	    >
+        trigger={trigger}
+        artist={null}
+        title={playlist.name}
+        thumb={playlist.tracks[0].thumbnail}
+      >
         <div />
       </ContextPopup>
     );
   }
-  
+
   render() {
     let {
       playlist,
       addTracks,
       musicSources,
       selectSong,
-      startPlayback
+      startPlayback,
     } = this.props;
 
-    let popupTrigger = (<a href="#" className={styles.more_button}><FontAwesome name="ellipsis-h" /></a>); 
-    
+    let popupTrigger = (
+      <a href="#" className={styles.more_button}>
+        <FontAwesome name="ellipsis-h" />
+      </a>
+    );
+
     return (
       <div className={styles.playlist_view_container}>
         <div className={styles.playlist}>
-	        <div className={styles.playlist_info}>
-	          <div>
-	            <img 
-                className={styles.playlist_thumbnail} 
-                src={playlist.tracks[0].thumbnail} 
-		            />
-	          </div>
+          <div className={styles.playlist_info}>
+            <div>
+              <img
+                className={styles.playlist_thumbnail}
+                src={playlist.tracks[0].thumbnail}
+              />
+            </div>
             <div className={styles.playlist_header}>
-	            <div className={styles.playlist_name}>
-		            {playlist.name}
-	            </div>
-	            <Spacer />
+              <div className={styles.playlist_name}>{playlist.name}</div>
+              <Spacer />
               <div className={styles.playlist_buttons}>
-                <a href="#" className={styles.play_button} onClick={() => this.addPlaylistToQueue(musicSources, playlist, addTracks, selectSong, startPlayback)}><FontAwesome name="play" /> Play</a>
-		            {this.renderOptions(popupTrigger, playlist)}
-	            </div>
-	          </div>
-	          
+                <a
+                  href="#"
+                  className={styles.play_button}
+                  onClick={() =>
+                    this.addPlaylistToQueue(
+                      musicSources,
+                      playlist,
+                      addTracks,
+                      selectSong,
+                      startPlayback
+                    )
+                  }
+                >
+                  <FontAwesome name="play" /> Play
+                </a>
+                {this.renderOptions(popupTrigger, playlist)}
+              </div>
+            </div>
+          </div>
 
-	        </div>
-
-	        <div className={styles.playlist_tracks}>
-	          
-	          {
-	            playlist.tracks.map(track => {
-		            return (
-		              <div className={styles.playlist_track}>
-		                <img className={styles.track_thumbnail} src={track.thumbnail} />
-		                <div className={styles.track_info}>
-		                  <div className={styles.track_artist}>{track.artist}</div>
-		                  <div className={styles.track_name}>{track.name}</div>
-		                </div>
-		              </div>
-		            );
-	            })
-	          }
-      
+          <div className={styles.playlist_tracks}>
+            {playlist.tracks.map((track, i) => {
+              return (
+                <div key={i} className={styles.playlist_track}>
+                  <img
+                    className={styles.track_thumbnail}
+                    src={track.thumbnail}
+                  />
+                  <div className={styles.track_info}>
+                    <div className={styles.track_artist}>{track.artist}</div>
+                    <div className={styles.track_name}>{track.name}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-	      </div>
-	      </div>
     );
   }
 }
--- app/components/Playlists/index.js
+++ app/components/Playlists/index.js
@@ -6,28 +6,25 @@ import styles from './styles.scss';
 
 class Playlists extends React.Component {
   render() {
-  	let {
-      history,
-  		playlists
-  	} = this.props;
-    
+    let { history, playlists } = this.props;
+
     return (
       <div className={styles.playlists_container}>
-        {
-        (!playlists || Object.keys(playlists).length === 0 || playlists.length === 0) &&
-            <h3>No playlists.</h3>
-        }
-        {
-        	playlists && playlists.length > 0 && playlists.map((playlist, i) => {
-        		return (
-        			<Playlist
-        				playlist={playlist}
+        {(!playlists ||
+          Object.keys(playlists).length === 0 ||
+          playlists.length === 0) && <h3>No playlists.</h3>}
+        {playlists &&
+          playlists.length > 0 &&
+          playlists.map((playlist, i) => {
+            return (
+              <Playlist
+                playlist={playlist}
                 history={history}
                 index={i}
-        			/>
-        		);
-        	})
-        }
+                key={i}
+              />
+            );
+          })}
       </div>
     );
   }
