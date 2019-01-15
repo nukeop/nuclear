73c76cef5f5a44c88446eed7692783189acb8343
trekiteasy <dubenjspamster@gmail.com>
2019-01-15 15:45:41 +0100

    Added a key to BestNewListActiveItem to remove a react warning

--- app/components/Dashboard/BestNewMusicTab/BestNewList/BestNewListActiveItem/index.js
+++ app/components/Dashboard/BestNewMusicTab/BestNewList/BestNewListActiveItem/index.js
@@ -8,11 +8,7 @@ class BestNewListActiveItem extends React.Component {
   }
 
   render() {
-    let {
-      item,
-      artistInfoSearchByName,
-      history
-    } = this.props;
+    let { item, artistInfoSearchByName, history } = this.props;
 
     if (!item) {
       return null;
@@ -24,48 +20,47 @@ class BestNewListActiveItem extends React.Component {
           <div
             className={styles.item_thumbnail}
             style={{
-              backgroundImage: `url(${item.thumbnail})`
-            }}/>
-          </div>
-          <div className={styles.review_box}>
-            <div className={styles.header_row}>
-              {
-                item.score
-                ? <div className={styles.score}>
-                  {item.score}
-                </div>
-                : null
-              }
-
-              <div className={styles.artist_title_box}>
-                <div className={styles.title}>
-                  {item.title}
-                </div>
-                <div className={styles.artist}>
-                  by <a onClick={() => artistInfoSearchByName(item.artist, history)} href='#' >{item.artist}</a>
-                </div>
-              </div>
-
-
-            </div>
-            <div className={styles.review}>
-              {
-                item.abstract
-                ? <div className={styles.abstract}>{item.abstract}</div>
-                : null
-              }
-              <div className={styles.review_content}>
-                {item.review.split("\n").map(i => {
-                  return <div className={styles.paragraph}>{i}</div>;
-                })}
-              </div>
-
-            </div>
-          </div>
-
+              backgroundImage: `url(${item.thumbnail})`,
+            }}
+          />
         </div>
-      );
-    }
-  }
+        <div className={styles.review_box}>
+          <div className={styles.header_row}>
+            {item.score ? (
+              <div className={styles.score}>{item.score}</div>
+            ) : null}
 
-  export default BestNewListActiveItem;
+            <div className={styles.artist_title_box}>
+              <div className={styles.title}>{item.title}</div>
+              <div className={styles.artist}>
+                by{' '}
+                <a
+                  onClick={() => artistInfoSearchByName(item.artist, history)}
+                  href="#"
+                >
+                  {item.artist}
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles.review}>
+            {item.abstract ? (
+              <div className={styles.abstract}>{item.abstract}</div>
+            ) : null}
+            <div className={styles.review_content}>
+              {item.review.split('\n').map(i => {
+                return (
+                  <div key={'item-' + i} className={styles.paragraph}>
+                    {i}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BestNewListActiveItem;
