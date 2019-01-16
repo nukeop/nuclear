cbe841bc5aa614c72b20de037dc9c4e1f14125fd
trekiteasy <dubenjspamster@gmail.com>
2019-01-16 17:38:56 +0100

    Added the ability to add lastfm top tracks to queue

--- app/containers/DashboardContainer/index.js
+++ app/containers/DashboardContainer/index.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../../actions';
 import * as DashboardActions from '../../actions/dashboard';
+import * as QueueActions from '../../actions/queue';
 
 import Dashboard from '../../components/Dashboard';
 
 class DashboardContainer extends React.Component {
   render() {
-    let { actions, dashboard, history } = this.props;
+    let { actions, dashboard, history, musicSources } = this.props;
+
     return (
       <Dashboard
         albumInfoSearch={actions.albumInfoSearch}
@@ -20,6 +22,8 @@ class DashboardContainer extends React.Component {
         loadTopTracks={actions.loadTopTracks}
         dashboardData={dashboard}
         history={history}
+        addToQueue={actions.addToQueue}
+        musicSources={this.props.musicSources}
       />
     );
   }
@@ -28,13 +32,14 @@ class DashboardContainer extends React.Component {
 function mapStateToProps(state) {
   return {
     dashboard: state.dashboard,
+    musicSources: state.plugin.plugins.musicSources,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(
-      Object.assign({}, Actions, DashboardActions),
+      Object.assign({}, Actions, DashboardActions, QueueActions),
       dispatch
     ),
   };
