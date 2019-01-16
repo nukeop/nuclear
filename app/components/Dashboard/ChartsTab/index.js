4c0d98490fbd14dd5f86d15c175966959d3a7911
trekiteasy <dubenjspamster@gmail.com>
2019-01-16 09:55:29 +0100

    Added Top Tracks from lastfm in dashboard
    Todo : better presentation + clickable tracks / artists

--- app/components/Dashboard/ChartsTab/index.js
+++ app/components/Dashboard/ChartsTab/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import {Tab} from 'semantic-ui-react';
-import PropTypes from 'prop-types';
+import { Tab } from 'semantic-ui-react';
+import artPlaceholder from '../../../../resources/media/art_placeholder.png';
+import numeral from 'numeral';
 
 import styles from './styles.scss';
 
-const ChartsTab = props => {
-  return (
-    <Tab.Pane attached={false}>
-      <div className={styles.charts_container}>
-        <h3>Charts in Nuclear are coming soon.</h3>
-      </div>
-    </Tab.Pane>
-  );
-};
+class ChartsTab extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-ChartsTab.propTypes = {
-
-};
+  render() {
+    console.log(this.props);
+    return (
+      <Tab.Pane attached={false}>
+        <div className={styles.charts_container}>
+          <h3>Top Tracks from LastFm.</h3>
+          {this.props.topTracks.map((track, i) => {
+            return (
+              <div key={'toptrack-' + i} className={styles.track_row}>
+                <img src={track.image[0]['#text'] || artPlaceholder} />
+                <div className={styles.popular_track_name}>{track.name}</div>
+                <div className={styles.popular_track_artist}>
+                  {track.artist.name}
+                </div>
+                <div className={styles.playcount}>
+                  {numeral(track.playcount).format('0,0')} plays
+                </div>
+              </div>
+            );
+          })}
+          {console.log(this.props.topTracks)}
+        </div>
+      </Tab.Pane>
+    );
+  }
+}
 
 export default ChartsTab;
--- app/containers/DashboardContainer/index.js
+++ app/containers/DashboardContainer/index.js
@@ -8,11 +8,7 @@ import Dashboard from '../../components/Dashboard';
 
 class DashboardContainer extends React.Component {
   render() {
-    let {
-      actions,
-      dashboard,
-      history
-    } = this.props;
+    let { actions, dashboard, history } = this.props;
     return (
       <Dashboard
         albumInfoSearch={actions.albumInfoSearch}
@@ -21,6 +17,7 @@ class DashboardContainer extends React.Component {
         loadBestNewTracks={actions.loadBestNewTracks}
         loadNuclearNews={actions.loadNuclearNews}
         loadTopTags={actions.loadTopTags}
+        loadTopTracks={actions.loadTopTracks}
         dashboardData={dashboard}
         history={history}
       />
@@ -30,14 +27,20 @@ class DashboardContainer extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    dashboard: state.dashboard
-  }
+    dashboard: state.dashboard,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(Object.assign({}, Actions, DashboardActions), dispatch)
+    actions: bindActionCreators(
+      Object.assign({}, Actions, DashboardActions),
+      dispatch
+    ),
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardContainer);
