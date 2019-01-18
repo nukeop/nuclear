d07d2174eb69a7ffa562eba557847973d41367ea
trekiteasy <dubenjspamster@gmail.com>
2019-01-18 19:38:20 +0100

    Factorized PlayOptionsControls renders

--- app/components/PlayOptionsControls/index.js
+++ app/components/PlayOptionsControls/index.js
@@ -1,58 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import FontAwesome from 'react-fontawesome';
 import _ from 'lodash';
 
 import settingsConst from '../../constants/settings';
 
-import styles from './styles.scss';
+function renderOptionControl(props, settingName, fontAwesomeName) {
+  return (
+    <div
+      className={classnames('icon', {
+        active: props.settings[settingName],
+      })}
+      onClick={() =>
+        props.toggleOption(
+          _.find(settingsConst, { name: settingName }),
+          props.settings
+        )
+      }
+    >
+      <FontAwesome name={fontAwesomeName} />
+    </div>
+  );
+}
 
 const PlayOptionsControls = props => {
   return (
     <div className="play_options_controls">
-      <div
-        className={classnames('icon', {
-          active: props.settings.loopAfterQueueEnd,
-        })}
-        onClick={() =>
-          props.toggleOption(
-            _.find(settingsConst, { name: 'loopAfterQueueEnd' }),
-            props.settings
-          )
-        }
-      >
-        <FontAwesome name="repeat" />
-      </div>
-      <div
-        className={classnames('icon', { active: props.settings.shuffleQueue })}
-        onClick={() =>
-          props.toggleOption(
-            _.find(settingsConst, { name: 'shuffleQueue' }),
-            props.settings
-          )
-        }
-      >
-        <FontAwesome name="random" />
-      </div>
-
-      <div
-        className={classnames('icon', { active: props.settings.autoradio })}
-        onClick={() =>
-          props.toggleOption(
-            _.find(settingsConst, { name: 'autoradio' }),
-            props.settings
-          )
-        }
-      >
-        <FontAwesome name="magic" />
-      </div>
+      {renderOptionControl(props, 'loopAfterQueueEnd', 'repeat')}
+      {renderOptionControl(props, 'shuffleQueue', 'random')}
+      {renderOptionControl(props, 'autoradio', 'magic')}
     </div>
   );
 };
 
 PlayOptionsControls.propTypes = {};
-
 PlayOptionsControls.defaultProps = {};
 
 export default PlayOptionsControls;
