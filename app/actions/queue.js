f7b14a41f16e48d9e06bb77bd0e403b0ca4b8ef3
trekiteasy <dubenjspamster@gmail.com>
2019-01-19 09:27:39 +0100

    Merged revert refactoring

--- app/actions/queue.js
+++ app/actions/queue.js
@@ -11,54 +11,38 @@ export const PREVIOUS_SONG = 'PREVIOUS_SONG';
 export const SELECT_SONG = 'SELECT_SONG';
 export const SWAP_SONGS = 'SWAP_SONGS';
 
-export function addToQueue(musicSources, item) {
+export function addToQueue (musicSources, item) {
   return dispatch => {
     item.loading = true;
     item.uuid = uuidv4();
     dispatch({
       type: ADD_TO_QUEUE,
-<<<<<<< HEAD
-      payload: track
-    });
 
-    Promise.all(
-      _.map(musicSources, m => m.search(track.artist + ' ' + track.name))
-    )
-      .then(results => Promise.all(results))
-      .then(results => {
-        let item = track;
-        let payloadItem = { streams: results };
-        if (!loading) {
-          payloadItem = { streams: results, loading: false };
-        }
-        dispatch({
-          type: ADD_STREAMS_TO_QUEUE_ITEM,
-          payload: Object.assign({}, track, payloadItem)
-        });
-=======
       payload: item
     });
 
-    Promise.all(_.map(musicSources, m => m.search(item.artist + ' ' + item.name)))
-    .then(results => Promise.all(results))
-    .then(results => {
-      dispatch({
-        type: ADD_STREAMS_TO_QUEUE_ITEM,
-        payload: Object.assign({}, item, {loading: false, streams: results})
->>>>>>> parent of ef3e635... Factorized addToQueue
+    Promise.all(
+      _.map(musicSources, m => m.search(item.artist + ' ' + item.name))
+    )
+      .then(results => Promise.all(results))
+      .then(results => {
+        dispatch({
+          type: ADD_STREAMS_TO_QUEUE_ITEM,
+          payload: Object.assign({}, item, { streams: results, loading: false })
+        });
       });
-    });
+
   };
 }
 
-export function removeFromQueue(item) {
+export function removeFromQueue (item) {
   return {
     type: REMOVE_FROM_QUEUE,
     payload: item
   };
 }
 
-export function addPlaylistTracksToQueue(musicSources, tracks) {
+export function addPlaylistTracksToQueue (musicSources, tracks) {
   return (dispatch) => {
     tracks.map((track, i) => {
       dispatch({
@@ -68,71 +52,63 @@ export function addPlaylistTracksToQueue(musicSources, tracks) {
 
 
       Promise.all(_.map(musicSources, m => m.search(track.artist + ' ' + track.name)))
-      .then(results => Promise.all(results))
-      .then(results => {
-        let item = track;
-        dispatch({
-          type: ADD_STREAMS_TO_QUEUE_ITEM,
-          payload: Object.assign({}, item, {streams: results})
+        .then(results => Promise.all(results))
+        .then(results => {
+
+          dispatch({
+            type: ADD_STREAMS_TO_QUEUE_ITEM,
+            payload: Object.assign({}, track, { streams: results })
+          });
         });
-      });
     });
 
   };
 }
 
-export function rerollTrack(musicSource, selectedStream, track) {
+export function rerollTrack (musicSource, selectedStream, track) {
   return dispatch => {
     musicSource.getAlternateStream(track.artist + ' ' + track.name, selectedStream)
-    .then(newStream => {
-      let streams = _.map(track.streams, stream => {
-        return stream.source === newStream.source ? newStream : stream;
-      });
-
-<<<<<<< HEAD
+      .then(newStream => {
+        let streams = _.map(track.streams, stream => {
+          return stream.source === newStream.source ? newStream : stream;
+        });
         dispatch({
           type: REPLACE_STREAMS_IN_QUEUE_ITEM,
           payload: Object.assign({}, track, { streams })
         });
-=======
-      dispatch({
-        type: REPLACE_STREAMS_IN_QUEUE_ITEM,
-        payload: Object.assign({}, track, {streams})
->>>>>>> parent of ef3e635... Factorized addToQueue
       });
-    });
   };
 }
 
-export function clearQueue() {
+export function clearQueue () {
   return {
     type: CLEAR_QUEUE,
     payload: null
   };
 }
 
-export function nextSong() {
+export function nextSong () {
   return {
     type: NEXT_SONG,
     payload: null
   };
 }
 
-export function previousSong() {
+export function previousSong () {
   return {
     type: PREVIOUS_SONG,
     payload: null
   };
 }
 
-export function selectSong(index) {
+export function selectSong (index) {
   return {
     type: SELECT_SONG,
     payload: index
   };
 }
 
-export function swapSongs(itemFrom, itemTo) {
+export function swapSongs (itemFrom, itemTo) {
   return {
     type: SWAP_SONGS,
     payload: {
