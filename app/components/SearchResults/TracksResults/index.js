abc3dae153391715fafbef3095974094dc430f6a
trekiteasy <dubenjspamster@gmail.com>
2019-01-23 09:00:45 +0100

    Added a youtube playlist tracks identifier
    When a youtube playlist is entered in the search box, the different songs appear in the search results.
    Known limitations :
    - we should also be able to enter a song url (and not only playlist url) - not implemented
    - for now, only the first 100 songs of the playlist are displayed
    - some functionalities like "add all" or play all would make sense
    
    A first step towards uniformization of components was taken but isn't deployed everywhere yet (only in search results for now).

---
+++ app/components/SearchResults/TracksResults/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import artPlaceholder from '../../../../resources/media/art_placeholder.png';
+
+import styles from './styles.scss';
+
+class TracksResults extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderTrack (track, index = 0) {
+        let addToQueue = this.props.addToQueue;
+        return (
+            <div key={'track-' + index} className={styles.track_row} onClick={() => {
+                addToQueue(this.props.musicSources, {
+                    artist: track.name,
+                    name: track.name,
+                    thumbnail: track.image[1]['#text']
+                });
+            }}>
+                <img src={track.image[0]['#text'] || artPlaceholder} />
+                <div className={styles.popular_track_name}>{track.name}</div>
+
+            </div>
+        );
+    }
+
+    render () {
+
+        let collection = this.props.tracks || []
+        let limit = this.props.limit
+        if (collection.length == 0) {
+            return "No result"
+        } else {
+            return (
+                <div className={styles.popular_tracks_container}>
+                    {
+                        (collection || []).slice(0, limit).map((track, i) => {
+                            if (track) {
+                                if (track.name && track.image && track.artist) {
+                                    { return this.renderTrack(track, i) }
+                                }
+                            }
+                        })
+                    }
+                </div>
+            );
+        }
+    }
+}
+
+export default TracksResults;
