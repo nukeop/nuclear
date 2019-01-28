7eaa7c88935fa8358132a36b841c5718acdbd8d7
trekiteasy <dubenjspamster@gmail.com>
2019-01-28 17:56:20 +0100

    Code simplification as per @FabianLauer 's comment on commit bb3ef3fabc874f9b71f4c11eca960efc89492ca9

--- app/plugins/MusicSources/JamendoPlugin.js
+++ app/plugins/MusicSources/JamendoPlugin.js
@@ -12,26 +12,26 @@ class JamendoPlugin extends MusicSourcePlugin {
     this.description = 'Allows Nuclear to find music streams on Jamendo';
   }
 
-  search(query) {
+  search (query) {
     return this.getSearchResults(query).then(responseJson => {
-      if (responseJson.results.length > 0) {
-        let track = responseJson.results[0].tracks[0];
-
-        return {
-          source: this.sourceName,
-          id: track.id,
-          stream: track.audio,
-          duration: track.duration,
-          title: track.name,
-          thumbnail: track.image
-        };
-      } else {
+      if (responseJson.results.length === 0) {
         return null;
       }
+
+      let track = responseJson.results[0].tracks[0];
+
+      return {
+        source: this.sourceName,
+        id: track.id,
+        stream: track.audio,
+        duration: track.duration,
+        title: track.name,
+        thumbnail: track.image
+      };
     });
   }
 
-  getSearchResults(query) {
+  getSearchResults (query) {
     return Jamendo.search(query)
       .then(response => response.json())
       .catch(err => {
@@ -40,7 +40,7 @@ class JamendoPlugin extends MusicSourcePlugin {
       });
   }
 
-  getAlternateStream(query, currentStream) {
+  getAlternateStream (query, currentStream) {
     return this.getSearchResults(query).then(results => {
       let info = _.find(results, result => result && result.id !== currentStream.id);
       return info ? this.resultToStream(info) : null;
