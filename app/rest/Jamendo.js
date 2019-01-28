52b82c427b623ae385063ed042744886742f7919
trekiteasy <dubenjspamster@gmail.com>
2019-01-28 17:56:20 +0100

    Code simplification as per @FabianLauer 's comment on commit bb3ef3fabc874f9b71f4c11eca960efc89492ca9

--- app/rest/Jamendo.js
+++ app/rest/Jamendo.js
@@ -1,18 +1,18 @@
 import globals from '../globals';
 
-export function search(query) {
-  let limit = 10;
-  let url = encodeURI(
+export function search (query) {
+  const limit = 10;
+  const url = encodeURI(
     'https://api.jamendo.com/v3.0/artists/tracks/' +
-      '?client_id=' +
-      globals.jamendoClientId +
-      '&format=jsonpretty' +
-      '&limit=' +
-      limit +
-      '&name=' +
-      query.artist +
-      '&track_name=' +
-      query.track
+    '?client_id=' +
+    globals.jamendoClientId +
+    '&format=jsonpretty' +
+    '&limit=' +
+    limit +
+    '&name=' +
+    query.artist +
+    '&track_name=' +
+    query.track
   );
 
   return fetch(url);
