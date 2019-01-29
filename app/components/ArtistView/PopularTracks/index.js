ddf3194adb8bce2feac3b6d2167135dd9a4d4603
trekiteasy <dubenjspamster@gmail.com>
2019-01-29 10:09:58 +0100

    Cleaned up PopularTracks from unused functions

--- app/components/ArtistView/PopularTracks/index.js
+++ app/components/ArtistView/PopularTracks/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
-import numeral from 'numeral';
-import artPlaceholder from '../../../../resources/media/art_placeholder.png';
-import ContextPopup from '../../ContextPopup';
 import TrackRow from '../../TrackRow';
+import artPlaceholder from '../../../../resources/media/art_placeholder.png';
 
 import styles from './styles.scss';
 
@@ -22,67 +20,26 @@ class PopularTracks extends React.Component {
     });
   }
 
-  addToQueue(artist, track) {
-    this.props.addToQueue(this.props.musicSources, {
-      artist: artist.name,
-      name: track.name,
-      thumbnail: track.image[0]['#text'] || artPlaceholder
-    });
-  }
-
-  renderPopup(key, artist, track, contents) {
-    return (
-      <ContextPopup
-        key={key}
-        trigger={
-          <div className={styles.track_row}>
-            <img src={track.image[0]['#text'] || artPlaceholder} />
-            <div className={styles.popular_track_name}>{track.name}</div>
-            <div className={styles.playcount}>
-              {numeral(track.playcount).format('0,0')} plays
-            </div>
-          </div>
-        }
-        artist={artist.name}
-        title={track.name}
-        thumb={track.image[0]['#text'] || artPlaceholder}
-      >
-        {contents}
-      </ContextPopup>
-    );
-  }
-
-  renderAddTrackToQueueButton(track, index, artist) {
+  renderAddAllButton(artist, tracks) {
     return (
       <a
-        key={'add-track-' + index}
-        href='#'
-        onClick={() => this.addToQueue(artist, track)}
-        className={styles.add_button}
-        aria-label='Add track to queue'
-      >
-        <FontAwesome name='plus' /> Add to queue
-      </a>
-    );
-  }
-
-  renderPlayTrackButton(track, index) {
-    let { artist, selectSong, startPlayback, clearQueue } = this.props;
-
-    return (
-      <a
-        key={'play-track-' + index}
+        key='add-all-tracks-to-queue'
         href='#'
         onClick={() => {
-          clearQueue();
-          this.addToQueue(artist, track);
-          selectSong(0);
-          startPlayback();
+          tracks.track
+            .slice(0, this.state.expanded ? 15 : 5)
+            .map((track, index) => {
+              this.props.addToQueue(this.props.musicSources, {
+                artist: artist.name,
+                name: track.name,
+                thumbnail: track.image[0]['#text'] || artPlaceholder
+              });
+            });
         }}
         className={styles.add_button}
-        aria-label='Play this track now'
+        aria-label='Add all tracks to queue'
       >
-        <FontAwesome name='play' /> Play now
+        <FontAwesome name='plus' /> Add all
       </a>
     );
   }
@@ -93,21 +50,7 @@ class PopularTracks extends React.Component {
     return (
       <div className={styles.popular_tracks_container}>
         <div className={styles.header}>Popular tracks:</div>
-        <a
-          key='add-all-tracks-to-queue'
-          href='#'
-          onClick={() => {
-            tracks.track
-              .slice(0, this.state.expanded ? 15 : 5)
-              .map((track, index) => {
-                this.addToQueue(artist, track);
-              });
-          }}
-          className={styles.add_button}
-          aria-label='Add all tracks to queue'
-        >
-          <FontAwesome name='plus' /> Add all
-        </a>
+        {this.renderAddAllButton(artist, tracks)}
         {tracks.track
           .slice(0, this.state.expanded ? 15 : 5)
           .map((track, index) => {
@@ -124,13 +67,6 @@ class PopularTracks extends React.Component {
                 musicSources={this.props.musicSources}
               />
             );
-            /* 
-            let popupContents = [
-              this.renderAddTrackToQueueButton(track, index, artist),
-              this.renderPlayTrackButton(track, index)
-            ];
-            return this.renderPopup(index, artist, track, popupContents);
-            */
           })}
         <div className='expand_button' onClick={this.toggleExpand.bind(this)}>
           <FontAwesome
