aa23ace475131f1b91a29185f0c5e486d053515b
trekiteasy <dubenjspamster@gmail.com>
2019-01-30 09:53:07 +0100

    Adding covers to top charts

--- app/components/Dashboard/ChartsTab/index.js
+++ app/components/Dashboard/ChartsTab/index.js
@@ -38,6 +38,7 @@ class ChartsTab extends React.Component {
                   startPlayback={this.props.startPlayback}
                   selectSong={this.props.selectSong}
                   musicSources={this.props.musicSources}
+                  displayCover={true}
                   displayArtist={true}
                   displayDuration={false}
                   displayPlayCount={true}
