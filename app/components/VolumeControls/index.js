d660af18eb09737c1dd52792531544f3ad321e54
Chandan Rauniyar <chandankkrr@gmail.com>
2019-01-31 22:53:44 +1100

    Added ability to mute volume when clicking on volume icon Volume icon changes when clicking on it

--- app/components/VolumeControls/index.js
+++ app/components/VolumeControls/index.js
@@ -8,10 +8,42 @@ import VolumeSlider from './VolumeSlider';
 
 class VolumeControls extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVolumeMuted: false,
+      volume: 100
+    };
+  }
+
   handleClick(value) {
+    this.setState({
+      volume: value,
+      isVolumeMuted: this.state.isVolumeMuted && value === 0
+    });
     return this.props.updateVolume(value);
   }
-  
+
+  toggleMute = () => {
+    if (this.state.isVolumeMuted) {
+      this.handleMuteOff();
+    } else {
+      this.handleMuteOn();
+    }
+
+    this.setState({
+      isVolumeMuted: !this.state.isVolumeMuted
+    });
+  }
+
+  handleMuteOn() {
+    this.props.updateVolume(0);
+  }
+
+  handleMuteOff() {
+    this.handleClick(this.state.volume);
+  }
+
   render() {
     return (
       <div className={styles.volume_controls_container}>
@@ -19,11 +51,15 @@ class VolumeControls extends React.Component {
           toggleOption={this.props.toggleOption}
           settings={this.props.settings}
         />
-        <FontAwesome name='volume-up'/>
+        <div className={styles.volume_speaker_control}
+          onClick={this.toggleMute}>
+          <FontAwesome name={this.state.isVolumeMuted ? 'volume-off' : 'volume-up'} />
+        </div>
+
         <VolumeSlider
-          fill={this.props.fill}
+          fill={this.state.volume}
           handleClick={this.handleClick.bind(this)}
-	      />
+        />
       </div>
     );
   }
