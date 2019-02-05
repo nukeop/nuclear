cfb96ad87165cdef66871cae6e84c00fe0112921
trekiteasy <dubenjspamster@gmail.com>
2019-02-05 09:19:29 +0100

    Code quality VolumeSlider

--- app/components/VolumeControls/VolumeSlider/index.js
+++ app/components/VolumeControls/VolumeSlider/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import FontAwesome from 'react-fontawesome';
 
 import styles from './styles.scss';
 import Range from 'react-range-progress';
 
+const volumeSliderColors = {
+  fillColor: { r: 248, g: 248, b: 242, a: 1 },
+  trackColor: { r: 68, g: 71, b: 90, a: 1 },
+  thumbColor: { r: 248, g: 248, b: 242, a: 1 }
+};
+
 class VolumeSlider extends React.Component {
 
-  handleClick(value) {
+  handleClick (value) {
     this.props.handleClick(value);
   }
 
-  render() {
+  render () {
     return (
       <div className={styles.volume_slider_container}>
         <Range
           value={this.props.fill}
-          fillColor={volume_slider_colors.fillColor}
-          trackColor={volume_slider_colors.trackColor}
-          thumbColor={volume_slider_colors.thumbColor}
+          fillColor={volumeSliderColors.fillColor}
+          trackColor={volumeSliderColors.trackColor}
+          thumbColor={volumeSliderColors.thumbColor}
           height={4}
           width='100%'
           onChange={this.handleClick.bind(this)}
@@ -26,11 +31,5 @@ class VolumeSlider extends React.Component {
     );
   }
 }
-
-const volume_slider_colors = {
-  fillColor: { r: 248, g: 248, b: 242, a: 1 },
-  trackColor: { r: 68, g: 71, b: 90, a: 1 },
-  thumbColor: { r: 248, g: 248, b: 242, a: 1 }
-};
 
 export default VolumeSlider;
