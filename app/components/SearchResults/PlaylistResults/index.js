50567d8a401b924c1a2c79af1a5f74ab0c7861c0
trekiteasy <dubenjspamster@gmail.com>
2019-02-06 15:19:19 +0100

    Added an add all button to playlist search result

--- app/components/SearchResults/PlaylistResults/index.js
+++ app/components/SearchResults/PlaylistResults/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import TracksResults from '../TracksResults';
+import FontAwesome from 'react-fontawesome';
+import artPlaceholder from '../../../../resources/media/art_placeholder.png';
+import _ from 'lodash';
 
 import styles from './styles.scss';
 
@@ -8,14 +11,55 @@ class PlaylistResults extends React.Component {
   constructor(props) {
     super(props);
   }
-  render () {
-    return (
-      this.props.playlistSearchStarted ? ((this.props.playlistSearchStarted.length > 0 && typeof this.props.playlistSearchResults.info === 'undefined') ? <div>Loading...</div> : (<TracksResults
+
+  addTrack (track) {
+    if (typeof track !== 'undefined') {
+      this.props.addToQueue(this.props.musicSources, {
+        artist: track.artist,
+        name: track.name,
+        thumbnail: _.get(track, 'image[1][\'#text\']', artPlaceholder)
+      });
+    }
+  }
+  renderAddAllButton (tracks) {
+    return (<a
+      key='add-all-tracks-to-queue'
+      href='#'
+      onClick={() => {
+        tracks
+          .map(track => {
+            this.addTrack(track);
+          });
+      }}
+      className={styles.add_button}
+      aria-label='Add all tracks to queue'
+    >
+      <FontAwesome name='plus' /> Add all
+    </a>
+    );
+  }
+
+  renderLoading () {
+    return (<div>Loading... <FontAwesome name='spinner' pulse /></div>);
+  }
+
+  renderResults () {
+    return (<div>
+      {this.renderAddAllButton(this.props.playlistSearchResults.info)}
+      <TracksResults
         addToQueue={this.props.addToQueue}
-              tracks={this.props.playlistSearchResults.info}
+        tracks={this.props.playlistSearchResults.info}
         limit='100'
         musicSources={this.props.musicSources}
-       />)) : <div>No result</div>
+      /></div>);
+  }
+
+  renderNoResult () {
+    return (<div>No result</div>);
+  }
+  render () {
+    return (
+      this.props.playlistSearchStarted ? ((this.props.playlistSearchStarted.length > 0 && typeof this.props.playlistSearchResults.info === 'undefined') ? this.renderLoading() : this.renderResults()) : this.renderNoResult()
     );
   }
 }
