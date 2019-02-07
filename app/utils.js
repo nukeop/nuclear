d37b5a5fdfd74a06160c167283cb90d2f8d24d74
nukeop <12746779+nukeop@users.noreply.github.com>
2019-02-07 01:13:37 +0100

    Merge pull request #241 from Brn9hrd7/master
    Removed a few duplicated code lines

--- app/utils.js
+++ app/utils.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 
 export function formatDuration (duration) {
-  let sec_num = parseInt(duration, 10);
-  let hours = Math.floor(sec_num / 3600);
-  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  let seconds = sec_num - (hours * 3600) - (minutes * 60);
+  let secNum = parseInt(duration, 10);
+  let hours = Math.floor(secNum / 3600);
+  let minutes = Math.floor((secNum - (hours * 3600)) / 60);
+  let seconds = secNum - (hours * 3600) - (minutes * 60);
 
   if (hours < 10) {
     hours = '0' + hours;
@@ -37,7 +37,7 @@ export function stringDurationToSeconds (duration) {
 export function getSelectedStream (streams, defaultMusicSource) {
   let selectedStream = _.find(streams, { source: defaultMusicSource });
 
-  return selectedStream === undefined
+  return typeof selectedStream === 'undefined'
     ? streams ? streams[0] : null
     : selectedStream;
 }
