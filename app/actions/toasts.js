0dccd5da4849fca646170e681094fabdc933a530
nukeop <nukeop@gumblert.tech>
2019-02-10 03:01:29 +0100

    Add all notification types, send a success toast when creating a playlist

--- app/actions/toasts.js
+++ app/actions/toasts.js
@@ -18,36 +18,35 @@ export function removeNotification(id) {
 }
 
 export function notify(title, details, icon, timeout=3) {
-  return dispatch => {
-    let id = uuidv4();
-    dispatch(addNotification({
-      onClick: () => {
-        dispatch(removeNotification(id));
-      },
-      id,
-      title,
-      details,
-      icon
-    }));
-
-    setTimeout(() => dispatch(removeNotification(id)), timeout * 1000);
-  };
-
+  return generateNotification(title, details, icon, {}, timeout);
 }
 
 export function error(title, details, icon, timeout=3) {
+  return generateNotification(title, details, icon, {error: true}, timeout);
+}
+
+export function warning(title, details, icon, timeout=3) {
+  return generateNotification(title, details, icon, {warning: true}, timeout);
+}
+
+export function success(title, details, icon, timeout=3) {
+  return generateNotification(title, details, icon, {success: true}, timeout);
+}
+
+export function info(title, details, icon, timeout=3) {
+  return generateNotification(title, details, icon, {info: true}, timeout);
+}
+
+function generateNotification(title, details, icon, type, timeout=3) {
   return dispatch => {
     let id = uuidv4();
-    dispatch(addNotification({
+    dispatch(addNotification(Object.assign({}, {
       onClick: () => {
         dispatch(removeNotification(id));
       },
-      error: true,
-      id,
-      title,
-      details,
-      icon
-    }));
+      id, title, details, icon
+    },
+    type)));
 
     setTimeout(() => dispatch(removeNotification(id)), timeout * 1000);
   };
