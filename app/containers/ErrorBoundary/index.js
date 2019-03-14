9c42411283822d202e6654d37f0f7e4ee488d6bd
Charles Jacquin <charles.jacquin@autistici.org>
2019-03-15 00:06:41 +0100

    feat(error): add a global ErrorBoundary component

---
+++ app/containers/ErrorBoundary/index.js
@@ -0,0 +1,54 @@
+import logger from 'electron-timber';
+import React from 'react';
+import { withRouter } from 'react-router';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as ToastActions from '../../actions/toasts';
+
+const initialState = { hasError: false };
+
+class ErrorBoundary extends React.Component {
+  state = initialState;
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    logger.error(error);
+    this.props.actions.error('error', 'Something wrong happen');
+
+    this.setState({ hasError: true }, () => {
+      this.props.history.goBack();
+      this.setState(initialState);
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return null;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  actions: PropTypes.shape({
+    error: PropTypes.func
+  })
+};
+
+function mapStateToProps() {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(ToastActions, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ErrorBoundary));
