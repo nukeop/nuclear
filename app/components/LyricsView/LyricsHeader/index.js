5c7ca5512fc0f7f07733564ed18318d76328e4e8
nukeop <alsw@protonmail.com>
2019-03-18 23:48:32 +0100

    Improved header for lyrics view

---
+++ app/components/LyricsView/LyricsHeader/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.scss';
+
+const LyricsHeader = props => {
+  return (
+    <div className={styles.lyrics_header}>
+      <div className={styles.lyrics_header_name}>{ props.name }</div>
+      <div className={styles.lyrics_header_artist}>by { props.artist
+        }</div>
+      <hr/>
+    </div>
+  );
+}
+
+LyricsHeader.propTypes = {
+  name: PropTypes.string,
+  artist: PropTypes.string
+};
+
+LyricsHeader.defaultProps = {
+  name: '',
+  artist: ''
+};
+
+export default LyricsHeader;
