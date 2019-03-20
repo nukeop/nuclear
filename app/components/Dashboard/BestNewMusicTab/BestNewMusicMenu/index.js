1e7525f48fa7671a6a259175097c674edf4826d8
nukeop <alsw@protonmail.com>
2019-03-20 01:14:28 +0100

    Redesigned best new music; review box coming tomorrow

---
+++ app/components/Dashboard/BestNewMusicTab/BestNewMusicMenu/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Menu } from 'semantic-ui-react';
+
+import BestNewMusicCard from './BestNewMusicCard';
+
+import styles from './styles.scss';
+
+const BestNewMusicMenu = props => {
+  return (
+    <div className={styles.best_new_music_menu}>
+      <Menu vertical size='large'>
+        <Menu.Item
+          className={styles.best_new_music_menu_header}
+          link
+          >
+          Best new albums
+        </Menu.Item>
+        {
+          props.albums.map(album => {
+            return (
+              <Menu.Item link>
+                <BestNewMusicCard
+                  item={ album }
+                  onClick={ () => props.setActiveItem(album )}
+                />
+              </Menu.Item>
+            );
+          })
+        }
+
+      <Menu.Item
+    className={styles.best_new_music_menu_header}
+    link
+      >
+      Best new tracks
+    </Menu.Item>
+      {
+        props.tracks.map(track => {
+          return (
+            <Menu.Item link>
+              <BestNewMusicCard
+                item={ track }
+                onClick={ () => props.setActiveItem(track)}
+              />
+            </Menu.Item>
+          );
+        })
+      }
+    </Menu>
+      </div>
+  );
+};
+
+export const bestNewItemShape = PropTypes.shape({
+  title: PropTypes.string,
+  artist: PropTypes.string,
+  thumbnail: PropTypes.string,
+  score: PropTypes.string,
+  abstract: PropTypes.string,
+  review: PropTypes.string
+});
+
+BestNewMusicMenu.propTypes = {
+  albums: PropTypes.arrayOf(bestNewItemShape),
+  tracks: PropTypes.arrayOf(bestNewItemShape),
+  setActiveItem: PropTypes.func
+};
+
+export default BestNewMusicMenu;
