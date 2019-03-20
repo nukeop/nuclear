d4d88b382d178870fcd01df17a12a5d84fda6be7
nukeop <alsw@protonmail.com>
2019-03-20 23:10:16 +0100

    New, nicer review component

--- app/components/Dashboard/BestNewMusicTab/BestNewMusicContent/index.js
+++ app/components/Dashboard/BestNewMusicTab/BestNewMusicContent/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import { bestNewItemShape } from '..';
 
@@ -11,11 +12,31 @@ const BestNewMusicContent = props => {
   if (item === null) {
     return null;
   }
-  
+  console.log(item);
   return (
     <div className={styles.best_new_music_content}>
+      <div className={styles.review_header}>
+        <div className={styles.thumbnail}>
+          <img alt={ item.title } src={ item.thumbnail }/>
+        </div>
+        <div className={styles.review_headings}>
+          <div className={styles.artist}>{ item.artist }</div>
+          <div className={styles.title}>{ item.title }</div>
+        </div>
+        <div className={styles.score}>
+          <div className={styles.score_box}>
+            { item.score }
+          </div>
+        </div>
+      </div>
       {
-        item.review
+        item.review.split('\n').map(i => {
+          return (
+              <p key={'item-' + i} className={styles.paragraph}>
+                  {i}
+                </p>
+          );
+        })
       }
     </div>
   );
