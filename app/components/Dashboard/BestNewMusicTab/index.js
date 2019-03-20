b5c4a4c2de790d2e978c570d68a8bb1c0bc271a2
nukeop <alsw@protonmail.com>
2019-03-20 01:14:28 +0100

    Redesigned best new music; review box coming tomorrow

--- app/components/Dashboard/BestNewMusicTab/index.js
+++ app/components/Dashboard/BestNewMusicTab/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { Tab } from 'semantic-ui-react';
+import _ from 'lodash';
 
-import BestNewList from './BestNewList';
+import BestNewMusicMenu from './BestNewMusicMenu';
+import BestNewMusicContent from './BestNewMusicContent';
 import styles from './styles.scss';
 
 class BestNewMusicTab extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      activeItem: null
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.state.activeItem === null) {
+      const firstAlbum = _.head(
+        _.get(nextProps, 'dashboardData.bestNewAlbums')
+      );
+      
+      if(firstAlbum) {
+        this.setState({
+          activeItem: firstAlbum
+        });
+      }
+    }
   }
 
   isLoading () {
     return this.props.dashboardData.bestNewAlbums.length < 1 || this.props.dashboardData.bestNewTracks.length < 1;
+  }
+
+  setActiveItem(activeItem) {
+    this.setState({ activeItem });
   }
 
   render () {
@@ -20,43 +44,23 @@ class BestNewMusicTab extends React.Component {
       history,
       albumInfoSearchByName
     } = this.props;
+    
     return (
-      <Tab.Pane loading={this.isLoading()} attached={false} className={styles.best_new_music_tab_pane}>
-        <div className={styles.best_new_music_tab_container}>
-          <h2>
-            Best new albums
-          </h2>
-          <div className={styles.best_new_music_section_container}>
-            <BestNewList
-              data={dashboardData.bestNewAlbums}
-              artistInfoSearchByName={artistInfoSearchByName}
-              albumInfoSearchByName={albumInfoSearchByName}
-              history={history}
-              addToQueue={this.props.addToQueue}
-              selectSong={this.props.selectSong}
-              clearQueue={this.props.clearQueue}
-              startPlayback={this.props.startPlayback}
-              musicSources={this.props.musicSources}
-            />
-          </div>
-          <h2>
-            Best new tracks
-          </h2>
-          <div className={styles.best_new_music_section_container}>
-            <BestNewList
-              data={dashboardData.bestNewTracks}
-              artistInfoSearchByName={artistInfoSearchByName}
-              history={history}
-              addToQueue={this.props.addToQueue}
-              selectSong={this.props.selectSong}
-              clearQueue={this.props.clearQueue}
-              startPlayback={this.props.startPlayback}
-              musicSources={this.props.musicSources}
-            />
-          </div>
-        </div>
+      <Tab.Pane
+        loading={this.isLoading()}
+        attached={false}
+        className={styles.best_new_music_tab_pane}
+      >
+        <BestNewMusicMenu
+          albums={ dashboardData.bestNewAlbums }
+          tracks={ dashboardData.bestNewTracks }
+          setActiveItem={ this.setActiveItem.bind(this) }
+        />
+        <BestNewMusicContent
+          item={ this.state.activeItem }
+          />
       </Tab.Pane>
-    );
+    );      
   }
 }
 
