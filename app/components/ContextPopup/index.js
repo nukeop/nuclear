38ca3e80f49f33aa6543f6f8f4ba75915e4ec49a
nukeop <alsw@protonmail.com>
2019-04-06 03:25:27 +0200

    Refactor: turn track row popups into their own containers that manage their own props

--- app/components/ContextPopup/index.js
+++ app/components/ContextPopup/index.js
@@ -38,16 +38,16 @@ class ContextPopup extends React.Component {
         onClose={this.handleClose.bind(this)}
         onOpen={this.handleOpen.bind(this)}
         hideOnScroll
-	    >
+      >
         <div className={styles.popup_header}>
           <div className={styles.popup_thumb}><img src={this.props.thumb} /></div>
           <div className={styles.popup_info}>
             <div className={styles.popup_title}>{this.props.title}</div>
             {
-	            this.props.artist
-	              ? <div className={styles.popup_artist}>by {this.props.artist}</div>
-	              : null
-	          }
+              this.props.artist
+                ? <div className={styles.popup_artist}>by {this.props.artist}</div>
+                : null
+            }
           </div>
         </div>
 
