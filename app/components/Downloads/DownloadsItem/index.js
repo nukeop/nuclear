1ba436625d5484bf6c20535dc7d6094b7ebe2d2c
nukeop <alsw@protonmail.com>
2019-04-16 00:08:13 +0200

    Styles for download list and its items

--- app/components/Downloads/DownloadsItem/index.js
+++ app/components/Downloads/DownloadsItem/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Table } from 'semantic-ui-react';
+import { Icon, Table } from 'semantic-ui-react';
 import _ from 'lodash';
 
 import styles from './styles.scss';
+
+const StatusIcon = props => {
+  switch (props.status) {
+  case 'Waiting':
+    return <Icon name='hourglass start'/>;
+  case 'Finished':
+    return <Icon name='checkmark' color='green'/>;
+  case 'Started':
+    return <Icon name='cloud download'/>;
+  default:
+    return <Icon name='times' color='red'/>;
+  }
+};
+
+StatusIcon.propTypes = {
+  status: PropTypes.string.isRequired
+};
 
 const DownloadsItem = props => {
   const {
@@ -13,13 +30,14 @@ const DownloadsItem = props => {
   return (
     <Table.Row>
       <Table.Cell>
+        <StatusIcon status={ item.status }/>
         { item.status }
       </Table.Cell>
       <Table.Cell>
         { _.get(item, 'track.artist.name') } - { _.get(item, 'track.name') }
       </Table.Cell>
       <Table.Cell>
-        { item.completion }
+        { _.round(item.completion, 2) * 100 + '%' }
       </Table.Cell>
     </Table.Row>
   );
