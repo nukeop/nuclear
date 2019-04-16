ad1d0a378e7b2d77f13aa69aa7cda4dd62bb016e
nukeop <alsw@protonmail.com>
2019-04-17 00:59:40 +0200

    help button stub

---
+++ app/components/HelpButton/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Icon } from 'semantic-ui-react';
+
+import styles from './styles.scss';
+
+const HelpButton = props => {
+  return (
+    <Button icon
+      className={styles.help_button}
+    >
+      <Icon name='question circle outline'/>
+    </Button>
+  );
+};
+
+HelpButton.propTypes = {
+
+};
+
+HelpButton.defaultProps = {
+
+};
+
+export default HelpButton;
