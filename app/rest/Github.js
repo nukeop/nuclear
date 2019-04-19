cd0e7cf8bd322df7ab0d2cfec58ce6990a4e5cf9
nukeop <alsw@protonmail.com>
2019-04-20 00:51:06 +0200

    Create github login button and oauth popup

---
+++ app/rest/Github.js
@@ -0,0 +1,16 @@
+import {
+  githubClientId
+} from '../globals';
+
+const GITHUB_OAUTH_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize';
+const GITHUB_OAUTH_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+const GITHUB_API_USER_ENDPOINT = 'https://api.github.com/user';
+
+
+export const getGithubOauthUrl = () => {
+  return GITHUB_OAUTH_AUTHORIZE_URL +
+    '?client_id=' +
+    githubClientId +
+    '&redirect_uri=' +
+    window.location.href;
+};
