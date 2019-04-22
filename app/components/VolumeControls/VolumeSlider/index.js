458926effd2d27098f48723373a038e1028c77a5
Sharl <ssharl@ymail.com>
2019-04-22 21:28:57 +0200

    when the audio is muted, you can't click on the slider to unmute & change the volume level at the same time

--- app/components/VolumeControls/VolumeSlider/index.js
+++ app/components/VolumeControls/VolumeSlider/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-import styles from './styles.scss';
-import Range from 'react-range-progress';
+import styles from './styles.scss'
+import Range from 'react-range-progress'
 
 const volumeSliderColors = {
   fillColor: { r: 248, g: 248, b: 242, a: 1 },
   trackColor: { r: 68, g: 71, b: 90, a: 1 },
   thumbColor: { r: 248, g: 248, b: 242, a: 1 }
-};
+}
 
 class VolumeSlider extends React.Component {
-
   handleClick (value) {
-    this.props.handleClick(value);
+    this.props.handleClick(value)
   }
 
   render () {
     return (
-      <div className={styles.volume_slider_container}>
+      <div
+        className={styles.volume_slider_container}
+        onClick={this.props.muted ? this.props.toggleMute : () => {}}
+      >
         <Range
           value={this.props.fill}
           fillColor={volumeSliderColors.fillColor}
@@ -28,8 +30,8 @@ class VolumeSlider extends React.Component {
           onChange={this.handleClick.bind(this)}
         />
       </div>
-    );
+    )
   }
 }
 
-export default VolumeSlider;
+export default VolumeSlider
--- app/components/VolumeControls/index.js
+++ app/components/VolumeControls/index.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import FontAwesome from 'react-fontawesome';
+import React from 'react'
+import FontAwesome from 'react-fontawesome'
 
-import styles from './styles.scss';
+import styles from './styles.scss'
 
-import PlayOptionsControls from '../PlayOptionsControls';
-import VolumeSlider from './VolumeSlider';
+import PlayOptionsControls from '../PlayOptionsControls'
+import VolumeSlider from './VolumeSlider'
 
 class VolumeControls extends React.Component {
-
-  handleClick(value) {
-    return this.props.updateVolume(value);
+  handleClick (value) {
+    return this.props.updateVolume(value)
   }
 
-  toggleMute(){
-    this.props.toggleMute(!this.props.muted);
+  toggleMute () {
+    this.props.toggleMute(!this.props.muted)
   }
 
-  render() {
+  render () {
     return (
       <div className={styles.volume_controls_container}>
         <PlayOptionsControls
           toggleOption={this.props.toggleOption}
           settings={this.props.settings}
         />
-        <div className={styles.volume_speaker_control}
-          onClick={this.toggleMute.bind(this)}>
+        <div
+          className={styles.volume_speaker_control}
+          onClick={this.toggleMute.bind(this)}
+        >
           <FontAwesome name={this.props.muted ? 'volume-off' : 'volume-up'} />
         </div>
 
         <VolumeSlider
-          fill={this.props.fill}
+          fill={this.props.muted ? 0 : this.props.fill}
           handleClick={this.handleClick.bind(this)}
+          muted={this.props.muted}
+          toggleMute={this.toggleMute.bind(this)}
         />
       </div>
-    );
+    )
   }
 }
 
-export default VolumeControls;
+export default VolumeControls
