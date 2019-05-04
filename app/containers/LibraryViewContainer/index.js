8b7db46a1a290b2d666ac773dff0a213d8a1e61f
Charles Jacquin <charles.jacquin@autistici.org>
2019-05-04 10:56:01 +0200

    feat(local): add filter and sortable Table

--- app/containers/LibraryViewContainer/index.js
+++ app/containers/LibraryViewContainer/index.js
@@ -1,16 +1,34 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import _ from 'lodash';
+import { sortBy } from 'lodash';
 
 import LibraryView from '../../components/LibraryView';
 import * as LocalActions from '../../actions/local';
 
 function mapStateToProps(state) {
+  const filteredTracks = Object.values(state.local.tracks)
+    .filter(({ artist, name }) => artist.name.includes(state.local.filter) || name.includes(state.local.filter));
+
+  let tracks;
+  switch (state.local.sortBy) {
+  case 'artist':
+    tracks = sortBy(filteredTracks, track => track.artist.name);
+    break;
+  case 'name':
+    tracks = sortBy(filteredTracks, track => track.name);
+    break;
+  default:
+    tracks = filteredTracks;
+    break;
+  }
+
   return {
     pending: state.local.pending,
-    tracks: Object.values(state.local.tracks),
-    localFolders: state.local.folders
-    // byArtist: _.groupBy(Object.values(state.local.tracks), track => track.artist)
+    tracks: state.local.direction === 'ascending' ? tracks : tracks.reverse(),
+      
+    localFolders: state.local.folders,
+    sortBy: state.local.sortBy,
+    direction: state.local.direction
   };
 }
 
