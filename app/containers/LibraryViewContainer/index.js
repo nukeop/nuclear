742f3b93128cfef9eaa0b6cc25871cbebc5650ae
nukeop <alsw@protonmail.com>
2019-05-27 23:37:57 +0200

    Fix filtering in library view

--- app/containers/LibraryViewContainer/index.js
+++ app/containers/LibraryViewContainer/index.js
@@ -6,9 +6,16 @@ import LibraryView from '../../components/LibraryView';
 import * as LocalActions from '../../actions/local';
 
 function mapStateToProps(state) {
-  const filteredTracks = Object.values(state.local.tracks)
-    .filter(({ artist, name }) => artist.name.includes(state.local.filter) || name.includes(state.local.filter));
-
+  const lowercaseFilter = _.lowerCase(state.local.filter);
+  const checkFilter = string => _.includes(_.lowerCase(string), lowercaseFilter);
+  const filteredTracks = _.filter(_.values(state.local.tracks), track => {
+    return _.some([
+      checkFilter(track.name),
+      checkFilter(track.album),
+      checkFilter(_.get(track, 'artist.name'))
+    ]);
+  });
+  
   let tracks;
   switch (state.local.sortBy) {
   case 'artist':
