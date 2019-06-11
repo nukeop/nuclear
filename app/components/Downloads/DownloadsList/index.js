cf01ef3332c529a1f9bf83484f6f8dff0cd2a5d2
Saim <saimfd00@gmail.com>
2019-06-11 16:08:22 +0500

    Added sort by name in Downloads

--- app/components/Downloads/DownloadsList/index.js
+++ app/components/Downloads/DownloadsList/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import { Button, Icon, Segment, Table } from 'semantic-ui-react';
 import { useTranslation } from 'react-i18next';
@@ -8,13 +8,13 @@ import DownloadsItem from '../DownloadsItem';
 import styles from './styles.scss';
 
 const DownloadsList = props => {
+  const [sortAsc, setSort] = useState(true);
   const {
     items,
     clearFinishedTracks
   } = props;
 
   const { t } = useTranslation('downloads');
-  
   return (
     <Segment inverted>
       <Button primary onClick={ clearFinishedTracks }>
@@ -25,7 +25,23 @@ const DownloadsList = props => {
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>{t('status')}</Table.HeaderCell>
-            <Table.HeaderCell>{t('name')}</Table.HeaderCell>
+            <Table.HeaderCell onClick={() => {
+                if(sortAsc){
+                  items.sort((a, b) => {
+                    return a.track.name.toLowerCase() > b.track.name.toLowerCase();
+                  })
+                  setSort(false)
+                } else {
+                  items.sort((a, b) => {
+                    return a.track.name.toLowerCase() < b.track.name.toLowerCase();
+                  })
+                  setSort(true)
+                }
+              }
+            }>{t('name')} {
+                sortAsc ? <Icon name="caret up icon"></Icon> : <Icon name="caret down icon"></Icon>
+              }
+            </Table.HeaderCell>
             <Table.HeaderCell>{t('completion')}</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
