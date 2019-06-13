f59e6fcf49f6126d2042b02b3274fd00f5ff61ed
Martin Nøhr Åskov Hansen <noehraaskov@gmail.com>
2019-06-13 16:56:06 +0200

    Danish translation

--- app/constants/settings.js
+++ app/constants/settings.js
@@ -89,7 +89,7 @@ export default [
     type: settingType.LIST,
     prettyName: 'language',
     placeholder: 'language-placeholder',
-    options: ['en', 'fr', 'nl'],
+    options: ['en', 'fr', 'nl', 'dk'],
     default: undefined
   }
   // To be enabled when MPD integration is ready
