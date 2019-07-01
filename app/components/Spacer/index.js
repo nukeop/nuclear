389338de2e294f18c5d059efd5b3d3ef6b924954
nukeop <alsw@protonmail.com>
2019-07-01 22:59:43 +0200

    Fix broken spacer styles

--- app/components/Spacer/index.js
+++ app/components/Spacer/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import cx from 'classnames';
+
 import styles from './styles.scss';
 
-export default () => (<div className={styles.spacer}/>);
+export default ({ className, style }) => (<div
+  style={style}
+  className={
+    cx(
+      className,
+      styles.spacer
+    )
+  }
+/>);
