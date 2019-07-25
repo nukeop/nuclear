4869fca10b81d8048241db6466ac25ccb6be5cf4
Jakub <jakubjezewski@outlook.com>
2019-07-25 19:02:20 +0200

    Tests for VerticalPanel, WindowControls, Navbar and WindowButtons (#416)
    * tests for VerticalPanel and WindowControls
    
    * Navbar and WindowButton tests
    
    * improve VerticalPanel test

---
+++ test/enzyme.windowcontrols.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { expect } from "chai";
+import { shallow } from "enzyme";
+import { describe, it } from "mocha";
+
+import FontAwesome from "react-fontawesome";
+
+import WindowButton from "../app/components/WindowControls/WindowButton";
+import WindowControls from "../app/components/WindowControls";
+
+describe("<WindowControls />", () => {
+  it("renders with window buttons", () => {
+    const wrapper = shallow(<WindowControls />);
+    expect(wrapper.containsMatchingElement(<WindowButton />)).to.equal(true);
+  });
+});
+
+describe("<WindowButton />", () => {
+  const color = "#fff";
+  const icon = "icon";
+
+  it("fires onClick func", () => {
+    let testVal = null;
+    const onClick = () => {
+      testVal = "clicked";
+    };
+    const wrapper = shallow(
+      <WindowButton color={color} icon={icon} onClick={onClick} />
+    );
+
+    wrapper.simulate("click");
+    expect(testVal).to.equal("clicked");
+  });
+
+  it("displays correct icon", () => {
+    const wrapper = shallow(
+      <WindowButton color={color} icon={icon} onClick={() => {}} />
+    );
+
+    expect(
+      wrapper
+        .find(FontAwesome)
+        .at(0)
+        .prop("name")
+    ).to.equal(icon);
+  });
+});
