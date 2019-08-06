29bd73d164e9452f10228d7109bed2c95cc9468c
nukeop <nukeop@gumblert.tech>
2019-08-07 01:50:42 +0200

    Allow injecting stor einto updatePlaylist; test for updatePlaylist

--- test/playlists.test.js
+++ test/playlists.test.js
@@ -1,25 +1,44 @@
 import { expect } from 'chai';
 
 import {
-  deletePlaylistInjectable
+  deletePlaylistInjectable,
+  updatePlaylistInjectable
 } from '../app/actions/playlists.injectable';
 
 describe('Playlist actions tests', () => {
-  it('deletes a track from a playlist', () => {
-    const fakeStore = {
+  const createFakeStore = () => ({
       get: () => ([
         { id: 'qwerty' },
         { id: 'asdf' },
         { id: 'abc' }
       ]),
       set: () => {}
-    };
-    
+  });
+  
+  it('deletes a track from a playlist', () => {
+    const fakeStore = createFakeStore();
     const deletePlaylist = deletePlaylistInjectable(fakeStore);
     const playlists = deletePlaylist('abc');
     expect(playlists).to.eql([
       { id: 'qwerty' },
       { id: 'asdf' }
+    ]);
+  });
+
+  it('replaces a playlist', () => {
+    const fakeStore = createFakeStore();
+    const updatePlaylist = updatePlaylistInjectable(fakeStore);
+    const playlists = updatePlaylist({
+      id: 'abc',
+      tracks: [ 4, 8, 15 ]
+    });
+    expect(playlists).to.eql([
+      { id: 'qwerty' },
+        { id: 'asdf' },
+      {
+        id: 'abc',
+        tracks: [ 4, 8, 15 ]
+      }
     ]);
   });
 });
