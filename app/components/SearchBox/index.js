c31b98cdec736e93b47e1c1e8973d6caaece3d11
Nikolay Karev <v8cult@yandex-team.ru>
2019-08-11 17:10:01 +0500

    Repeat search on Enter keypress in the search box

--- app/components/SearchBox/index.js
+++ app/components/SearchBox/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import classnames from 'classnames';
 import FontAwesome from 'react-fontawesome';
 import DebounceInput from 'react-debounce-input';
@@ -8,6 +8,17 @@ import { useTranslation } from 'react-i18next';
 
 const SearchBox = ({ handleSearch, loading }) => {
   const { t } = useTranslation('search');
+  const [latestSearch, setSearch] = useState('');
+  const handleKeyDown = useCallback((e) => {
+    if (e.which === 13) {
+      handleSearch(latestSearch);
+    }
+  }, [handleSearch, latestSearch]);
+
+  const handleSearchInner = useCallback((value) => {
+    setSearch(value);
+    handleSearch(value);
+  }, [handleSearch]);
 
   return (
     <div className={styles.search_box_container}>
@@ -17,7 +28,8 @@ const SearchBox = ({ handleSearch, loading }) => {
           placeholder={t('placeholder')}
           minLength={2}
           debounceTimeout={500}
-          onChange={handleSearch}
+          onChange={handleSearchInner}
+          onKeyDown={handleKeyDown}
           autoFocus
         />
         <FontAwesome name='spinner' pulse className={classnames({loading})} />
