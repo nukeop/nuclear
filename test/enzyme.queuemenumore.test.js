4b89200416cc88f93ebd53fae34745b029d43516
Jen Yee <jenyee@Jens-MacBook-Pro.local>
2019-08-12 21:21:32 -0700

    refactor and better test names

--- test/enzyme.queuemenumore.test.js
+++ test/enzyme.queuemenumore.test.js
@@ -10,44 +10,50 @@ import QueueMenuMore from '../app/components/PlayQueue/QueueMenu/QueueMenuMore'
 chai.use(spies);
 const { expect } = chai;
 
-describe('Clear queue button', () => {
+describe('<QueueMenuMore /> Clear queue button', () => {
   it('Has trash icon and clearQueue action fired on click', () => {
     const spy = chai.spy();
     const wrapper = mount(<QueueMenuMore clearQueue={spy}/>);
-    wrapper.find(Dropdown.Item).at(0).simulate('click');
+    const trashItem = wrapper.find(Dropdown.Item).at(0);
 
+    trashItem.simulate('click');
     expect(spy).to.have.been.called;
-    expect(wrapper.find(Dropdown.Item).at(0).find(Icon).prop('name')).to.equal('trash');
+    expect(trashItem.find(Icon).prop('name')).to.equal('trash');
   });
 });
 
-describe('Playlist button', () => {
+describe('<QueueMenuMore /> Playlist button', () => {
   it('Has music icon and adds track to playlist on click', () => {
     const spy = chai.spy();
     const wrapper = mount(<QueueMenuMore updatePlaylist={spy} playlists={[1]}/>);
+    const trackItem = wrapper.find(Dropdown.Item).at(2)
 
-    wrapper.find(Dropdown.Item).at(2).simulate('click');
+    trackItem.simulate('click');
     expect(spy).to.have.been.called;
-    expect(wrapper.find(Dropdown.Item).at(2).find(Icon).prop('name')).to.equal('music');
+    expect(trackItem.find(Icon).prop('name')).to.equal('music');
   });
 });
 
-describe('Add favorite track button', () => {
+describe('<QueueMenuMore /> Add favorite track button', () => {
   it('Has star icon and addFavoriteTrack fired on click of playlist item', () => {
     const spy = chai.spy();
     const wrapper = mount(<QueueMenuMore addFavoriteTrack={spy} playlists={[1]}/>);
-    wrapper.find(Dropdown.Item).at(3).simulate('click');
+    const favItem = wrapper.find(Dropdown.Item).at(3);
+
+    favItem.simulate('click');
     expect(spy).to.have.been.called;
-    expect(wrapper.find(Dropdown.Item).at(3).find(Icon).prop('name')).to.equal('star');
+    expect(favItem.find(Icon).prop('name')).to.equal('star');
   });
 });
 
-describe('Add to downloads button', () => {
+describe('<QueueMenuMore /> Add to downloads button', () => {
   it('Has download icon and fires addToDownloads on click of playlist item', () => {
     const spy = chai.spy();
     const wrapper = mount(<QueueMenuMore addToDownloads={spy} playlists={[1]}/>);
-    wrapper.find(Dropdown.Item).at(4).simulate('click');
+    const downloadItem = wrapper.find(Dropdown.Item).at(4)
+
+    downloadItem.simulate('click');
     expect(spy).to.have.been.called;
-    expect(wrapper.find(Dropdown.Item).at(4).find(Icon).prop('name')).to.equal('download');
+    expect(downloadItem.find(Icon).prop('name')).to.equal('download');
   });
 });
