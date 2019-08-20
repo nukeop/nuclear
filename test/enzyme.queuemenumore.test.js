7817ad456616a4a8fa6e2be0ae62aabaefd977a0
Nikolay Karev <karev.n@gmail.com>
2019-08-21 01:29:00 +0500

    New tests

--- test/enzyme.queuemenumore.test.js
+++ test/enzyme.queuemenumore.test.js
@@ -1,59 +1,58 @@
 import React from 'react';
-import chai from 'chai';
-import spies from 'chai-spies';
 import { mount } from 'enzyme';
-import { describe, it } from 'mocha';
 import { Dropdown, Icon } from 'semantic-ui-react';
 
-import QueueMenuMore from '../app/components/PlayQueue/QueueMenu/QueueMenuMore'
+import QueueMenuMore from '../app/components/PlayQueue/QueueMenu/QueueMenuMore';
 
-chai.use(spies);
-const { expect } = chai;
 
 describe('<QueueMenuMore /> Clear queue button', () => {
   it('Has trash icon and clearQueue action fired on click', () => {
-    const spy = chai.spy();
+    const spy = jest.fn();
     const wrapper = mount(<QueueMenuMore clearQueue={spy}/>);
+    expect(wrapper).toMatchSnapshot();
     const trashItem = wrapper.find(Dropdown.Item).at(0);
 
     trashItem.simulate('click');
-    expect(spy).to.have.been.called;
-    expect(trashItem.find(Icon).prop('name')).to.equal('trash');
+    expect(spy).toHaveBeenCalled();
+    expect(trashItem.find(Icon).prop('name')).toBe('trash');
   });
 });
 
 describe('<QueueMenuMore /> Playlist button', () => {
   it('Has music icon and adds track to playlist on click', () => {
-    const spy = chai.spy();
-    const wrapper = mount(<QueueMenuMore updatePlaylist={spy} playlists={[1]}/>);
-    const trackItem = wrapper.find(Dropdown.Item).at(2)
+    const spy = jest.fn();
+    const wrapper = mount(<QueueMenuMore updatePlaylist={spy} playlists={[{name: 'test', tracks: []}]} currentItem={{name: 'test'}}/>);
+    expect(wrapper).toMatchSnapshot();
+    const trackItem = wrapper.find(Dropdown.Item).at(2);
 
     trackItem.simulate('click');
-    expect(spy).to.have.been.called;
-    expect(trackItem.find(Icon).prop('name')).to.equal('music');
+    expect(spy).toHaveBeenCalled();
+    expect(trackItem.find(Icon).prop('name')).toBe('music');
   });
 });
 
 describe('<QueueMenuMore /> Add favorite track button', () => {
   it('Has star icon and addFavoriteTrack fired on click of playlist item', () => {
-    const spy = chai.spy();
-    const wrapper = mount(<QueueMenuMore addFavoriteTrack={spy} playlists={[1]}/>);
+    const spy = jest.fn();
+    const wrapper = mount(<QueueMenuMore addFavoriteTrack={spy} playlists={[{name: 'test', tracks: []}]} currentItem={{name: 'test'}}/>);
+    expect(wrapper).toMatchSnapshot();
     const favItem = wrapper.find(Dropdown.Item).at(3);
 
     favItem.simulate('click');
-    expect(spy).to.have.been.called;
-    expect(favItem.find(Icon).prop('name')).to.equal('star');
+    expect(spy).toHaveBeenCalled();
+    expect(favItem.find(Icon).prop('name')).toBe('star');
   });
 });
 
 describe('<QueueMenuMore /> Add to downloads button', () => {
   it('Has download icon and fires addToDownloads on click of playlist item', () => {
-    const spy = chai.spy();
+    const spy = jest.fn();
     const wrapper = mount(<QueueMenuMore addToDownloads={spy} playlists={[1]}/>);
-    const downloadItem = wrapper.find(Dropdown.Item).at(4)
+    expect(wrapper).toMatchSnapshot();
+    const downloadItem = wrapper.find(Dropdown.Item).at(4);
 
     downloadItem.simulate('click');
-    expect(spy).to.have.been.called;
-    expect(downloadItem.find(Icon).prop('name')).to.equal('download');
+    expect(spy).toHaveBeenCalled();
+    expect(downloadItem.find(Icon).prop('name')).toBe('download');
   });
 });
