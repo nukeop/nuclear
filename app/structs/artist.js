ca83c9ed92328debfcf4d6419512689343adbcc2
nukeop <nukeop@gumblert.tech>
2019-09-17 01:29:22 +0200

    Upgraded artists struct + tests

--- app/structs/artist.js
+++ app/structs/artist.js
@@ -1,23 +1,27 @@
+import get from 'lodash/get';
+import uuidv4 from 'uuid/v4';
+
 export default class Artist {
   constructor(data) {
-    this.id = data.id;
-    this.name = data.name;
-    this.description = data.description;
-    this.tags = data.tags;
-    this.onTour = data.onTour;
+    this.uuid = uuidv4();
+    this.ids = get(data, 'ids');
+    this.name = get(data, 'name');
+    this.description = get(data, 'description');
+    this.tags = get(data, 'tags');
+    this.onTour = get(data, 'onTour');
 
-    this.coverImage = data.coverImage;
-    this.thumbnail = data.thumbnail;
-    this.images = data.images;
+    this.coverImage = get(data, 'coverImage');
+    this.thumbnail = get(data, 'thumbnail');
+    this.images = get(data, 'images');
 
-    this.albums = data.albums;
-    this.topTracks = data.topTracks;
-    this.similarArtists = data.similarArtists;
+    this.albums = get(data, 'albums');
+    this.topTracks = get(data, 'topTracks');
+    this.similarArtists = get(data, 'similarArtists');
   }
 
   static fromDiscogsSearchData(data) {
     return new Artist({
-      id: data.id,
+      ids: { discogs: data.id },
       name: data.title,
       coverImage: data.cover_image,
       thumbnail: data.thumb
@@ -26,7 +30,7 @@ export default class Artist {
 
   static fromDiscogsData(data) {
     return new Artist({
-      id: data.id,
+      ids: { discogs: data.id },
       name: data.name,
       description: data.profile,
       images: data.images
@@ -35,6 +39,7 @@ export default class Artist {
 
   static fromLastfmData(data) {
     return new Artist({
+      ids: { musicbrainz: data.mbid },
       name: data.name,
       description: _.get(data, 'bio.content'),
       tags: _.map(_.get(data, 'tags.tag'), 'name'),
