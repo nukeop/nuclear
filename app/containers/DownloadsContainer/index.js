e99594938dc5c078831d47edb33bc1ef7d13d0a4
nukeop <nukeop@gumblert.tech>
2019-09-22 02:12:34 +0200

    Add a box for setting the downloads dir in the downloads section

--- app/containers/DownloadsContainer/index.js
+++ app/containers/DownloadsContainer/index.js
@@ -4,13 +4,16 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import * as DownloadActions from '../../actions/downloads';
+import * as SettingsActions from '../../actions/settings';
 import Downloads from '../../components/Downloads';
 
 const DownloadsContainer = props => {
   return (
     <Downloads
       downloads={props.downloads}
+      downloadsDir={props.settings['downloads.dir']}
       clearFinishedTracks={props.downloadActions.clearFinishedDownloads}
+      setStringOption={props.settingsActions.setStringOption}
     />
   );
 };
@@ -19,6 +22,9 @@ DownloadsContainer.propTypes = {
   downloads: PropTypes.array,
   downloadActions: PropTypes.shape({
     clearFinishedDownloads: PropTypes.func
+  }),
+  settingsActions: PropTypes.shape({
+    setStringOption: PropTypes.func
   })
 };
 
@@ -29,13 +35,15 @@ DownloadsContainer.defaultProps = {
 
 function mapStateToProps (state) {
   return {
-    downloads: state.downloads.downloads
+    downloads: state.downloads.downloads,
+    settings: state.settings
   };
 }
 
 function mapDispatchToProps (dispatch) {
   return {
-    downloadActions: bindActionCreators(DownloadActions, dispatch)
+    downloadActions: bindActionCreators(DownloadActions, dispatch),
+    settingsActions: bindActionCreators(SettingsActions, dispatch)
   };
 }
 
