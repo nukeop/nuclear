72ed204bf2ece2f645c7ac186045f1a8a8b665de
Daniel Wang <danielwang405@gmail.com>
2019-10-03 18:05:06 -0400

    #489 Disabling more queue options if queue is empty (#496)
    * Fix add favorite track button test
    
    * Disable queue menu when queue is empty
    
    * Create test for queue menu
    
    * Use lodash for better readability

--- packages/app/app/components/PlayQueue/QueueMenu/index.js
+++ packages/app/app/components/PlayQueue/QueueMenu/index.js
@@ -59,6 +59,7 @@ class QueueMenu extends React.Component {
           {
             !compact &&
               <QueueMenuMore
+                disabled={_.isEmpty(items)}
                 clearQueue={clearQueue}
                 updatePlaylist={updatePlaylist}
                 addFavoriteTrack={addFavoriteTrack}
