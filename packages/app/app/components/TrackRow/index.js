50dbee0b1bd3a3b8f342232276be8e505e0b887a
Zalaxx <leo.jaillet@protonmail.com>
2019-10-10 22:24:51 +0200

    remove useless .get

--- packages/app/app/components/TrackRow/index.js
+++ packages/app/app/components/TrackRow/index.js
@@ -48,7 +48,7 @@ class TrackRow extends React.Component {
     return _.get(
       this.props.track,
       'thumbnail',
-      _.get(this.props.track, 'image[0][#text]', artPlaceholder)
+      artPlaceholder
     );
   }
 
