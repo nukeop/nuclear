5baf4a49c378995034d2accc83f82051a5796561
nukeop <nukeop@gumblert.tech>
2019-10-12 03:28:50 +0200

    Create a function for transforming source strings

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,6 @@
 export {
   LastFmApi,
   isElectron,
+  transformSource,
   transformPluginFile
 } from './lib';
--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,4 +1,4 @@
 import 'isomorphic-fetch';
 export { default as LastFmApi } from './rest/lastfm';
 export { isElectron } from './util';
-export { transformPluginFile } from './plugins/transform';
+export { transformSource, transformPluginFile } from './plugins/transform';
