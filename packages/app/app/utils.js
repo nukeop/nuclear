bde96417cc9bfd7c32d619e92cd3bf91f1926876
Zalax <leo.jaillet@protonmail.com>
2019-10-20 20:47:26 +0000

    improve source fallback (#521)
    * improve source fallback
    
    * cleanup code with lodash

--- packages/app/app/utils.js
+++ packages/app/app/utils.js
@@ -37,9 +37,9 @@ export function stringDurationToSeconds(duration) {
 export function getSelectedStream(streams, defaultMusicSource) {
   let selectedStream = _.find(streams, { source: defaultMusicSource });
 
-  return typeof selectedStream === 'undefined'
-    ? streams ? streams[0] : null
-    : selectedStream;
+  return _.isNil(selectedStream)
+   ? _.filter(streams, 'source')[0] || null
+   : selectedStream;
 }
 
 export function removeQuotes(text) {
