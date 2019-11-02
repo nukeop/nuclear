fe93b483e55b43e41f221b0d1ad95cef65f1f675
nukeop <nukeop@gumblert.tech>
2019-11-02 02:25:54 +0100

    Transpile nuclear packages for tests

--- packages/app/test/testHelper.js
+++ packages/app/test/testHelper.js
@@ -1,5 +1,9 @@
 const register = require('ignore-styles').default;
 
+require('@babel/register')({
+  ignore: [/node_modules\/(?!@nuclear)/]
+});
+
 register(['.css', '.scss']);
 register(undefined, (module, filename) => {
   if (['.png', '.jpg'].some(ext => filename.endsWith(ext))) {
