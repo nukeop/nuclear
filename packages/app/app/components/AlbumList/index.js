fb0ac68cd3177b0285adcd38daaf17f414e4d47f
nukeop <nukeop@gumblert.tech>
2019-11-06 22:46:46 +0100

    Modify tests for refactored components

--- packages/app/app/components/AlbumList/index.js
+++ packages/app/app/components/AlbumList/index.js
@@ -4,12 +4,12 @@ import { compose, withHandlers, lifecycle } from 'recompose';
 
 import {AlbumGrid} from '@nuclear/ui';
 
+const AlbumListComponent = props => <AlbumGrid {...props} />;
 export default compose(
   withHandlers({
     onAlbumClick: ({albumInfoSearch, history}) => (album) => {
       albumInfoSearch(album.id, album.type);
       history.push('/album/' + album.id);
     }
-  }),
-  lifecycle({componentDidUpdate(){console.log(this.props)}})
-)(AlbumGrid);
+  })
+)(AlbumListComponent);
