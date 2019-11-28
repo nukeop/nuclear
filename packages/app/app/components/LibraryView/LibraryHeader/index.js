3851666ddea8e560a28671af12c255a2d03738f9
nukeop <nukeop@gumblert.tech>
2019-11-28 01:52:46 +0100

    Collapsible library header

--- packages/app/app/components/LibraryView/LibraryHeader/index.js
+++ packages/app/app/components/LibraryView/LibraryHeader/index.js
@@ -1,20 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { Button } from 'semantic-ui-react';
+import { withTranslation } from 'react-i18next';
+import { withHandlers, withState, compose } from 'recompose';
 
 import Header from '../../Header';
 import LibraryFolders from '../LibraryFolders';
 
 import styles from './styles.scss';
 
-const LibraryHeader = props => (
+const LibraryHeader = ({
+  t,
+  collapsed,
+  toggleCollapsed,
+  ...rest
+}) => (
   <>
-    <Header>{t('header')}</Header>
-  
+    <div className={cx(
+      styles.library_header_row,
+      { collapsed }
+    )}>
+      <Header>{ t('header') }</Header>
+      <Button
+        basic
+        icon={`chevron ${collapsed ? 'down' : 'up'}`}
+        onClick={toggleCollapsed}
+      />
+    </div>
+    { !collapsed && <LibraryFolders {...rest}/> }
   </>
 );
 
 LibraryHeader.propTypes = {
-
+  collapsed: PropTypes.bool,
+  setCollapsed: PropTypes.func
 };
 
-export default LibraryHeader;
+LibraryHeader.defaultProps = {
+  collapsed: false,
+  setCollapsed: () => {}
+};
+
+export default compose(
+  withTranslation('library'),
+  withState('collapsed', 'setCollapsed', 'false'),
+  withHandlers({
+    toggleCollapsed: ({ collapsed, setCollapsed }) => () => setCollapsed(!collapsed)
+  })
+)(LibraryHeader);
