672160cef9f801a1647a291de4e3eb39044a2643
nukeop <nukeop@gumblert.tech>
2019-11-28 01:24:51 +0100

    Sort tracks by other attributes in order

--- packages/app/app/containers/LibraryViewContainer/utils.js
+++ packages/app/app/containers/LibraryViewContainer/utils.js
@@ -3,11 +3,11 @@ import _ from 'lodash';
 export function sortTracks(tracks, criteria) {
   switch (criteria) {
   case 'artist':
-    return _.sortBy(tracks, track => track.artist.name);
+    return _.sortBy(tracks, ['artist.name', 'album', 'pos']);
   case 'name':
-    return _.sortBy(tracks, track => track.name);
+    return _.sortBy(tracks, ['name', 'artist.name']);
   case 'album':
-    return _.sortBy(tracks, track => track.album);
+    return _.sortBy(tracks, ['album', 'pos']);
   default:
     return tracks;
   }
