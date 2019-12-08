be9d0b04ed70ab2e3be7b54a957084f8481a8539
Chazz <charles.jacquin@autistici.org>
2019-12-08 14:28:21 +0100

    chore: add back babel.config.js

---
+++ packages/app/babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        electron: '4.2'
+      }
+    }],
+    '@babel/preset-react'
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread'
+  ]
+};
