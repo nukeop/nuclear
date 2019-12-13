20a6aad85780d7f7f6b0f434bdd2968e2fa9ae99
nukeop <nukeop@gumblert.tech>
2019-12-13 01:53:37 +0100

    Fix display bugs related to compact queue items

---
+++ packages/ui/stories/queueItem.stories.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { QueueItem } from '..';
+import { formatDuration } from '../lib/utils';
+import { format } from 'path';
+
+const commonProps = {
+    track: {
+        thumbnail: 'https://i.imgur.com/4euOws2.jpg',
+        name: 'Test track name',
+        artist: 'Test artist'
+    },
+    isLoading: false,
+    isCurrent: false,
+    isCompact: false,
+    duration: formatDuration(123),
+    defaultMusicSource: {},
+    selectSong: () => {
+        alert('Item selected');
+    },
+    removeFromQueue: () => {
+        alert('Item removed from queue');
+    }
+};
+
+storiesOf('Queue item', module)
+    .add('Basic', () => {
+        return (
+            <div className='bg'>
+                <QueueItem {...commonProps} />
+                <QueueItem
+                    {...commonProps}
+                    track={{
+                        thumbnail: 'https://i.imgur.com/aVNWf3j.jpg',
+                        name: 'Small thumbnail',
+                        artist: 'Test artist'
+                    }}
+                />
+            </div>
+        );
+    })
+    .add('Loading', () => {
+        return (
+            <div className='bg'>
+                <QueueItem {...commonProps} isLoading />
+            </div>
+        );
+    })
+    .add('Current', () => {
+        return (
+            <div className='bg'>
+                <QueueItem {...commonProps} isCurrent />
+            </div>
+        );
+    })
+    .add('Compact', () => (
+        <div className='bg'>
+            <div
+                style={{ width: '3rem' }}
+            >
+                <QueueItem {...commonProps} isCompact />
+                <QueueItem {...commonProps} isCompact isLoading />
+                <QueueItem {...commonProps} isCompact isCurrent />
+            </div>
+        </div>
+    ))
