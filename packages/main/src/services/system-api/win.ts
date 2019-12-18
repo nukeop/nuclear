bcf44f7c9a15cf840ee2bc89623efba1a2ec9663
nukeop <nukeop@gumblert.tech>
2019-12-18 01:13:36 +0100

    Add injectable decorator to media services

--- packages/main/src/services/system-api/win.ts
+++ packages/main/src/services/system-api/win.ts
@@ -1,6 +1,8 @@
 import { Event } from 'electron';
 import NuclearApi from '../../utils/nuclear-api';
+import { mprisController } from '../../utils/decorators';
 
+@mprisController()
 class WindowsMediaService implements NuclearApi {
   rendererWindow: Event['sender'];
 
