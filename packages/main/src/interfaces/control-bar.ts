fe2b2d5bb72a9badba5ee3cd33e2b817d4843d97
Chazz <charles.jacquin@autistici.org>
2019-12-24 18:57:10 +0100

    fix(main): remove useless code

--- packages/main/src/interfaces/control-bar.ts
+++
@@ -1,9 +0,0 @@
-import { PlaybackStatus } from "@nuclear/common";
-
-export interface ControlBarState {
-  status: PlaybackStatus;
-  canNext: true;
-  canPrevious: true;
-  volume?: number;
-  seek?: number;
-}
