ed053bffebfb498c8f922bb327668fa9d0cff683
Charles <charles.jacquin@autistici.org>
2020-01-01 00:04:26 +0000

    fix(app): use explicitly Local plugin for startup track

--- packages/app/app/containers/IpcContainer/index.js
+++ packages/app/app/containers/IpcContainer/index.js
@@ -65,7 +65,10 @@ class IpcContainer extends React.Component {
     ipcRenderer.on('local-files', (event, data) => onLocalFiles(event, this.props.actions, data));
     ipcRenderer.on('local-files-error', (event, err) => onLocalFilesError(event, this.props.actions, err));
     ipcRenderer.on('play-startup-track', (event, track) => {
-      this.props.actions.playTrack(this.props.streamProviders, track);
+      this.props.actions.playTrack(
+        this.props.streamProviders.filter(({ sourceName }) => sourceName === 'Local'),
+        track
+      );
     });
 
     ipcRenderer.on('download-started', (event, data) => {
