4f5d8680c5183cc96e55688be8beabe12df692de
nukeop <nukeop@gumblert.tech>
2020-01-19 02:23:57 +0100

    Contributors component improvements

--- packages/app/app/components/HelpModal/Contributors/index.js
+++ packages/app/app/components/HelpModal/Contributors/index.js
@@ -2,46 +2,46 @@ import React from 'react';
 import { Loader, Card, Image } from 'semantic-ui-react';
 import styles from './styles.scss';
 
-export default function Contributors(props) {
-  const { loading, error, contributors} = props;
-
+const Contributors = ({
+  loading,
+  error,
+  contributors,
+  handleGithubClick
+}) => {
   if (loading) {
-    return <Loader />;
+    return <Loader className={styles.contributors_loader} />;
   }
 
   if (error) {
-    console.warn(error);
-    return <div>Errors when loading contributors from github...</div>;
+    return <div className={styles.contributors_error}>There was an error loading contributors from Github</div>;
   }
 
   const top10 = contributors.sort((a, b) => b.total - a.total).slice(0, 10).map((contributor) => {
     return (
-      <div
-        className={styles.contributors_card_container} 
-        key={contributor.author.id}>
-        <Card
-          href='#'
-          onClick={() => props.handleGithubClick(contributor.author.html_url)}
+      <Card
+        href='#'
+        className={styles.contributors_card}
+        key={contributor.author.id}
+        onClick={() => handleGithubClick(contributor.author.html_url)}
         >
           <Card.Content>
-            <Image
-              floated='right'
-              size='mini'
-              src={contributor.author.avatar_url}
-            />
-            <Card.Header>{contributor.author.login}</Card.Header>
-            <Card.Meta className={styles.meta_text}>{contributor.author.html_url}</Card.Meta>
-            <Card.Description>
-              <strong>{contributor.total}</strong> contributions.
+          <Image
+            floated='right'
+            size='mini'
+            src={contributor.author.avatar_url}
+          />
+          <Card.Header>{contributor.author.login}</Card.Header>
+          <Card.Meta className={styles.meta_text}>{contributor.author.html_url}</Card.Meta>
+          <Card.Description>
+            <strong>{contributor.total}</strong> contributions.
             </Card.Description>
-          </Card.Content>
-        </Card>
-      </div>
+        </Card.Content>
+      </Card>
     );
   });
 
-  return ( 
-    <Card.Group centered={true}>
+  return (
+    <Card.Group centered={true} itemsPerRow={2}>
       {top10}
     </Card.Group>
   );
@@ -50,3 +50,5 @@ export default function Contributors(props) {
 Contributors.defaultProps = {
   contributors: []
 };
+
+export default Contributors;
