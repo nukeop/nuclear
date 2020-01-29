585cd179b4470c1b2bd47a03cac7ba13b27107e6
nukeop <nukeop@gumblert.tech>
2020-01-29 01:49:38 +0100

    Bandcamp test output

--- packages/core/src/rest/Bandcamp.ts
+++ packages/core/src/rest/Bandcamp.ts
@@ -3,8 +3,6 @@ eslint-disable
 */
 import bandcamp from 'bandcamp-scraper';
 
-export const search = query => {
-  bandcamp.search({ query, page: 1 }, (err, results) => {
-    console.log(results);
-  });
+export const search = (query, callback) => {
+  bandcamp.search({ query, page: 1 }, callback);
 };
