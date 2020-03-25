c7776d670378d9e5f10d0698a290e88364124b1b
nukeop <nukeop@gumblert.tech>
2020-03-25 01:39:55 +0100

    TS config for ui package, Add TrackPopup to ui

---
+++ packages/ui/stories/trackPopup.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+import { TrackPopup } from '..';
+
+export default {
+  title: 'Track popup'
+}
+
+const track = {
+  artist: 'Test',
+  title: 'Test title',
+  thumb: 'https://i.imgur.com/4euOws2.jpg'
+};
+
+export const WithNoButtons = () => <div className='bg'>
+  <TrackPopup
+    trigger={<Button>Click here</Button>}
+    {...track}
+  />
+</div>;
+
+export const WithAllButtons = () => <div className='bg'>
+  <TrackPopup
+    trigger={<Button>Click here</Button>}
+    {...track}
+    withAddToQueue
+    withPlayNow
+    withAddToFavorites
+    withAddToDownloads
+  />
+</div>;
