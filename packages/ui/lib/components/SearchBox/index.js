6836082719a807397be4acd8337e34eb94833767
nukeop <nukeop@gumblert.tech>
2020-03-29 03:52:40 +0200

    Add keyboard operation and translation to search box

--- packages/ui/lib/components/SearchBox/index.js
+++ packages/ui/lib/components/SearchBox/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import _ from 'lodash';
 import { Icon, Dropdown } from 'semantic-ui-react';
-import { withHandlers } from 'recompose';
+import { compose, withHandlers, withState } from 'recompose';
 
 import common from '../../common.scss';
 import styles from './styles.scss';
@@ -15,34 +15,36 @@ const SearchBox = ({
   searchProviders,
   selectedSearchProvider,
   onSearchProviderSelect,
-  onChange
+  onChange,
+  onKeyDown
 }) => (
-  <div
-    className={cx(
-      common.nuclear,
-      styles.search_box_container
-    )}
-  >
-    <Icon name='search' disabled={disabled}/>
-    <input
-      autoFocus
-      placeholder={placeholder}
-      onChange={onChange}
-      disabled={disabled}
-      className={cx({ [styles.disabled]: disabled })}
-    />
-    { loading && <Icon name='spinner' loading /> }
-    {
-      !_.isNil(searchProviders) && !_.isEmpty(searchProviders) &&
-        <Dropdown 
+    <div
+      className={cx(
+        common.nuclear,
+        styles.search_box_container
+      )}
+    >
+      <Icon name='search' disabled={disabled} />
+      <input
+        autoFocus
+        placeholder={placeholder}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        disabled={disabled}
+        className={cx({ [styles.disabled]: disabled })}
+      />
+      {loading && <Icon name='spinner' loading />}
+      {
+        !_.isNil(searchProviders) && !_.isEmpty(searchProviders) &&
+        <Dropdown
           value={selectedSearchProvider.value}
           onChange={onSearchProviderSelect}
           options={searchProviders}
           disabled={disabled}
         />
-    }
-  </div>
-);
+      }
+    </div>
+  );
 
 const optionShape = PropTypes.shape({
   key: PropTypes.string,
@@ -57,15 +59,25 @@ SearchBox.propTypes = {
   searchProviders: PropTypes.arrayOf(optionShape),
   selectedSearchProvider: optionShape,
   onSearchProviderSelect: PropTypes.func,
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
+  onSearch: PropTypes.func,
+
+  setTerms: PropTypes.func,
+  terms: PropTypes.string
 };
 
-export default withHandlers(
-  {
-    onSearchProviderSelect: 
-    ({searchProviders, onSearchProviderSelect}) => 
-      (e, {value}) => 
+const RETURN_KEYCODE = 13;
+export default compose(
+  withState('terms', 'setTerms', ''),
+  withHandlers({
+  onSearchProviderSelect:
+    ({ searchProviders, onSearchProviderSelect }) =>
+      (e, { value }) =>
         onSearchProviderSelect(_.find(searchProviders, { value })),
-    onChange: ({ onChange }) => e => onChange(e.target.value)
-  }
+  onChange: ({ onChange, setTerms }) => e => {
+    setTerms(e.target.value);
+    onChange(e.target.value);
+  },
+  onKeyDown: ({ onSearch, terms }) => e => e.which === RETURN_KEYCODE && onSearch(terms)
+})
 )(SearchBox);
