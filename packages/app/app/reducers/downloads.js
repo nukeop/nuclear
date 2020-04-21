3fdf1aeb76a768efb527be9f3b288866a3c88f0c
Charles Jacquin <charles.jacquin@autistici.org>
2020-04-21 18:18:24 +0200

    refactor(ipc): move download ipc code in ipc middleware

--- packages/app/app/reducers/downloads.js
+++ packages/app/app/reducers/downloads.js
@@ -16,10 +16,11 @@ const initialState = [];
 export default function DownloadsReducer(state=initialState, action) {
   switch (action.type) {
   case ADD_TO_DOWNLOADS:
+  case DOWNLOAD_RESUMED:
+  case DOWNLOAD_PAUSED:
+    return [...action.payload.downloads];
   case READ_DOWNLOADS:
   case DOWNLOAD_STARTED:
-  case DOWNLOAD_PAUSED:
-  case DOWNLOAD_RESUMED:
   case DOWNLOAD_PROGRESS:
   case DOWNLOAD_FINISHED:
   case DOWNLOAD_ERROR:
--- packages/app/app/store/middlewares/ipc.js
+++ packages/app/app/store/middlewares/ipc.js
@@ -1,10 +1,13 @@
+import { getOption } from '@nuclear/core';
 import { ipcRenderer } from 'electron';
+import _ from 'lodash';
 
 import { PAUSE_PLAYBACK, START_PLAYBACK, UPDATE_VOLUME } from '../../actions/player';
 import { SCAN_LOCAL_FOLDER, REMOVE_LOCAL_FOLDER, UPDATE_LOCAL_FOLDERS, SCAN_LOCAL_FOLDER_SUCCESS } from '../../actions/local';
 import { ADD_QUEUE_ITEM, CLEAR_QUEUE, REMOVE_QUEUE_ITEM } from '../../actions/queue';
 import { SET_BOOLEAN_OPTION } from '../../actions/settings';
 import { CHANGE_CONNECTIVITY } from '../../actions/connectivity';
+import { ADD_TO_DOWNLOADS, DOWNLOAD_RESUMED, DOWNLOAD_PAUSED, DOWNLOAD_FINISHED, DOWNLOAD_ERROR } from '../../actions/downloads';
 
 const ipcConnect = () => next => {
   next({
@@ -68,6 +71,37 @@ const ipcConnect = () => next => {
     case CHANGE_CONNECTIVITY:
       ipcRenderer.send('connectivity', payload);
       break;
+
+    case ADD_TO_DOWNLOADS:
+    case DOWNLOAD_RESUMED: {
+      const {track} =_.find(payload.downloads, (item) => item.track.uuid === payload.track);
+
+      let maxDownloads;
+      try {
+        maxDownloads=parseInt(getOption('max.downloads'));
+      } catch (err){
+        maxDownloads=1;
+      }
+      if (payload.downloads.filter(({status}) => status==='Started' || status === 'Waiting').length > maxDownloads) {
+        break;
+      }
+      ipcRenderer.send('start-download', track);
+      break;
+    }
+    case DOWNLOAD_PAUSED: {
+      const {track} =_.find(payload.downloads, (item) => item.track.uuid === payload.track);
+
+      ipcRenderer.send('pause-download', track);
+      break;
+    }
+    case DOWNLOAD_FINISHED:
+    case DOWNLOAD_ERROR: {
+      const nextDownload = payload.find((download) =>
+        download.status==='Waiting'
+      );
+      nextDownload ? ipcRenderer.send('start-download', nextDownload.track) : null;
+      break;
+    }
     }
   
     next({ type, payload });
