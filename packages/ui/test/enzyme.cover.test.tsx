59c3594e2921ea59532005fdacc74e5558b7d3fb
nukeop <nukeop@gumblert.tech>
2020-07-23 02:43:05 +0200

    Streamline snapshot tests

--- packages/ui/test/enzyme.cover.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react'
-
-import { Cover } from '../lib';
-
-it('Cover snapshot', () => {
-  const { asFragment } = render(<Cover cover='https://i.imgur.com/4euOws2.jpg' />);
-  expect(asFragment()).toMatchSnapshot();
-});
