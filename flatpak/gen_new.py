639604b891d0236f4fc8c8d9b1259b7fbccf177b
advaithm <advaith.madhukar@gmail.com>
2020-08-01 16:47:03 +0530

    reduced memory usage
    still haven't fixed the quirk

--- flatpak/gen_new.py
+++ flatpak/gen_new.py
@@ -1,29 +1,30 @@
+#!/usr/bin/python3.8
 import json
 import argparse
 
 
 def reconstruct(url):
+    # opens file with op handler in read mode
     with open("org.js.nuclear.Nuclear.json") as f:
         print("loading data")
+        # loading json as dict
         data = json.load(f)
+        # closing the file handler b/c it looks the file
         f.close()
-        print("extracting data")
-        extracted_data = data["modules"]
-        x = extracted_data[0]
-        downloads = x["sources"]
-        sources = downloads[0]
-        print("extracted data complete\nmodifying data")
-        sources["url"] = url
-        downloads[0] = sources
-        x["sources"] = downloads
-        extracted_data[0] = x
-        data["modules"] = extracted_data
-        print("modified data")
-
+        print("Modifying data")
+        # updates the url value. the wierd formating is due to python loading the file
+        # using python data types
+        data["modules"][0]["sources"][0]["url"] = url
+        print("done")
+    # opens file in write mode
     with open("org.js.nuclear.Nuclear.json", "w") as f:
-        print("writing data")
+        print("updating json")
+        # formates data with some pretty printing
         formated_data = json.dumps(data, indent=4, sort_keys=True)
+        # writes data to the file
         f.writelines(formated_data)
+        # close file handler b/c best pratices
+        f.close()
         print("done")
 
 
