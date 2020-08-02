825771c028b7cf62a06cdbce64c161669c66208d
nukeop <nukeop@gumblert.tech>
2020-08-03 01:47:48 +0200

    Fix types in hook

--- packages/app/app/containers/PlayerBarContainer/hooks.ts
+++ packages/app/app/containers/PlayerBarContainer/hooks.ts
@@ -17,6 +17,7 @@ import { queue as queueSelector } from '../../selectors/queue';
 import { settingsSelector } from '../../selectors/settings';
 import { getFavoriteTrack } from '../../selectors/favorites';
 import { useCallback } from 'react';
+import { SemanticICONS } from 'semantic-ui-react/dist/commonjs/generic';
 
 export const useSeekbarProps = () => {
   const dispatch = useDispatch();
@@ -157,25 +158,25 @@ export const useVolumeControlsProps = () => {
   const playOptions = [
     {
       name: t('loop'),
-      icon: 'repeat',
+      icon: 'repeat' as SemanticICONS,
       enabled: _.get(settings, 'loopAfterQueueEnd'),
       onToggle: useToggleOptionCallback(toggleOption, 'loopAfterQueueEnd', settings)
     },
     {
       name: t('shuffle'),
-      icon: 'random',
+      icon: 'random' as SemanticICONS,
       enabled: _.get(settings, 'shuffleQueue'),
       onToggle: useToggleOptionCallback(toggleOption, 'shuffleQueue', settings)
     },
     {
       name: t('autoradio'),
-      icon: 'magic',
+      icon: 'magic' as SemanticICONS,
       enabled: _.get(settings, 'autoradio'),
       onToggle: useToggleOptionCallback(toggleOption, 'autoradio', settings)
     },
     {
       name: t('mini-player'),
-      icon: 'compress',
+      icon: 'compress' as SemanticICONS,
       enabled: _.get(settings, 'miniPlayer'),
       onToggle: useToggleOptionCallback(toggleOption, 'miniPlayer', settings)
     }
