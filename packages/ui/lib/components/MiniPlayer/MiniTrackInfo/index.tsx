5208abad101b84caec1242e17cc8f519fe7831a5
nukeop <nukeop@gumblert.tech>
2020-08-02 02:18:54 +0200

    Play options for mini player

--- packages/ui/lib/components/MiniPlayer/MiniTrackInfo/index.tsx
+++ packages/ui/lib/components/MiniPlayer/MiniTrackInfo/index.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 
 import styles from './styles.scss';
 import artPlaceholder from '../../../../resources/media/art_placeholder.png';
 
-export type MiniTrackInfoProps = {
-  cover?: string;
-  track?: string;
-  artist?: string;
-  isFavorite?: boolean;
-};
+import { TrackInfoProps } from '../../TrackInfo';
+
+export type MiniTrackInfoProps = Pick<TrackInfoProps, 'cover' | 'track' | 'artist' | 'isFavorite' | 'addToFavorites' | 'removeFromFavorites'>;
 
 const MiniTrackInfo: React.FC<MiniTrackInfoProps> = ({
   cover = artPlaceholder as unknown as string,
   track,
   artist,
-  isFavorite
+  isFavorite,
+  addToFavorites,
+  removeFromFavorites
 }) => (
     <div className={styles.mini_track_info}>
       <div className={styles.mini_cover}>
         <img src={cover} />
       </div>
-      <div className={styles.mini_track}>{track}</div>
-      <div className={styles.mini_artist}>{artist}</div>
+      <div className={styles.mini_track_info_text}>
+        <div className={styles.left}>
+          <div className={styles.mini_track}>{track}</div>
+          <div className={styles.mini_artist}>{artist}</div>
+        </div>
+        <div className={styles.right}>
+          <Icon
+            name={
+              isFavorite ? 'heart' : 'heart outline'
+            }
+            size='large'
+            onClick={
+              isFavorite
+                ? removeFromFavorites
+                : addToFavorites
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 
