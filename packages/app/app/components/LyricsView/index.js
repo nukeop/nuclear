25981abf9bad5f032dc5333de39b9a469e7c1ffe
pfeiffera <aurelien@pfeiffer@etu.unistra.fr>
2020-08-18 10:28:02 +0200

    Adding of a button to export lyrics into PDF file

--- packages/app/app/components/LyricsView/index.js
+++ packages/app/app/components/LyricsView/index.js
@@ -7,9 +7,15 @@ import LyricsHeader from './LyricsHeader';
 
 import styles from './styles.scss';
 
+///
+import { jsPDF } from 'jspdf';
+
+
 export class LyricsView extends React.Component {
   constructor(props) {
     super(props);
+	///
+	this.pdfToHTML = this.pdfToHTML.bind(this);
   }
 
   renderLyrics () {
@@ -18,21 +24,41 @@ export class LyricsView extends React.Component {
     lyricsStr = _.get(lyricsStr, 'type', '');
     if (lyricsStr === '') {
       lyricsStr = this.props.t('not-found');
-    }
-    return (
-      <div className={styles.lyrics_text}>
-        {lyricsStr}
-      </div>);
+	}
+	
+	//Changes by WildLeons
+	if(lyricsStr === this.props.t('not-found')) {
+		return (
+			<div className={styles.lyrics_text}>
+				{lyricsStr}
+			</div>
+		);
+	}
+	else{
+		return(
+		<>
+		<div className={styles.lyrics_text}>
+			{lyricsStr}
+		</div>
+		
+		<div className={styles.button_container}>
+		<a href='#' className={styles.export_button} onClick={this.pdfToHTML}>
+			{this.props.t('export-lyrics')}
+		</a>
+		</div>
+		</>
+		);
+	}
   }
 
   renderLyricsHeader () {
-    const track = this.props.track;
+	  const track = this.props.track;
     return (
       <LyricsHeader
         name={track.name}
         artist={track.artist}
       />
-    );
+	);
   }
 
   renderNoSelectedTrack () {
@@ -43,6 +69,37 @@ export class LyricsView extends React.Component {
         <div>{this.props.t('empty-help')}</div>
       </div>
     );
+  }
+  
+  //Addition of WildLeons
+  
+  pdfToHTML () {
+	  //Creation of a PDF file with portrait orientation and a measurement unit of millimeter
+	  let pdf = new jsPDF('p', 'mm');
+	  
+	  //Retrieve the lyrics only from the props.lyrics like in renderLyrics()
+	  let lyrics = _.get(this.props.lyrics, 'lyricsSearchResults', '');
+	  lyrics = _.get(lyrics, 'type', '');
+	  
+	  //Get the name of the song and its artist
+	  let name = this.props.track.name;
+	  let artist = this.props.track.artist;
+	  
+	  //Margins for shaping
+	  let margins = {
+		  top: 50,
+		  left: 50
+	  };
+	  
+	  //Coordinates for the positions from the top left of the document
+	  let x = 10;
+	  let y = 10;
+	  
+	  //Add the name, artist and lyrics to the file and save the file.
+	  pdf.text(name, x, y);
+	  pdf.text(artist, x, y+20);
+	  pdf.text(lyrics, x, y+50);
+	  pdf.save(name + "_" + artist +"_Lyrics.pdf");
   }
 
   render () {
@@ -61,7 +118,7 @@ export class LyricsView extends React.Component {
         {
           track !== null &&
             this.renderLyrics()
-        }
+        }		
       </div >
     );
   }
