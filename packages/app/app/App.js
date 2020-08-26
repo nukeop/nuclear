c4aa71b234ba966d5d953f1f06ec5f4479e4eb57
nukeop <nukeop@gumblert.tech>
2020-08-27 00:29:57 +0200

    Bump version number to 0.6.5

--- packages/app/app/App.js
+++ packages/app/app/App.js
@@ -157,7 +157,7 @@ class App extends React.PureComponent {
               src={this.props.settings.compactMenuBar ? logoIcon : logoImg}
             />
             <div className={styles.version_string}>
-              {this.props.settings.compactMenuBar ? '0.6.3' : 'Version 0.6.3'}
+              {this.props.settings.compactMenuBar ? '0.6.5' : 'Version 0.6.5'}
             </div>
           </div>
           <div className={styles.sidebar_menus}>
