2b429e0e66b994b9a1849ba9f7e2bbf0b8870405
nukeop <nukeop@gumblert.tech>
2021-01-05 01:07:16 +0100

    FIx flaky audius test

--- packages/core/test/audius.test.ts
+++ packages/core/test/audius.test.ts
@@ -11,11 +11,27 @@ mock('electron', electron);
 
 import { rest } from '../src';
 
+const hosts = [
+  'https://audius-discovery.nz.modulational.com/v1',
+  'https://audius-dp.johannesburg.creatorseed.com/v1',
+  'https://discoveryprovider.audius4.prod-us-west-2.staked.cloud/v1',
+  'https://disc-gru01.audius.hashbeam.com/v1',
+  'https://dn-jpn.audius.metadata.fyi/v1',
+  'https://audius-discovery-1.altego.net/v1',
+  'https://audius-metadata-1.figment.io/v1',
+  'https://discoveryprovider.audius1.prod-us-west-2.staked.cloud/v1',
+  'https://discoveryprovider.mumbaudius.com/v1',
+  'https://discoveryprovider.audius3.prod-us-west-2.staked.cloud/v1',
+  'https://discovery-au-01.audius.openplayer.org/v1',
+  'https://audius-disco.ams-x01.nl.supercache.org/v1',
+  'https://audius-metadata-2.figment.io/v1',
+  'https://discoveryprovider.audius5.prod-us-west-2.staked.cloud/v1',
+  'https://discoveryprovider.audius2.prod-us-west-2.staked.cloud/v1'
+];
 
 test('audius host is selected', async (t) => {
   const endpoint = await rest.Audius._findHost();
-  const gex = /^https:\/\/discoveryprovider[0-9]?/;
-  t.true(gex.test(endpoint));
+  t.true(hosts.includes(endpoint));
 });
 
 test('search artists', async t => {
