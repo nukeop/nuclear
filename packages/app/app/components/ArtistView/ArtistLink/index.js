29af269e62c4a45abbc8260e74fc33b798340111
yahya.kayani <yahyakiani1@gmail.com>
2021-02-10 23:55:23 +0500

    Localizing Label using localization api

--- packages/app/app/components/ArtistView/ArtistLink/index.js
+++ packages/app/app/components/ArtistView/ArtistLink/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import FontAwesome from 'react-fontawesome';
+import { useTranslation } from 'react-i18next';
 
-const shell = require('electron').shell;
+const ArtistLink = ({ artistLink, size, circular=false}) => {
 
-const ArtistLink = ({ artistLink, size, label='Visit', circular=false}) => (
-  <Button
+  const shell = require('electron').shell;
+  const { t } = useTranslation('artist');
+
+
+  return (<Button
     primary
     onClick={() => shell.openExternal(artistLink)}
     size={size}
     circular={circular}
     aria-label={'Visit Artist Profile'}
   >
-    <FontAwesome name='link' /> {label}
-  </Button>
-);
+    <FontAwesome name='link' /> {t('link')}
+  </Button>);
+};
 
 export default ArtistLink;
