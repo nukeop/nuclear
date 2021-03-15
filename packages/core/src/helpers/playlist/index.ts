1f371c46a273ee09ee0f20494bf29a0b461c64f7
Haidang666 <hdcompany123@gmail.com>
2021-03-15 14:33:33 +0700

    add: format playlist data before saving

---
+++ packages/core/src/helpers/playlist/index.ts
@@ -0,0 +1,71 @@
+import uuidv4 from 'uuid/v4';
+import {Playlist, PlaylistTrack, PlaylistTrackStream} from './types';
+
+const formatPlaylistForStored = (name: string, tracks: Array<any>, id: string = uuidv4()): Playlist => {
+  return {
+    name,
+    id,
+    tracks: formatTrackList(tracks)
+  };
+};
+
+const formatTrackList = (tracks): PlaylistTrack[] => {
+  const formattedTrack: PlaylistTrack[] = [];
+  if (tracks) {
+    tracks.forEach(track => {
+      const formattedData = extractTrackData(track);
+      if (formattedData) {
+        formattedTrack.push(formattedData);
+      }
+    });
+  }
+  
+  return formattedTrack;
+};
+
+const extractTrackData = (track): PlaylistTrack => {
+  return track && track.name ? 
+    {
+      artist: track.artist,
+      name: track.name,
+      album: track.album,
+      thumbnail: track.thumbnail,
+      uuid: track.uuid, 
+      streams: formatTrackStreamList(track.streams)
+    } : 
+    null;
+};
+
+const formatTrackStreamList = (streams): PlaylistTrackStream[] => {
+  const formattedStreams: PlaylistTrackStream[] = [];
+  if (streams) {
+    streams.forEach(stream => {
+      const formattedData = extractStreamData(stream);
+      if (formattedData) {
+        formattedStreams.push(formattedData);
+      }
+    });
+  }
+  
+  return formattedStreams;
+};
+
+const extractStreamData = (stream): PlaylistTrackStream => {
+  return stream && stream.source && stream.id ? 
+    {
+      source: stream.source,
+      id: stream.id,
+      duration: stream.duration,
+      title: stream.title,
+      thumbnail: stream.thumbnail
+    } : 
+    null;
+};
+
+export default {
+  formatPlaylistForStored, 
+  formatTrackList,
+  extractTrackData,
+  formatTrackStreamList,
+  extractStreamData
+};
---
+++ packages/core/src/helpers/playlist/types.ts
@@ -0,0 +1,22 @@
+export class Playlist {
+  name: string;
+  id: string;
+  tracks: PlaylistTrack[];
+}
+
+export class PlaylistTrack {
+  artist: string;
+  name: string;
+  album: string;
+  thumbnail: string;
+  uuid: string;
+  streams: PlaylistTrackStream[];
+}
+
+export class PlaylistTrackStream {
+  source: string;
+  id: string;
+  duration: number;
+  title: string;
+  thumbnail: string;
+}
