607c371e9826edd719c8ddf45ea632128d493e48
Haidang666 <hdcompany123@gmail.com>
2021-03-24 11:00:18 +0700

    refactor(bandcamp): set type in fetchAlbumDetails

--- packages/ui/lib/components/Card/index.tsx
+++ packages/ui/lib/components/Card/index.tsx
@@ -51,7 +51,7 @@ const Card: React.FC<CardProps> = ({
           >
             {
               _.isArray(menuEntries) && !_.isEmpty(menuEntries) &&
-              <Dropdown.Menu basic={true} inverted={true}>
+              <Dropdown.Menu basic='true' inverted='true'>
                 {
                   menuEntries.map((entry, i) => {
                     switch (entry.type) {
