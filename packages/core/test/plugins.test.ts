2a17989529fb0bfa0475b5ee7e9413f8eb512ada
nukeop <nukeop@gumblert.tech>
2021-04-04 21:02:34 +0200

    WIP

--- packages/core/test/plugins.test.ts
+++ packages/core/test/plugins.test.ts
@@ -1,41 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import test from 'ava';
 import path from 'path';
-import mock from 'mock-require';
-
-import MockStore from '../../../__mocks__/electron-store';
-import MockLogger from '../../../__mocks__/electron-timber';
-import * as electron from '../../../__mocks__/electron';
-
-mock('electron-store', MockStore);
-mock('electron-timber', MockLogger);
-mock('electron', electron);
 
 import { transformSource, transformPluginFile } from '../src';
 
-test('transform a simple plugin', async t => {
+it('transform a simple plugin', async () => {
   const result: any = await transformPluginFile(path.resolve(__dirname, 'plugins/simple.js'));
-  eval(result.code);
-  t.pass();
+  expect(() => eval(result.code)).not.toThrow();
 });
 
-test('throw an error when a nonexistent path is provided', async t => {
-  await t.throwsAsync(transformPluginFile('test'));
+it('throw an error when a nonexistent path is provided', async () => {
+  expect(transformPluginFile('test'));
 });
 
-test('transform some code', async t => {
+it('transform some code', async () => {
   const result: any = await transformSource('const f = (a,b) => a+b; const test = f(2,3); export default test;');
-  t.is(eval(result.code), 5);
+  expect(eval(result.code)).toEqual(5);
 });
 
-test('throw an error on invalid code', async t => {
-  await t.throwsAsync(transformSource('()asd'));
+it('throw an error on invalid code', async () => {
+  expect(() => transformSource('()asd')).toThrow();
 });
 
-test('transform jsx', async t => {
+it('transform jsx', async () => {
   const result: any = await transformSource(
     'import React from \'react\'; export const component = () => <div />;'
   );
-  eval(result.code);
-  t.pass();
+  expect(() => eval(result.code)).not.toThrow();
 });
