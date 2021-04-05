3b8c3e2c11a2e2d0cb69801cde93d23ff45d37dc
nukeop <nukeop@gumblert.tech>
2021-04-05 17:08:09 +0200

    Mock fetch in audius tests

--- packages/core/src/rest/audius.test.ts
+++ packages/core/src/rest/audius.test.ts
@@ -1,26 +1,42 @@
+import _ from 'lodash';
 import { rest } from '..';
 
+const mockFetch = (data) => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: jest.fn(() => ({
+        data
+      }))
+    })
+  ) as any;
+};
+
+
 describe('Audius tests', () => {
+  beforeEach(() => {
+    _.invoke(fetch, 'resetMocks');
+  });
+
   it('audius host is selected', async () => {
     const endpoint = await rest.Audius._findHost();
     expect(endpoint.length > 0).toBe(true);
   });
-  
+
   it('search artists', async () => {
+    mockFetch('test');
     const endpoint = await rest.Audius._findHost();
+    mockFetch([{ test: 'test data' }]);
     const response = await rest.Audius.artistSearch(endpoint, 'roto');
     const json = await response.json();
-    expect(typeof json).toBe('object');
-    expect(json.data instanceof Array).toBe(true);
-    expect(json.data.length > 0).toBe(true);
+    expect(json.data).toEqual([{ test: 'test data'}]);
   });
-  
+
   it('search tracks', async () => {
+    mockFetch('test');
     const endpoint = await rest.Audius._findHost();
+    mockFetch([{ test: 'test data' }]);
     const response = await rest.Audius.trackSearch(endpoint, 'roto');
     const json = await response.json();
-    expect(typeof json).toBe('object');
-    expect(json.data instanceof Array).toBe(true);
-    expect(json.data.length > 0).toBe(true);
-  });  
+    expect(json.data).toEqual([{ test: 'test data'}]);
+  });
 });
