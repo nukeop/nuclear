15ad6c59679b0c19188ff7a28fcb11ecc4abdcd8
Haidang666 <hdcompany123@gmail.com>
2021-04-08 15:59:02 +0700

    feat: new Playlists view

---
+++ packages/app/app/components/Playlists/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Icon, Segment } from 'semantic-ui-react';
+
+import PlaylistItem from './PlaylistItem';
+import PlaylistsHeader from './PlaylistsHeader';
+import { Playlist } from '@nuclear/core/src/helpers/playlist/types';
+import styles from './styles.scss';
+
+const EmptyState = () => {
+  const { t } = useTranslation('playlists');
+  return (
+    <div className={styles.empty_state}>
+      <Icon.Group>
+        <Icon name='list alternate outline' />
+      </Icon.Group>
+      <h2>{t('empty')}</h2>
+      <div>{t('empty-help')}</div>
+    </div>
+  );
+};
+
+type PlaylistsProps = {
+  playlists: Playlist[];
+}
+
+const Playlists: React.FC<PlaylistsProps> = ({ playlists }) => {
+  
+  function isPlaylistsReallyEmpty() {
+    return (
+      !playlists || Object.keys(playlists).length === 0 || playlists.length === 0
+    );
+  }
+
+  function isPlaylistsReallyNotEmpty() {
+    return playlists && playlists.length > 0;
+  }
+
+  return (
+    <div className={styles.playlists_container}>
+      <PlaylistsHeader 
+        showText={isPlaylistsReallyNotEmpty()}
+      />
+      {
+        isPlaylistsReallyEmpty() && 
+        <EmptyState />
+      }
+      
+      {
+        isPlaylistsReallyNotEmpty() &&  
+          <Segment className={styles.playlists_segment}>
+            {playlists.map((playlist, i) => {
+              return (
+                <PlaylistItem
+                  playlist={playlist}
+                  index={i}
+                  key={i}
+                />
+              );
+            })}
+          </Segment>    
+      }
+      
+    </div>
+  );
+};
+
+export default Playlists;
---
+++ packages/app/app/containers/PlaylistsContainer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Playlists from '../../components/Playlists';
+import { usePlaylistsProps } from './hooks';
+
+const PlaylistsContainer: React.FC = () => {
+  const props = usePlaylistsProps();
+  
+  return <Playlists
+    {...props}
+  />;
+};
+
+export default PlaylistsContainer;
