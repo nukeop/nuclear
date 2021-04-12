964213f667329bba04fb9c737c72079b8f85d00a
Jghaddad123 <51768855+Jghaddad123@users.noreply.github.com>
2021-04-11 19:55:24 -0700

    Update sleeptimer.js

--- packages/app/app/containers/SoundContainer/sleeptimer.js
+++ packages/app/app/containers/SoundContainer/sleeptimer.js
@@ -7,7 +7,7 @@ export function putToSleep (callProps) {
   const PERMITTEDDIFFERENCE= 5; 
 
   if (sleeptimerTimeToSleep !== props.settings.sleeptime){                      // sleep time setting was changed
-    sleeptimerSleepDate = 'undefined';
+    sleeptimerSleepDate = undefined;
   }
 
   if (sleeptimerSleepDate === 'undefined'){                                    // set sleeptimerSleepDate to pause time
@@ -16,14 +16,14 @@ export function putToSleep (callProps) {
   } else {                                                     
     if (new Date(((new Date().getTime()) - sleeptimerTimeToSleep) -sleeptimerSleepDate) >= PERMITTEDDIFFERENCE*60*1000){ // this will insure that sleeptimer was not turned off then back on at a later date and instantly pause the song 
       // (it will make sure it is turned off within PERMITTEDDIFFERENCE minutes of sleepdate being surpassed)
-      sleeptimerSleepDate = 'undefined';
+      sleeptimerSleepDate = undefined;
       return;
     }
         
     // sleeptimerSleepDate exists and is accurate so grab currentdate and compare 
     const currentDate = new Date(new Date().getTime());
     if (sleeptimerSleepDate < currentDate){                                  // if sleeptimerSleepDate passed then pause song and set sleeptimerSleepDate to undefine to reset sleeptimer
-      sleeptimerSleepDate = 'undefined';
+      sleeptimerSleepDate = undefined;
       props.settings.sleeptimerSwitch = false;
       props.actions.pausePlayback();
     }
