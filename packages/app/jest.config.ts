114968100b212aa6df7d74ce476525f82ca6090c
nukeop <nukeop@gumblert.tech>
2021-04-18 23:37:13 +0200

    Update typings location

--- packages/app/jest.config.ts
+++ packages/app/jest.config.ts
@@ -1,8 +1,7 @@
 
 export default async () => ({
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'ts-jest'
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest'
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
