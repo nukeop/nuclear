ae8133370d82460400c7098dbeef284cd4025af2
nukeop <nukeop@gumblert.tech>
2021-04-18 20:38:35 +0200

    Fix jest config for hoisted dependencies

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,7 +1,9 @@
-
 export default async () => ({
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^react$': '<rootDir>/../../node_modules/react'
   }
 });
