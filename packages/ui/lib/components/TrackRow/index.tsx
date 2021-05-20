2bc21a019809be271fb8408d24dfa8d9cba2055c
nukeop <nukeop@gumblert.tech>
2021-05-20 02:27:25 +0200

    Track table component

--- packages/ui/lib/components/TrackRow/index.tsx
+++ packages/ui/lib/components/TrackRow/index.tsx
@@ -8,6 +8,7 @@ import artPlaceholder from '../../../resources/media/art_placeholder.png';
 // eslint-disable-next-line node/no-missing-import
 import { formatDuration } from '../../utils';
 import styles from './styles.scss';
+import { Track } from '../../types';
 
 const getTrackThumbnail = track => {
   return _.get(
@@ -18,16 +19,7 @@ const getTrackThumbnail = track => {
 };
 
 export type TrackRowProps = {
-  track: {
-    local?: boolean;
-    album?: string;
-    artist?: {name: string} | string;
-    duration?: number | string;
-    position?: number;
-    playcount?: number | string;
-    name: string;
-    thumbnail?: any;
-  };
+  track: Track;
 
   mini?: boolean;
 
@@ -101,7 +93,7 @@ const TrackRow: React.FC<TrackRowProps> = ({
       </td>
     }
     <td className={styles.track_row_name}>
-      { track.name }
+      { track.title ?? track.name }
     </td>
     {
       displayAlbum &&
