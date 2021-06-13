8939182324b23aabfa007e7c10e2c22f213c3c6e
nukeop <nukeop@gumblert.tech>
2021-06-14 00:55:39 +0200

    v0.6.14

--- packages/app/app/App.js
+++ packages/app/app/App.js
@@ -157,7 +157,7 @@ class App extends React.PureComponent {
               src={this.props.settings.compactMenuBar ? logoIcon : logoImg}
             />
             <div className={styles.version_string}>
-              {this.props.settings.compactMenuBar ? '0.6.13' : 'Version 0.6.13'}
+              {this.props.settings.compactMenuBar ? '0.6.14' : 'Version 0.6.14'}
             </div>
           </div>
           <div className={styles.sidebar_menus}>
