b6c7eb24310997f3bec3074b25da969b00c6a850
Haidang666 <hdcompany123@gmail.com>
2021-06-23 08:40:06 +0700

    refactor(artist grid): use Hook instead of compose

--- packages/ui/lib/components/ArtistGrid/index.tsx
+++ packages/ui/lib/components/ArtistGrid/index.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import cx from 'classnames';
 import _ from 'lodash';
 import { Dimmer, Loader } from 'semantic-ui-react';
-import { compose, withState, withHandlers } from 'recompose';
 
 import { getThumbnail } from '../../utils';
 import Card from '../Card';
 import common from '../../common.scss';
 import styles from './styles.scss';
 
-const ArtistGrid = ({
+type ArtistGridProps = {
+  artists: {
+    name: string
+  } [];
+  removeFavoriteArtist: (artist) => void;
+  onArtistClick: (artist) => void
+  loading: boolean;
+  autoSize: boolean;
+}
+
+export const ArtistGrid: React.FC<ArtistGridProps> = ({
   artists,
   onArtistClick,
   removeFavoriteArtist,
   loading,
   autoSize
-}) => (
-  <div className={cx(
-    common.nuclear,
-    styles.artist_grid,
-    { [styles.loading]: loading },
-    { [styles.auto_size]: autoSize && !loading }
-  )} >
-    <div className={styles.artist_cards}>
-      {
-        !loading &&
+}) => {
+
+  return (
+    <div className={cx(
+      common.nuclear,
+      styles.artist_grid,
+      { [styles.loading]: loading },
+      { [styles.auto_size]: autoSize && !loading }
+    )} >
+      <div className={styles.artist_cards}>
+        {
+          !loading &&
           !_.isEmpty(artists) &&
           artists.map((artist, i) => (
             <Card
@@ -44,29 +54,12 @@ const ArtistGrid = ({
               ]}
             />
           ))
-      }
+        }
+      </div>
+
+      {loading && <Dimmer active><Loader /></Dimmer>}
     </div>
-
-    {loading && <Dimmer active><Loader /></Dimmer>}
-  </div>
-);
-
-ArtistGrid.propTypes = {
-  artists: PropTypes.array,
-  streamProviders: PropTypes.array, // eslint-disable-line react/no-unused-prop-types
-  onArtistClick: PropTypes.func,
-  removeFavoriteArtist: PropTypes.func,
-  loading: PropTypes.bool,
-  autoSize: PropTypes.bool
+  );
 };
 
-export default compose(
-  withState(
-    'selectedArtist',
-    'selectArtist',
-    ({ artists }) => _.head(artists)
-  ),
-  withHandlers({
-    onArtistClick: ({ onArtistClick, selectArtist }) => artist => _.isNil(onArtistClick) ? selectArtist(artist) : onArtistClick(artist)
-  })
-)(ArtistGrid);
+export default ArtistGrid;
