c08c69c4512b41d66bd4d7e98ea732f4b3fe5b4a
ere <celisacosta.3r3@gmail.com>
2021-07-05 18:17:48 -0500

    fix more conflicts

--- packages/app/app/containers/LyricsContainer/index.tsx
+++ packages/app/app/containers/LyricsContainer/index.tsx
@@ -8,15 +8,11 @@ import { queue as queueSelector } from '../../selectors/queue';
 
 const LyricsContainer = () => {
   const lyricsSearchResults = useSelector(lyricsSelectors.lyricsSearchResults);
-  // eslint-disable-next-line no-console
-  console.log(lyricsSearchResults);
   const queue = useSelector(queueSelector);
   const track = _.get(
     queue.queueItems,
     queue.currentSong
   );
-  // eslint-disable-next-line no-console
-  console.log(track, queue);
 
   return <LyricsView
     showHeader={true}
