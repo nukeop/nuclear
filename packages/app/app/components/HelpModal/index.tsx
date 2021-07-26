d61186192385409bbd027f9d923050969d6d9a94
Felix <hdcompany123@gmail.com>
2021-07-27 04:43:36 +0700

    Convert HelpModal to ts (#1013)
    * feat: HelpModal to ts
    
    * Move functions to containter

--- packages/app/app/components/HelpModal/index.tsx
+++ packages/app/app/components/HelpModal/index.tsx
@@ -1,12 +1,9 @@
 import React, { useState, useCallback } from 'react';
-import electron from 'electron';
 import cx from 'classnames';
 import { Button, Header, Image, Modal, Icon } from 'semantic-ui-react';
 import { useTranslation } from 'react-i18next';
 
-import { withHandlers, compose } from 'recompose';
-
-import Contributors from './Contributors';
+import Contributors, { ContributorState } from './Contributors';
 
 import HelpButton from '../HelpButton';
 import { agplDisclaimer } from './const';
@@ -15,19 +12,32 @@ import logoImg from '../../../resources/media/logo_full_light.png';
 import mastodonImg from '../../../resources/media/mastodon.svg';
 import styles from './styles.scss';
 
-const HelpModal = ({
-  handleAuthorClick,
+export type HelpModalProps = {
+  githubContrib: ContributorState;
+  handleOpenExternalLink: (link:string) => void;
+  handleMastodonClick: () => void;
+  handleGithubClick: () => void;
+  handleTwitterClick: () => void;
+  handleAuthorClick: () => void;
+  handleDiscordClick: () => void;
+  handleReportIssueClick: () => void;
+}
+
+const HelpModal: React.FC<HelpModalProps> = ({
+  githubContrib,
+  handleOpenExternalLink,
+  handleMastodonClick,
   handleGithubClick,
   handleTwitterClick,
-  handleMastodonClick,
+  handleAuthorClick,
   handleDiscordClick,
-  handleReportIssueClick,
-  githubContrib
+  handleReportIssueClick
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const handleOpen = useCallback(() => setIsOpen(true), []);
   const handleClose = useCallback(() => setIsOpen(false), []);
   const { t } = useTranslation('help');
+
   return (
     <Modal
       open={isOpen}
@@ -42,17 +52,20 @@ const HelpModal = ({
       <Modal.Content image>
         <Image wrapped size='medium' src={logoImg} />
         <Modal.Description>
-          <Header inverted>
-            {t('header')}
-          </Header>
+          <Header inverted>{t('header')}</Header>
           <p>
-            Copyright © <a href='#' data-testid='author-link' onClick={handleAuthorClick}>nukeop</a>
+            Copyright ©{' '}
+            <a
+              href='#'
+              data-testid='author-link'
+              onClick={handleAuthorClick}
+            >
+              nukeop
+            </a>
             {` ${new Date().getFullYear()}, `}
             {t('released')}
           </p>
-          <p>
-            {t('thanks')}
-          </p>
+          <p>{t('thanks')}</p>
           <Button
             data-testid='issue-link'
             inverted
@@ -66,34 +79,48 @@ const HelpModal = ({
       </Modal.Content>
       <Modal.Content>
         <div className={styles.social_icons}>
-          <Icon data-testid='twitter-link' link size='big' name='twitter' onClick={handleTwitterClick} />
+          <Icon
+            data-testid='twitter-link'
+            link
+            size='big'
+            name='twitter'
+            onClick={handleTwitterClick}
+          />
           <div
             data-testid='mastodon-link'
-            className={cx(styles.mastodon, styles.icon)} 
+            className={cx(styles.mastodon, styles.icon)}
             onClick={handleMastodonClick}
             dangerouslySetInnerHTML={{ __html: mastodonImg }}
           />
-          <Icon data-testid='discord-link' link size='big' name='discord' onClick={handleDiscordClick} />
-          <Icon data-testid='github-link'link size='big' name='github' onClick={handleGithubClick} />
+          <Icon
+            data-testid='discord-link'
+            link
+            size='big'
+            name='discord'
+            onClick={handleDiscordClick}
+          />
+          <Icon
+            data-testid='github-link'
+            link
+            size='big'
+            name='github'
+            onClick={handleGithubClick}
+          />
         </div>
       </Modal.Content>
       <Modal.Content>
         <div className={styles.contributors}>
-          <Header className={styles.contributors_header}>Our top 10 Contributors</Header>
-          <Contributors handleGithubClick={handleGithubClick} {...githubContrib} />
+          <Header className={styles.contributors_header}>
+            Our top 10 Contributors
+          </Header>
+          <Contributors
+            {...githubContrib}
+            handleOpenExternalLink={handleOpenExternalLink}
+          />
         </div>
       </Modal.Content>
     </Modal>
   );
 };
 
-export default compose(
-  withHandlers({
-    handleMastodonClick: () => () => electron.shell.openExternal('https://mstdn.io/@nuclear'),
-    handleGithubClick: () => () => electron.shell.openExternal('https://github.com/nukeop/nuclear'),
-    handleTwitterClick: () => () => electron.shell.openExternal('https://twitter.com/nuclear_player'),
-    handleAuthorClick: () => () => electron.shell.openExternal('https://github.com/nukeop'),
-    handleDiscordClick: () => () => electron.shell.openExternal('https://discord.gg/JqPjKxE'),
-    handleReportIssueClick: () => () => electron.shell.openExternal('https://github.com/nukeop/nuclear/issues/new?assignees=&labels=bug&template=bug_report.md&title=')
-  })
-)(HelpModal);
+export default HelpModal;
---
+++ packages/app/app/containers/HelpModalContainer/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import electron from 'electron';
+
+import HelpModal from '../../components/HelpModal';
+import { ContributorState } from '../../components/HelpModal/Contributors';
+import { githubContribSelectors } from '../../selectors/githubContrib';
+
+const HelpModalContainer: React.FC<{}> = () => {
+
+  const githubContrib: ContributorState = {
+    contributors: useSelector(githubContribSelectors.contributors),
+    loading: useSelector(githubContribSelectors.loading),
+    error: useSelector(githubContribSelectors.error)
+  };
+
+  const handleOpenExternalLink = (link: string) => link && link.length && electron.shell.openExternal(link);
+  const handleMastodonClick = () => handleOpenExternalLink('https://mstdn.io/@nuclear');
+  const handleGithubClick = () => handleOpenExternalLink('https://github.com/nukeop/nuclear');
+  const handleTwitterClick = () => handleOpenExternalLink('https://twitter.com/nuclear_player');
+  const handleAuthorClick = () => handleOpenExternalLink('https://github.com/nukeop');
+  const handleDiscordClick = () => handleOpenExternalLink('https://discord.gg/JqPjKxE');
+  const handleReportIssueClick = () => handleOpenExternalLink(
+    'https://github.com/nukeop/nuclear/issues/new?assignees=&labels=bug&template=bug_report.md&title='
+  );
+
+  return (<HelpModal 
+    githubContrib={githubContrib}
+    handleOpenExternalLink={handleOpenExternalLink}
+    handleMastodonClick={handleMastodonClick}
+    handleGithubClick={handleGithubClick}
+    handleTwitterClick={handleTwitterClick}
+    handleAuthorClick={handleAuthorClick}
+    handleDiscordClick={handleDiscordClick}
+    handleReportIssueClick={handleReportIssueClick}
+  />);
+};
+
+export default HelpModalContainer;
