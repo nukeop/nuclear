ea9244cbcc9691e899e5adba356cc6a0bca7b2e8
Hardik <55993168+Hardik-hi@users.noreply.github.com>
2021-08-30 16:36:45 +0530

    Typed component and props declaration
    Made the component of React.FC type and props of FooterProps type

--- packages/app/app/components/Footer/index.tsx
+++ packages/app/app/components/Footer/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Footer = ({ className, children }) => (
-  <div className={className}>{children}</div>
-);
+//type declaration as [component name]Props
+type FooterProps={
+    className: string;
+};
+
+//component is of type React.FC
+const Footer: React.FC<FooterProps> = ({className,children})=>{
+  return (
+    <div className={className}>{children}</div>
+  );
+};
 
 export default Footer;
