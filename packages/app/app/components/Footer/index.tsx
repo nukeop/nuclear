dd3b4003c9cb37aef12aea66fdd707b3c7f63846
Hardik <55993168+Hardik-hi@users.noreply.github.com>
2021-09-11 16:50:27 +0530

    Converting to TypeScript
    Fixed linter issues

--- packages/app/app/components/Footer/index.tsx
+++ packages/app/app/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-//type declaration as [component name]Props
+//  type declaration as [component name]Props
 type FooterProps={
     className: string;
 };
 
-//component is of type React.FC
-const Footer: React.FC<FooterProps> = ({className,children})=>{
+//  component is of type React.FC
+const Footer: React.FC<FooterProps> = ({className, children}) => {
   return (
     <div className={className}>{children}</div>
   );
