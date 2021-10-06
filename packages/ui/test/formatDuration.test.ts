8d3b5ee4eb2ef2ae952629f6a3a2369d089f9274
Jacob Reiser <jreiser1@msudenver.edu>
2021-10-06 16:07:36 -0600

    adding unit tests for formatDuration optional parameter

--- packages/ui/test/formatDuration.test.ts
+++ packages/ui/test/formatDuration.test.ts
@@ -38,3 +38,17 @@ it('Should format seconds', () => {
   expect(formatDuration(30)).toBe('00:30');
   expect(formatDuration(11)).toBe('00:11');
 });
+
+it('Should return a timestamp if the optional parameter is false', () => {
+  expect(formatDuration(-30, false)).toBe('00:00');
+  expect(formatDuration(-3600, false)).toBe('00:00');
+  expect(formatDuration(1827, false)).toBe('30:27');
+  expect(formatDuration(7200, false)).toBe('02:00:00');
+});
+
+it('Should return LIVE if the optional parameter is true', () => {
+  expect(formatDuration(-30, true)).toBe('LIVE');
+  expect(formatDuration(-3600, true)).toBe('LIVE');
+  expect(formatDuration(1827, true)).toBe('LIVE');
+  expect(formatDuration(7200, true)).toBe('LIVE');
+});
