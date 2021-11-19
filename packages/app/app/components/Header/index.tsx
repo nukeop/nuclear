9f5e4bb473101230c2c15bafd3c7b27cfb8a399b
Yahya Kayani <yahyakiani1@gmail.com>
2021-11-19 10:51:20 +0500

    Converting to typescript

--- packages/app/app/components/Header/index.tsx
+++ packages/app/app/components/Header/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import styles from './styles.scss';
 
-const Header = ({ children }) => (
+type HeaderProps={
+  children: React.ReactNode;
+};
+
+const Header:React.FC<HeaderProps> = ({ children }) => (
   <div className={styles.header_container}>{children}</div>
 );
 
