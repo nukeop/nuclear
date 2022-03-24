2d015a71300a85a6b3de1a44f583d29a5800eb3d
nukeop <nukeop@gumblert.tech>
2022-03-24 02:18:18 +0100

    Update test dependencies

--- packages/app/jest.config.ts
+++ packages/app/jest.config.ts
@@ -7,5 +7,6 @@ export default async () => ({
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|scss)$': 'identity-obj-proxy',
     '^react$': '<rootDir>/../../node_modules/react'
-  }
+  },
+  testEnvironment: 'jsdom'
 });
