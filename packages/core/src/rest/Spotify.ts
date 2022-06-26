c3be71d0e484f079d7029ecc31fefd07e42e0060
nukeop <12746779+nukeop@users.noreply.github.com>
2022-06-26 21:51:36 +0200

    Use Spotify to fill in thumbnails

---
+++ packages/core/src/rest/Spotify.ts
@@ -0,0 +1,31 @@
+
+'https://open.spotify.com/get_access_token?reason=transport&productType=web_player';
+
+'https://api.spotify.com/v1/search?type=artist&q=Metallica&decorate_restrictions=false&best_match=true&include_external=audio&limit=1';
+
+const SPOTIFY_API_OPEN_URL = 'https://open.spotify.com';
+const SPOTIFY_API_URL = 'https://api.spotify.com/v1';
+
+type SpotifyArtist = {
+    images: {
+        height: number;
+        url: string;
+    }[];
+}
+
+export const getToken = async (): Promise<string> => {
+  const data = await (await fetch(`${SPOTIFY_API_OPEN_URL}/get_access_token?reason=transport&productType=web_player`)).json();
+
+  return data.accessToken as string;
+};
+
+export const searchArtists = async (token: string, query: string): Promise<SpotifyArtist> => {
+  const data = await (
+    await fetch(`${SPOTIFY_API_URL}/search?type=artist&q=${query}&decorate_restrictions=false&best_match=true&include_external=audio&limit=1`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }})
+  ).json();
+
+  return data.best_match.items[0];
+};
