56cdc653811971100d0d7e8f511aaeccf97d94be
nukeop <12746779+nukeop@users.noreply.github.com>
2022-10-26 23:58:47 +0200

    Fetch listening history and display it on frontend as a track table

--- packages/app/app/containers/ListeningHistoryContainer/index.tsx
+++ packages/app/app/containers/ListeningHistoryContainer/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { IpcEvents } from '@nuclear/core';
+import { ListeningHistoryEntry } from '@nuclear/main/src/services/listening-history/model/ListeningHistoryEntry';
+import { ipcRenderer } from 'electron';
+import React, { useEffect, useState } from 'react';
 import { ListeningHistoryView } from '../../components/ListeningHistoryView';
 
+type ListeningHistory = {
+  data: ListeningHistoryEntry[];
+  cursor: {
+    beforeCursor: string | null;
+    afterCursor: string | null;
+  }
+}
+
 export const ListeningHistoryContainer: React.FC = () => {
-    
-  return <ListeningHistoryView />;
+  const [entriesPerPage, setEntriesPerPage] = useState(10);
+  const [afterCursor, setAfterCursor] = useState(undefined);
+  const [listeningHistory, setListeningHistory] = useState<ListeningHistory>();
+
+  useEffect(() => {
+    ipcRenderer.invoke(IpcEvents.FETCH_LISTENING_HISTORY, { limit: entriesPerPage }).then(setListeningHistory);
+  }, [entriesPerPage, afterCursor]);
+
+  return <ListeningHistoryView tracks={listeningHistory?.data ?? []} />;
 };
