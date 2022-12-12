35a823aa8d89e0a9de4f0a8c88c2e765aaf41339
Derek Naing <62006976+dnaing@users.noreply.github.com>
2022-12-11 21:34:24 -0800

    Updated this thing
    Part 1 solution maybe

--- packages/main/src/services/store/index.ts
+++ packages/main/src/services/store/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { settingsConfig } from '@nuclear/core';
 import ElectronStore from 'electron-store';
 import { inject, injectable } from 'inversify';
@@ -6,6 +7,9 @@ import _ from 'lodash';
 
 import Logger, { $mainLogger } from '../logger';
 import Config from '../config';
+
+import * as yup from 'yup';
+
 
 /**
  * Wrapper around electron-store
@@ -18,6 +22,77 @@ class Store extends ElectronStore {
     @inject(Config) private config: Config
   ) {
     super();
+
+    // First thing we do is check if config is broken
+    // ===============================================
+
+    // Turn Config java object into JSON string (DONE?)
+    let jsonString = JSON.stringify(this.config);
+    jsonString = jsonString.replaceAll(':', 'LOL'); // Making our config json string invalid for testing purposes
+    console.log(jsonString);
+
+    // Try JSON.parse(json string) (DONE?)
+    try {
+      let jsonObject = JSON.parse(jsonString);
+      console.log(jsonObject);
+    }
+
+    // Catch error and reset config java object to default values (DONE?)
+    catch(error) {
+      console.log("BROKEN CONFIG");
+      // Reset config java object to default values (DONE?)
+      this.config.resetDefaultValues();
+    }
+
+    // Next we do versioning with defined schemas (Need yup module installed) 
+    // ================================================
+
+    // Create the schema for the Config (TODO)
+    // let schema = yup.object().shape({
+      // name: yup.string().required(),
+      // age: yup.number().required().positive().integer(),
+      // email: yup.string().email(),
+      // website: yup.string().url(),
+      // createdOn: yup.date().default(function () {
+      //   return new Date();
+      // }),
+
+      // acousticId: {
+      //   key: string;
+      //   url: string;
+      // };
+      // isConnected: boolean;
+      // youtubeUrl: yup.string().required(),
+      // youtubeSearch: string,
+      // title: string,
+      // appid: string,
+      // supportedFormats: string[],
+      // env: Env,
+      // icon: string,
+      // macIcon: string,
+      // discordClientId: string,
+      // defaultInvidiousUrl: string,
+      // thumbCleanInterval: number,
+      // localLibraryDbName: string,
+      // listeningHistoryDbName: string,
+    // });
+
+    // Validate our Config with the schema (TODO)
+
+    // check validity
+    // schema
+    // .isValid({
+    //   // name: 'jimmy',
+    //   // age: 24,
+    // })
+    // .then(function (valid) {
+    //   valid; // => true
+    // });
+
+
+    // If there are errors, we fix the parts of the config that don't comply with schema (TODO)
+    
+
 
     if (!this.getOption('invidious.url')) {
       this.setOption('invidious.url', this.config.defaultInvidiousUrl);
@@ -65,6 +140,8 @@ class Store extends ElectronStore {
   setLastThumbCleanDate(date: Date): void {
     this.set('last-thumb-clean-date', date.getTime());
   }
+
+
 }
 
 export default Store;
