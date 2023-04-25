cc513b1b8ba7639a9f50bba97c37fef7a801ecae
nukeop <12746779+nukeop@users.noreply.github.com>
2023-04-26 01:51:27 +0200

    Add a button to play a random track from favorites

--- packages/app/app/containers/PlayerBarContainer/hooks.ts
+++ packages/app/app/containers/PlayerBarContainer/hooks.ts
@@ -11,6 +11,7 @@ import * as playerActions from '../../actions/player';
 import * as queueActions from '../../actions/queue';
 import * as settingsActions from '../../actions/settings';
 import * as favoritesActions from '../../actions/favorites';
+import { favoritesSelectors } from '../../selectors/favorites';
 import { playerSelectors } from '../../selectors/player';
 import { queue as queueSelector } from '../../selectors/queue';
 import { settingsSelector } from '../../selectors/settings';
@@ -58,6 +59,7 @@ export const usePlayerControlsProps = () => {
   const currentTrackLoading: boolean = queue?.queueItems[queue.currentSong]?.loading;
   const playbackStreamLoading: boolean = useSelector(playerSelectors.playbackStreamLoading);
   const seek = useSelector(playerSelectors.seek);
+  const favoriteTracks = useSelector(favoritesSelectors.tracks);
 
   const couldPlay = queue.queueItems.length > 0;
   const couldForward = couldPlay && queue.currentSong + 1 < queue.queueItems.length;
@@ -84,7 +86,6 @@ export const usePlayerControlsProps = () => {
     },
     [dispatch, seek]
   );
-
 
   return {
     goBackDisabled: !couldBack,
