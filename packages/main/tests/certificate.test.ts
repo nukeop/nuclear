fd7916a526f2b9da27f84c7ca1556c01e3aac02f
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-06-12 10:39:18 -0500

    adding app.emit() to simplify test

--- packages/main/tests/certificate.test.ts
+++ packages/main/tests/certificate.test.ts
@@ -1,22 +1,30 @@
 // import handleCertificateError from main.ts
 // import { app } from 'electron';
 import { handleCertificateError } from '../src/main';
+import { app } from 'electron';
 
 
 jest.mock('electron', () => ({
   app: {
     on: jest.fn(),
+    emit: jest.fn(),
     getPath: jest.fn().mockReturnValue('/mocked/path') // Add this line
   }
+ 
 }));
 
-// Tests for the certificate error handler
 describe('handleCertificateError', () => {
-  it('calls preventDefault and callback with true', () => {
-    const preventDefault = jest.fn();
+  it('calls event.preventDefault and callback with true when certificate error event is emitted', () => {
+    const event = {
+      preventDefault: jest.fn()
+    };
     const callback = jest.fn();
-    handleCertificateError({ preventDefault }, null, null, null, null, callback);
-    expect(preventDefault).toHaveBeenCalled();
+
+    // Mock 'certificate-error' event
+    app.emit('certificate-error', event, {}, '', '', {}, callback);
+
+    expect(event.preventDefault).toHaveBeenCalledTimes(1);
     expect(callback).toHaveBeenCalledWith(true);
   });
 });
+
