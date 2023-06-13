b4fa650aada0c88b154a59f05fa4bb7cd4c2ed5c
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-06-12 20:52:49 -0500

    Cleaning up, removing app.emit()

--- packages/main/tests/certificate.test.ts
+++ packages/main/tests/certificate.test.ts
@@ -1,36 +1,9 @@
 import { handleCertificateError } from '../src/main';
-import { app } from 'electron';
 
-jest.mock('electron', () => ({
-  app: {
-    on: jest.fn(),
-    emit: jest.fn(),
-    getPath: jest.fn().mockReturnValue('/mocked/path'),
-    commandLine: {
-      appendSwitch: jest.fn()
-    },
-    requestSingleInstanceLock: jest.fn(),
-    quit: jest.fn()
-  },
-  TouchBar: {
-    TouchBarButton: jest.fn(),
-    TouchBarSlider: jest.fn()
-  },
-  nativeImage: {
-    createFromPath: jest.fn().mockReturnValue({})
-  }
-
-}));
-
+jest.mock('electron');
 jest.mock('@nuclear/core');
-
 jest.mock('electron-timber');
-
 jest.mock('electron-store');
-
-beforeEach(() => {
-  app.on('certificate-error', handleCertificateError);
-});
 
 describe('handleCertificateError', () => {
   it('calls event.preventDefault and callback with true when certificate error event is emitted', () => {
@@ -40,7 +13,7 @@ describe('handleCertificateError', () => {
     const callback = jest.fn();
 
     // Mock 'certificate-error' event
-    app.emit('certificate-error', event, {}, '', '', {}, callback);
+    handleCertificateError(event, {}, '', '', {}, callback);
 
     expect(event.preventDefault).toHaveBeenCalledTimes(1);
     expect(callback).toHaveBeenCalledWith(true);
