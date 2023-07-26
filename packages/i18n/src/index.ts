fbd1ebcfe1e09a2ef4c9a2f474a9aa981c44a04c
nukeop <12746779+nukeop@users.noreply.github.com>
2023-07-26 22:34:04 +0200

    Add new languages

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,5 +1,8 @@
 import i18n, { LanguageDetectorModule } from 'i18next';
 
+import ar_SA from './locales/ar_SA.json';
+import be_BY from './locales/be_BY.json';
+import bn_BD from './locales/bn_BD.json';
 import cs from './locales/cs.json';
 import de from './locales/de.json';
 import dk from './locales/dk.json';
@@ -23,16 +26,18 @@ import nl from './locales/nl.json';
 import no_NO from './locales/no_NO.json';
 import pl from './locales/pl.json';
 import pt_BR from './locales/pt_BR.json';
+import ro_RO from './locales/ro_RO.json';
 import ru from './locales/ru.json';
 import se from './locales/se.json';
 import sk from './locales/sk.json';
 import sq from './locales/sq.json';
 import tl from './locales/tl.json';
 import tr from './locales/tr.json';
+import uk_UA from './locales/uk_UA.json';
 import vi from './locales/vi.json';
 import yue_CN from './locales/yue_CN.json';
-import zh_TW from './locales/zh_TW.json';
 import zh_CN from './locales/zh_CN.json';
+import zh_TW from './locales/zh_TW.json';
 
 interface I18nOptions {
   languageDetector: LanguageDetectorModule;
@@ -44,6 +49,9 @@ export const setupI18n = ({ languageDetector, debug }: I18nOptions) => {
     fallbackLng: 'en',
     debug,
     resources: {
+      ar_SA,
+      be_BY,
+      bn_BD,
       cs,
       de,
       dk,
@@ -67,12 +75,14 @@ export const setupI18n = ({ languageDetector, debug }: I18nOptions) => {
       no_NO,
       pl,
       pt_BR,
+      ro_RO,
       ru,
       se,
       sk,
       sq,
       tl,
       tr,
+      uk_UA,
       vi,
       yue_CN,
       zh_TW,
