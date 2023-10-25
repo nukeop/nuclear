6c563385585500faa7dd16a7b648b9c242163724
nukeop <12746779+nukeop@users.noreply.github.com>
2023-10-26 00:46:06 +0200

    Remove recompose from AlbumListComponent

--- packages/app/app/components/AlbumList/index.js
+++ packages/app/app/components/AlbumList/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {compose, withHandlers} from 'recompose';
 import {AlbumGrid} from '@nuclear/ui';
 
-const AlbumListComponent = props => <AlbumGrid {...props} autoSize/>;
-export default compose(
-  withHandlers({
-    onAlbumClick: ({albumInfoSearch, history}) => (album) => {
-      albumInfoSearch(album.id, album.type, album);
-      history.push('/album/' + album.id);
-    }
-  })
-)(AlbumListComponent);
+const AlbumListComponent = props => {
+  const onAlbumClick = (album) => {
+    props.albumInfoSearch(album.id, album.type, album);
+    props.history.push('/album/' + album.id);
+  };
+
+  return <AlbumGrid {...props} onAlbumClick={onAlbumClick} autoSize />;
+};
+export default AlbumListComponent;
