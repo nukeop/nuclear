89bcaded14f5abe8a021554df3508787a795ee59
nukeop <12746779+nukeop@users.noreply.github.com>
2024-03-08 03:02:27 +0100

    Disable local library scanner test

--- packages/main/src/services/library-scanner/library-scanner.test.ts
+++ packages/main/src/services/library-scanner/library-scanner.test.ts
@@ -1,6 +1,7 @@
 import {scanFolders} from '@nuclear/scanner';
 
-describe('Local library scanner', () => {
+// Enable when testing the local library scanner rust package
+xdescribe('Local library scanner', () => {
   it('scans folders', async () => {
     const result = await scanFolders([''], ['mp3'], (progress, total, lastScanned) => {
       // console.log({progress, total, lastScanned});
