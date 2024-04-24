cbab5347a2e36c30429547ad149f9daca5f3e7b9
Blake Mischley <98132827+BMischley@users.noreply.github.com>
2024-04-24 10:55:59 -0400

    Updated to typescript and rename index.js to index.tsx

--- packages/app/app/components/SearchResults/TracksResults/index.tsx
+++ packages/app/app/components/SearchResults/TracksResults/index.tsx
@@ -2,17 +2,36 @@ import React from 'react';
 import _ from 'lodash';
 import { Segment } from 'semantic-ui-react';
 import { useTranslation } from 'react-i18next';
-
 import TrackRow from '../../TrackRow';
-
 import trackRowStyles from '../../TrackRow/styles.scss';
 
-const TracksResults = ({ tracks, limit }) => {
+type Artist = {
+  name?: string;
+}
+
+type Track = {
+  name?: string;
+  image?: string;
+  thumbnail?: string;
+  artist: Artist | string;
+}
+
+type TracksResultsProps = {
+  tracks?: Track[];
+  limit: number;
+}
+
+type TrackRowProps = {
+  track: Track;
+  displayArtist?: boolean;
+  displayCover?: boolean;
+}
+
+const TracksResults: React.FC<TracksResultsProps> = ({ tracks, limit }) => {
   const { t } = useTranslation('search');
   const collection = tracks || [];
-
   if (collection.length === 0) {
-    return t('empty');
+    return <>{t('empty')}</>;
   } else {
     return (
       <Segment className={trackRowStyles.tracks_container}>
@@ -21,20 +40,23 @@ const TracksResults = ({ tracks, limit }) => {
             {_.take(collection, limit).map((track, index) => {
               if (
                 track &&
-                _.hasIn(track, 'name') && (_.hasIn(track, 'image') || _.hasIn(track, 'thumbnail')) &&
+                _.hasIn(track, 'name') &&
+                (_.hasIn(track, 'image') || _.hasIn(track, 'thumbnail')) &&
                 _.hasIn(track, 'artist')
               ) {
                 const newTrack = _.cloneDeep(track);
-                if (!newTrack.artist.name) {
-                  _.set(newTrack, 'artist.name', newTrack.artist);
+                if (typeof newTrack.artist === 'string') {
+                  newTrack.artist = { name: newTrack.artist };
                 }
+                const trackRowProps: TrackRowProps = {
+                  track: newTrack,
+                  displayArtist: true,
+                  displayCover: true,
+                };
                 return (
                   <TrackRow
                     key={'search-result-track-row-' + index}
-                    track={newTrack}
-                    index={'popular-track-' + index}
-                    displayArtist
-                    displayCover
+                    {...trackRowProps}
                   />
                 );
               }
