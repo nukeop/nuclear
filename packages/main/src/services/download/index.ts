5befc0dc9ff43154f063efe445937a9cf675430d
Krishna zade <114551368+Krishcode264@users.noreply.github.com>
2024-07-07 11:33:10 +0530

    Fix bug in incorrect downloads

--- packages/main/src/services/download/index.ts
+++ packages/main/src/services/download/index.ts
@@ -4,7 +4,6 @@ import { inject, injectable } from 'inversify';
 import _ from 'lodash';
 import * as Youtube from '@nuclear/core/src/rest/Youtube';
 import { StreamQuery } from '@nuclear/core/src/plugins/plugins.types';
-
 import Store from '../store';
 import Config from '../config';
 import Window from '../window';
@@ -29,7 +28,6 @@ class Download {
   ) {
     registerDownloader();
   }
-
   /**
    * Download a son using Youtube
    */
@@ -39,34 +37,26 @@ class Download {
     onStarted,
     onProgress,
     onCompleted,
-    selectedStreamId
+    selectedStreamId,
   }: DownloadParams): Promise<any> {
-
-     
-  //  const videoWithStream = await Youtube.getStreamForId(tracks[0]?.id, undefined);
-    //const videoWithStream = await Youtube.getStreamForId(selectedStreamId, undefined);
-  //instaid of tracks[0].id we are using specific slectedstream id from user 
-
-     let videoWithStream:any;
-     if (selectedStreamId) {
-       videoWithStream = await Youtube.getStreamForId(
-         selectedStreamId,
-         undefined
-       );
-     } else {
-       const tracks = await Youtube.trackSearchByString(
-         query,
-         undefined,
-         false
-       );
-       videoWithStream = await Youtube.getStreamForId(tracks[0]?.id, undefined);
-     }
+    //  const videoWithStream = await Youtube.getStreamForId(tracks[0]?.id, undefined);
+    // instaid of tracks[0].id we are using specific slectedstream id ,its created on streamChange event in queueItems
+    let videoWithStream: any;
+    if (selectedStreamId) {
+      videoWithStream = await Youtube.getStreamForId(
+        selectedStreamId,
+        undefined
+      );
+    } else {
+      const tracks = await Youtube.trackSearchByString(query, undefined, false);
+      videoWithStream = await Youtube.getStreamForId(tracks[0]?.id, undefined);
+    }
     return download(this.window.getBrowserWindow(), videoWithStream?.stream, {
       filename: `${filename}.${videoWithStream?.format}`,
-      directory: this.store.getOption('downloads.dir'),
+      directory: this.store.getOption("downloads.dir"),
       onStarted,
       onProgress: _.throttle(onProgress, 1000),
-      onCompleted
+      onCompleted,
     });
   }
 }
