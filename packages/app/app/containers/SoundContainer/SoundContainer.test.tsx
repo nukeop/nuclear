d03ce0bb773b71897a1d064ddca33ddc4cde669f
nukeop <12746779+nukeop@users.noreply.github.com>
2024-07-17 02:04:41 +0200

    Rewrite sound container tests

--- packages/app/app/containers/SoundContainer/SoundContainer.test.tsx
+++ packages/app/app/containers/SoundContainer/SoundContainer.test.tsx
@@ -1,53 +1,80 @@
-import '../../../__mocks__/AudioContext';
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { waitFor } from '@testing-library/react';
 
-import { AnyProps, configureMockStore, TestStoreProvider } from '../../../test/testUtils';
+import { AnyProps, mountComponent } from '../../../test/testUtils';
 import { buildStoreState } from '../../../test/storeBuilders';
 import SoundContainer from '.';
 
+jest.mock('react-hls-player', () => {
+  return {
+    __esModule: true,
+    default: () => <div />
+  };
+});
+
+jest.mock('react-hifi', () => {
+  let finishPlayingCallback: () => void;
+
+  const MockReactHifi = ({ onFinishedPlaying }: { onFinishedPlaying: () => void }) => {
+    finishPlayingCallback = onFinishedPlaying;
+    return null;
+  };
+
+  MockReactHifi.status = {
+    PAUSED: 'PAUSED'
+  };
+
+  return {
+    ...jest.requireActual('react-hifi'),
+    __esModule: true,
+    default: jest.fn().mockImplementation(MockReactHifi),
+    status: {
+      PAUSED: 'PAUSED'
+    },
+    finishPlaying: () => {
+      if (finishPlayingCallback) {
+        finishPlayingCallback();
+      }
+    },
+    finishPlayingCallback
+  };
+});
+
+type ReactHiFiMock = {
+  default: React.FC<{ onFinishedPlaying: () => void }>;
+  finishPlaying: () => void;
+  finishPlayingCallback: () => void;
+}
+
 describe('Sound Container - visualizer shuffle functionality', () => {
   it('Should change visualizer preset on song end, if the setting is enabled', async () => {
-    const { store } = mountComponent({
-      queue: {
-        currentSong: 0,
-        queueItems: [{
-          artist: 'test artist 1',
-          name: 'test track 1',
-          streams: [{
-            duration: 1,
-            title: 'test track 1',
-            skipSegments: [],
-            stream: 'stream URL'
-          }]
-        }]
-      },
-      player: {
-        playbackStatus: 'PLAYING'
-      }
-    });
+    const { store, component } = mountSoundContainer();
     const state = store.getState();
+    const reactHifiMock = (await import('react-hifi')) as unknown as ReactHiFiMock;
+
+    await waitFor(() => expect(state.settings['visualizer.preset']).toBe('test preset'));
+    
+    await waitFor(() => expect(reactHifiMock.finishPlayingCallback).toBeDefined());
+    await waitFor(() => expect(reactHifiMock.finishPlaying).toBeDefined());
+
+    reactHifiMock.finishPlaying();
+
     await waitFor(() => expect(state.settings['visualizer.preset']).not.toBe('test preset'), { timeout: 2000 });
   });
 
-  const mountComponent = (initialStore?: AnyProps) => {
-    const store = configureMockStore({
+  const mountSoundContainer = (initialStore?: AnyProps) => {
+    const initialState = initialStore ?? {
       ...buildStoreState()
         .withTracksInPlayQueue()
+        .withPlayer({playbackStatus: 'PLAYING'})
         .withSettings({
           skipSponsorblock: true,
           ['visualizer.shuffle']: true,
           ['visualizer.preset']: 'test preset'
         })
-        .build(),
-      ...initialStore
-    });
+        .build()
+    };
 
-    const component = render(
-      <TestStoreProvider store={store}>
-        <SoundContainer />
-      </TestStoreProvider>
-    );
-    return { component, store };
+    return mountComponent(<SoundContainer />, ['/'], initialState);
   };
 });
