aea6730e884c05f649d8af6fba52ffbbc4ae2667
nukeop <12746779+nukeop@users.noreply.github.com>
2024-08-16 22:13:43 +0200

    Replace the old TrackRow component in the TagView with a TrackTable

---
+++ packages/app/app/components/TagView/TagTopTracks/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import { useTranslation } from 'react-i18next';
+
+import { Button, TrackTable } from '@nuclear/ui';
+
+import styles from './styles.scss';
+import TrackRow from '../../TrackRow';
+import TrackTableContainer from '../../../containers/TrackTableContainer';
+
+type TagTopTrack = {
+  artist: {
+    name: string;
+  };
+  name: string;
+  image: { '#text': string }[];
+};
+
+type TagTopTracksProps = {
+  tracks: TagTopTrack[];
+  addToQueue: (track: { artist: string; name: string; thumbnail: string }) => void;
+}
+
+const TagTopTracks: React.FC<TagTopTracksProps> = ({ tracks, addToQueue }) => {
+  const { t } = useTranslation('tags');
+
+  return (
+    <div className={styles.tag_top_tracks}>
+      <Button
+        primary
+        href='#'
+        aria-label={t('queue-add')}
+        onClick={() => {
+          tracks.map((track) => {
+            addToQueue({
+              artist: track.artist.name,
+              name: track.name,
+              thumbnail: track.image[1]['#text']
+            });
+          });
+        }}
+        className={styles.add_all_button}
+      >
+        <FontAwesome name='plus' /> {t('queue-add')}
+      </Button>
+      <TrackTableContainer
+        tracks={tracks}
+        displayAlbum={false}
+        displayDeleteButton={false}
+      />
+    </div>
+  );
+};
+
+export default TagTopTracks;
