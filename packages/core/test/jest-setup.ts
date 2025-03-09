c8880cbce65245e996ffa79d7db1babc8b66267e
nukeop <12746779+nukeop@users.noreply.github.com>
2025-03-10 00:34:51 +0100

    Update jest environment in core tests to handle browser apis

--- packages/core/test/jest-setup.ts
+++ packages/core/test/jest-setup.ts
@@ -8,6 +8,7 @@ import {
   TransformStream
 } from 'node:stream/web';
 
+import {Blob} from 'buffer';
 
 // ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 // ref: https://github.com/jsdom/jsdom/issues/2524
@@ -16,7 +17,10 @@ Object.defineProperties(global, {
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
   ReadableStream: { value: ReadableStream },
-  TransformStream: { value: TransformStream }
+  TransformStream: { value: TransformStream },
+  Blob: { value: Blob },
+  MessagePort: { value: {} },
+  DOMException: { value: Error }
 });
 
 Object.defineProperty(global, 'require', {
