7b35d3fe77e2f221a3001851ea8c2d1f9c3c3d4d
nukeop <12746779+nukeop@users.noreply.github.com>
2025-06-22 03:05:10 +0200

    Fix spotify test mocks, deprecate spotify meta plugin

--- packages/core/src/plugins/meta/index.ts
+++ packages/core/src/plugins/meta/index.ts
@@ -3,6 +3,5 @@ export { default as DiscogsMetaProvider } from './discogs';
 export { default as MusicbrainzMetaProvider } from './musicbrainz';
 export { default as BandcampMetaProvider } from './bandcamp';
 export { default as iTunesMusicMetaProvider } from './itunesmusic';
-export { SpotifyMetaProvider } from './spotify';
 export { SoundcloudMetaProvider } from './SoundcloudPlugin';
 export { SoytifyMetaProvider } from './soytify/soytify';
