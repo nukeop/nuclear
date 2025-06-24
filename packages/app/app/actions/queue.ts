85e85d72ab0bd5c082ca34a02afbe255f1a12a58
nukeop <12746779+nukeop@users.noreply.github.com>
2025-06-25 00:50:09 +0200

    Fix imports

--- packages/app/app/actions/queue.ts
+++ packages/app/app/actions/queue.ts
@@ -14,7 +14,7 @@ import { RootState } from '../reducers';
 import { LocalLibraryState } from './local';
 import { Queue } from './actionTypes';
 import StreamProviderPlugin from '@nuclear/core/src/plugins/streamProvider';
-import { isSuccessCacheEntry, NuclearStreamMappingsService } from '@nuclear/core/src/rest/nuclear';
+import { isSuccessCacheEntry, NuclearStreamMappingsService } from '@nuclear/core/src/rest/nuclear/StreamMappings';
 import { queue as queueSelector } from '../selectors/queue';
 import { error } from './toasts';
 import { random } from 'lodash';
