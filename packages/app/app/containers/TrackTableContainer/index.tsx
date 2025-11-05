a89c579ea52a8536883cd05235e8339b78f9f221
Dylan Hutson <dylan@jointaccounts.com>
2025-11-05 16:46:56 -0600

    Hover song implimented and in the right place

--- packages/app/app/containers/TrackTableContainer/index.tsx
+++ packages/app/app/containers/TrackTableContainer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { Icon } from 'semantic-ui-react';
@@ -9,6 +9,7 @@ import { GridTrackTable, areTracksEqualByName } from '@nuclear/ui';
 import { TrackTableProps } from '@nuclear/ui/lib/components/TrackTable';
 import { TrackTableSettings } from '@nuclear/ui/lib/components/TrackTable/types';
 import { Track } from '@nuclear/ui/lib/types';
+import { Column } from 'react-table';
 
 import { playlistsSelectors } from '../../selectors/playlists';
 import * as downloadsActions from '../../actions/downloads';
@@ -29,7 +30,7 @@ export type TrackTableContainerProps<T extends Track> = TrackTableSettings & {
   displayAddToFavorites?: boolean;
 };
 
-function TrackTableContainer<T extends Track> ({
+function TrackTableContainer<T extends Track>({
   tracks,
   onDelete,
   onReorder,
@@ -44,71 +45,93 @@ function TrackTableContainer<T extends Track> ({
   const playlists = useSelector(playlistsSelectors.localPlaylists);
   const favoriteTracks: Track[] = useSelector(favoritesSelectors.tracks);
 
+  const [hoveredTrack, setHoveredTrack] = useState<Track | null>(null);
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     dispatch(favoritesActions.readFavorites());
   }, [dispatch]);
 
-  const isTrackFavorite = (track: Track) => !_.isNil(favoriteTracks.find(t => areTracksEqualByName(t, track)));
+  const isTrackFavorite = (track: Track) =>
+    !_.isNil(favoriteTracks.find(t => areTracksEqualByName(t, track)));
 
-  const onAddToQueue = useCallback((track: Track) => {
-    dispatch(queueActions.addToQueue(queueActions.toQueueItem(track)));
-  }, [dispatch]);
+  const onAddToQueue = useCallback(
+    (track: Track) => dispatch(queueActions.addToQueue(queueActions.toQueueItem(track))),
+    [dispatch]
+  );
 
-  const onPlayNow = useCallback((track: Track) => {
-    dispatch(queueActions.playTrack(null, queueActions.toQueueItem(track)));
-  }, [dispatch]);
+  const onPlayNow = useCallback(
+    (track: Track) => dispatch(queueActions.playTrack(null, queueActions.toQueueItem(track))),
+    [dispatch]
+  );
 
-  const onPlayNext = useCallback((track: Track) => {
-    dispatch(queueActions.playNext(queueActions.toQueueItem(track)));
-  }, [dispatch]);
+  const onPlayNext = useCallback(
+    (track: Track) => dispatch(queueActions.playNext(queueActions.toQueueItem(track))),
+    [dispatch]
+  );
 
-  const onPlayAll = useCallback((tracks: Track[]) => {
-    dispatch(queueActions.clearQueue());
-    dispatch(queueActions.addPlaylistTracksToQueue(tracks));
-    dispatch(queueActions.selectSong(0));
-    dispatch(playerActions.startPlayback(false));
-  }, [dispatch]);
+  const onPlayAll = useCallback(
+    (tracksArr: Track[]) => {
+      dispatch(queueActions.clearQueue());
+      dispatch(queueActions.addPlaylistTracksToQueue(tracksArr));
+      dispatch(queueActions.selectSong(0));
+      dispatch(playerActions.startPlayback(false));
+    },
+    [dispatch]
+  );
 
-  const onAddToFavorites = useCallback((track: Track) => {
-    dispatch(favoritesActions.addFavoriteTrack(track));
-  }, [dispatch]);
+  const onAddToFavorites = useCallback(
+    (track: Track) => dispatch(favoritesActions.addFavoriteTrack(track)),
+    [dispatch]
+  );
 
-  const onRemoveFromFavorites = useCallback((track: Track) => {
-    dispatch(favoritesActions.removeFavoriteTrack(track));
-  }, [dispatch]);
+  const onRemoveFromFavorites = useCallback(
+    (track: Track) => dispatch(favoritesActions.removeFavoriteTrack(track)),
+    [dispatch]
+  );
 
-  const onAddToDownloads = useCallback((track: Track) => {
-    dispatch(downloadsActions.addToDownloads(null, track));
-  }, [dispatch]);
+  const onAddToDownloads = useCallback(
+    (track: Track) => dispatch(downloadsActions.addToDownloads(null, track)),
+    [dispatch]
+  );
 
-  const onAddToPlaylist = useCallback((track: Track, playlist: Playlist ) => {
-    const clonedTrack = {...safeAddUuid(track)};
-    const foundPlaylist = playlists.data?.find(p => p.name === playlist.name);
-    const newPlaylist = {
-      ...foundPlaylist,
-      tracks: [
-        ...foundPlaylist.tracks,
-        clonedTrack
-      ]
-    };
-    dispatch(playlistActions.updatePlaylist(newPlaylist));
-  }, [dispatch, playlists]);
+  const onAddToPlaylist = useCallback(
+    (track: Track, playlist: Playlist) => {
+      const clonedTrack = { ...safeAddUuid(track) };
+      const foundPlaylist = playlists.data?.find(p => p.name === playlist.name);
+      if (!foundPlaylist) {
+        return;
+      }
+      const newPlaylist = {
+        ...foundPlaylist,
+        tracks: [...foundPlaylist.tracks, clonedTrack]
+      };
+      dispatch(playlistActions.updatePlaylist(newPlaylist));
+    },
+    [dispatch, playlists]
+  );
 
   const onCreatePlaylist = useCallback(
-    (track: Track, { name }: { name: string } ) => {
-      const clonedTrack = {...safeAddUuid(track)};
-      if (clonedTrack.artist.name) {
-        _.set(clonedTrack, 'artist', clonedTrack.artist.name);
+    (track: Track, { name }: { name: string }) => {
+      const clonedTrack = { ...safeAddUuid(track) };
+      if ((clonedTrack as any).artist?.name) {
+        _.set(clonedTrack, 'artist', (clonedTrack as any).artist.name);
       }
       dispatch(playlistActions.addPlaylist([clonedTrack], name));
     },
     [dispatch]
   );
 
-  const onDragEnd = useCallback<TrackTableProps<Track>['onDragEnd']>((result) => {
-    const { source, destination } = result;
-    onReorder(source.index, destination.index);
-  }, [onReorder]);
+  const onDragEnd = useCallback<TrackTableProps<Track>['onDragEnd']>(
+    result => {
+      if (!onReorder) {
+        return;
+      }
+      const { source, destination } = result;
+      onReorder(source.index, destination.index);
+    },
+    [onReorder]
+  );
 
   const popupTranstation = useTranslation('track-popup').t;
   const popupStrings = {
@@ -138,33 +161,66 @@ function TrackTableContainer<T extends Track> ({
     filterInputPlaceholder: trackTableTranslation('filter-input-placeholder')
   };
 
-  return <TrackTableComponent
-    {...settings}
-    tracks={tracks}
-    positionHeader={<Icon name='hashtag' />}
-    thumbnailHeader={<Icon name='image' />}
-    artistHeader={t('artist')}
-    titleHeader={t('title')}
-    albumHeader={t('album')}
-    durationHeader={t('duration')}
-    strings={trackTableStrings}
-    playlists={playlists.data}
-    customColumns={customColumns}
-    onAddToQueue={onAddToQueue}
-    onPlay={onPlayNow}
-    onPlayNext={onPlayNext}
-    onPlayAll={onPlayAll}
-    onAddToFavorites={Boolean(displayAddToFavorites) && onAddToFavorites}
-    onRemoveFromFavorites={onRemoveFromFavorites}
-    onAddToDownloads={Boolean(displayAddToDownloads) && onAddToDownloads}
-    onAddToPlaylist={onAddToPlaylist}
-    onCreatePlaylist={onCreatePlaylist}
-    onDelete={onDelete}
-    onDragEnd={Boolean(onReorder) && onDragEnd}
-    popupActionStrings={popupStrings}
+  const enhancedCustomColumns: Column<T>[] = [
+    ...(customColumns || []) as Column<T>[],
+    {
+      id: 'hoverOverlay',
+      Header: '',
+      Cell: ({ row }: { row: any }) => {
+        const track = row.original as T;
+        return (
+          <div
+            onMouseEnter={(e: React.MouseEvent) => {
+              setHoveredTrack(track as unknown as Track);
+              setCursorPos({ x: e.pageX, y: e.pageY });
+            }}
+            onMouseMove={(e: React.MouseEvent) => {
+              setCursorPos({ x: e.pageX, y: e.pageY });
+            }}
+            onMouseLeave={() => setHoveredTrack(null)}
+            style={{ cursor: 'pointer' }}
+          >
+            <div>{(track as unknown as Track).title}</div>
+            {/* artist might be an object; use optional chaining */}
+            {((track as any).artist && (track as any).artist.name) && (
+              <div style={{ fontSize: '0.85em', color: '#666' }}>
+                {(track as any).artist.name}
+              </div>
+            )}
+          </div>
+        );
+      }
+    }
+  ];
 
-    isTrackFavorite={isTrackFavorite}
-  />;
+  return (
+    <TrackTableComponent<T>
+      {...settings}
+      tracks={tracks}
+      positionHeader={<Icon name='hashtag' />}
+      thumbnailHeader={<Icon name='image' />}
+      artistHeader={t('artist')}
+      titleHeader={t('title')}
+      albumHeader={t('album')}
+      durationHeader={t('duration')}
+      strings={trackTableStrings}
+      playlists={playlists.data}
+      customColumns={enhancedCustomColumns}
+      onAddToQueue={onAddToQueue}
+      onPlay={onPlayNow}
+      onPlayNext={onPlayNext}
+      onPlayAll={onPlayAll}
+      onAddToFavorites={displayAddToFavorites ? onAddToFavorites : undefined}
+      onRemoveFromFavorites={onRemoveFromFavorites}
+      onAddToDownloads={displayAddToDownloads ? onAddToDownloads : undefined}
+      onAddToPlaylist={onAddToPlaylist}
+      onCreatePlaylist={onCreatePlaylist}
+      onDelete={onDelete}
+      onDragEnd={onReorder ? onDragEnd : undefined}
+      popupActionStrings={popupStrings}
+      isTrackFavorite={isTrackFavorite}
+    />
+  );
 }
 
 export default TrackTableContainer;
