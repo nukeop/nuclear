c37bee310fc4e4bdf099efffbacb4876bef3e1de
Dylan Hutson <dylan@jointaccounts.com>
2025-11-06 12:47:46 -0600

    fixed Audiobooks

---
+++ packages/app/app/containers/AudiobookContainer/AudiobooksTab.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AudiobooksContainer: React.FC = () => {
+  return (
+    <div>
+      <h1>Audiobooks</h1>
+      <p>Welcome to the Audiobooks section!</p>
+    </div>
+  );
+};
+
+export default AudiobooksContainer;
