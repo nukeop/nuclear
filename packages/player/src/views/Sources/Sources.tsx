0b9cc649960c02d4b4dd2c7cc301a8f9201628f5
nukeop <12746779+nukeop@users.noreply.github.com>
2026-03-15 00:37:57 +0100

    Sources view stub

---
+++ packages/player/src/views/Sources/Sources.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import { ViewShell } from '@nuclearplayer/ui';
+
+export const Sources: FC = () => {
+  return (
+    <ViewShell data-testid="sources-view" title="Sources">
+      <div />
+    </ViewShell>
+  );
+};
