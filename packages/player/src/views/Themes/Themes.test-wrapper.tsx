68d8497c85e0900d9e6ac0ff8d8d479dcf57c58d
nukeop <12746779+nukeop@users.noreply.github.com>
2026-03-29 03:23:25 +0200

    Wrappers for themes

--- packages/player/src/views/Themes/Themes.test-wrapper.tsx
+++ packages/player/src/views/Themes/Themes.test-wrapper.tsx
@@ -7,6 +7,46 @@ import {
   useAdvancedThemeStore,
 } from '../../stores/advancedThemeStore';
 
+class ThemeStoreItemWrapper {
+  constructor(private element: HTMLElement) {}
+
+  get name() {
+    return within(this.element).getByTestId('theme-store-item-name')
+      .textContent;
+  }
+
+  get author() {
+    return within(this.element).getByTestId('theme-store-item-author')
+      .textContent;
+  }
+
+  get description() {
+    return within(this.element).getByTestId('theme-store-item-description')
+      .textContent;
+  }
+
+  get installButton() {
+    return within(this.element).getByRole('button');
+  }
+
+  get isInstalled() {
+    return this.installButton.textContent?.includes('Installed') ?? false;
+  }
+
+  get data() {
+    return {
+      name: this.name,
+      author: this.author,
+      description: this.description,
+      isInstalled: this.isInstalled,
+    };
+  }
+
+  install = async () => {
+    await userEvent.click(this.installButton);
+  };
+}
+
 export const ThemesWrapper = {
   async mount(opts?: {
     advancedThemes?: AdvancedThemeFile[];
@@ -42,5 +82,37 @@ export const ThemesWrapper = {
   },
   async selectDefaultTheme() {
     await this.selectAdvancedTheme('Default');
+  },
+
+  async goToStoreTab() {
+    await userEvent.click(screen.getByRole('tab', { name: 'Store' }));
+  },
+
+  async goToLocalTab() {
+    await userEvent.click(screen.getByRole('tab', { name: 'Local' }));
+  },
+
+  async getStoreThemes() {
+    const items = await screen.findAllByTestId('theme-store-item');
+    return items.map((element) => new ThemeStoreItemWrapper(element));
+  },
+
+  get storeThemeNames() {
+    return screen
+      .queryAllByTestId('theme-store-item')
+      .map(
+        (element) =>
+          within(element).getByTestId('theme-store-item-name').textContent,
+      );
+  },
+
+  async searchStore(query: string) {
+    const input = screen.getByPlaceholderText('Search themes...');
+    await userEvent.clear(input);
+    await userEvent.type(input, query);
+  },
+
+  get storeErrorState() {
+    return screen.findByTestId('theme-store-error');
   },
 };
