c2065f0282484c98a3f1114e03802e56f3fca70c
nukeop <12746779+nukeop@users.noreply.github.com>
2026-04-01 01:27:07 +0200

    Reset other themes on selection

--- packages/player/src/views/Themes/AdvancedThemeSelect.tsx
+++ packages/player/src/views/Themes/AdvancedThemeSelect.tsx
@@ -3,17 +3,22 @@ import { SectionShell, Select } from '@nuclearplayer/ui';
 
 import { useAdvancedThemeOptions } from '../../hooks/useAdvancedThemeOptions';
 import { loadAndApplyAdvancedThemeFromFile } from '../../services/advancedThemeService';
+import { useThemeStore } from '../../stores/themeStore';
 
 export const AdvancedThemeSelect = () => {
   const { t } = useTranslation('themes');
   const options = useAdvancedThemeOptions();
+  const activeTheme = useThemeStore((state) => state.activeTheme);
+  const value = activeTheme.type === 'advanced' ? activeTheme.path : '';
 
   return (
     <SectionShell data-testid="advanced-themes" title={t('advanced')}>
       <div className="max-w-80 p-1">
         <Select
           description={t('description')}
+          placeholder={t('selectPlaceholder')}
           options={options}
+          value={value}
           onValueChange={loadAndApplyAdvancedThemeFromFile}
         />
       </div>
--- packages/player/src/views/Themes/MarketplaceThemeSelect.tsx
+++ packages/player/src/views/Themes/MarketplaceThemeSelect.tsx
@@ -7,7 +7,9 @@ import { useThemeStore } from '../../stores/themeStore';
 export const MarketplaceThemeSelect = () => {
   const { t } = useTranslation('themes');
   const marketplaceThemes = useThemeStore((state) => state.marketplaceThemes);
+  const activeTheme = useThemeStore((state) => state.activeTheme);
   const options = useMarketplaceThemeOptions();
+  const value = activeTheme.type === 'marketplace' ? activeTheme.id : '';
 
   if (marketplaceThemes.length === 0) {
     return null;
@@ -16,7 +18,12 @@ export const MarketplaceThemeSelect = () => {
   return (
     <SectionShell data-testid="marketplace-themes" title={t('marketplace')}>
       <div className="max-w-80 p-1">
-        <Select options={options} onValueChange={() => {}} />
+        <Select
+          placeholder={t('selectPlaceholder')}
+          options={options}
+          value={value}
+          onValueChange={useThemeStore.getState().selectMarketplaceTheme}
+        />
       </div>
     </SectionShell>
   );
