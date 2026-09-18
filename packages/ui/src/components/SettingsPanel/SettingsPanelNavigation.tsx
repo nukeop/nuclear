d467b7e54d4c23d0672437981d76a3bc97c42901
nukeop <12746779+nukeop@users.noreply.github.com>
2026-09-18 12:50:14 +0200

    Scrollable settings nav bar

--- packages/ui/src/components/SettingsPanel/SettingsPanelNavigation.tsx
+++ packages/ui/src/components/SettingsPanel/SettingsPanelNavigation.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 
+import { ScrollableArea } from '../ScrollableArea/ScrollableArea';
 import { SettingsNavigationSection } from './SettingsPanel';
 import { SettingsPanelNavigationSection } from './SettingsPanelNavigationSection';
 
@@ -12,10 +13,12 @@ export const SettingsPanelNavigation: FC<SettingsPanelNavigationProps> = ({
   sections,
   footer,
 }) => (
-  <nav className="border-border flex w-56 shrink-0 flex-col gap-4 overflow-y-auto border-r-(length:--border-width) p-4">
-    {sections.map((section) => (
-      <SettingsPanelNavigationSection key={section.id} section={section} />
-    ))}
-    {footer && <div className="mt-auto">{footer}</div>}
+  <nav className="border-border flex w-56 shrink-0 flex-col border-r-(length:--border-width)">
+    <ScrollableArea viewportClassName="gap-4 p-4">
+      {sections.map((section) => (
+        <SettingsPanelNavigationSection key={section.id} section={section} />
+      ))}
+      {footer && <div className="mt-auto">{footer}</div>}
+    </ScrollableArea>
   </nav>
 );
